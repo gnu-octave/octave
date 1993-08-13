e406e20f6c0c9d2eb8a5b9355d0174d9bf0e1eb8
jwe <unknown>
1993-08-13 20:09:16 +0000

    [project @ 1993-08-13 20:09:16 by jwe] Initial revision

---
+++ scripts/general/is_symmetric.m
@@ -0,0 +1,23 @@
+function retval = is_symmetric (x,tol)
+
+# usage: is_symmetric (x{,tol})
+#
+# If x is symmetric, return the dimension of x, otherwise, return 0.
+#
+# See also: size, rows, columns, length, is_matrix, is_scalar, 
+# is_square, is_vector
+
+  if (nargin == 1 || nargin == 2)
+    if ((retval = is_square (x)))
+      if (nargin == 1)
+	tol = eps;
+      endif
+      if (norm (x - x') / norm(x) > tol)
+        retval = 0;
+      endif
+    endif
+  else
+    error ("usage: is_symmetric (x {,tol})");
+  endif
+
+endfunction

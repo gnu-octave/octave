f3cda31cb94faa0b0a84e362f5ddc7656a188048
jwe <unknown>
1993-11-30 20:21:19 +0000

    [project @ 1993-11-30 20:21:19 by jwe] Initial revision

---
+++ acconfig.h
@@ -0,0 +1,53 @@
+/* acconfig.h
+
+   Descriptive text for the C preprocessor macros that are needed by
+   Octave.
+
+   Leave the following blank line there!!  Autoheader needs it.  */
+
+
+/* Define if you don't have acosh(). */
+#undef ACOSH_MISSING
+
+/* Define if you don't have asinh(). */
+#undef ASINH_MISSING
+
+/* Define if you don't have atanh(). */
+#undef ATANH_MISSING
+
+/* Define if your Fortran compiler appends an underscore to external
+   names. */ 
+#undef F77_APPEND_UNDERSCORE
+
+/* Define if you don't have FSQP. */
+#undef FSQP_MISSING
+
+/* Define if you have sgtty.h. */
+#undef HAVE_SGTTY_H
+
+/* Define if you have termios.h. */
+#undef HAVE_TERMIOS_H
+
+/* Define if you have termio.h. */
+#undef HAVE_TERMIO_H
+
+/* Define if you don't have NPSOL. */
+#undef NPSOL_MISSING
+
+/* Define if this is Octave. */
+#undef OCTAVE_SOURCE
+
+/* Define if you don't have QPSOL. */
+#undef QPSOL_MISSING
+
+/* Define if using -fexternal-templates with g++. */
+#undef USE_EXTERNAL_TEMPLATES
+
+/* Define if using dld for dynamic linking of object files. */
+#undef WITH_DLD
+
+
+/* Leave that blank line there!!  Autoheader needs it.
+   If you're adding to this file, keep in mind:
+   The entries are in sort -df order: alphabetical, case insensitive,
+   ignoring punctuation (such as underscores).  */

2e8d219caff76b6d2b6202dea54ede1754268884
jwe <unknown>
1994-07-08 02:08:49 +0000

    [project @ 1994-07-08 02:08:49 by jwe] Initial revision

---
+++ src/oct-obj.h
@@ -0,0 +1,35 @@
+// oct-obj.h                                            -*- C -*-
+/*
+
+Copyright (C) 1992, 1993, 1994 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include "Array.h"
+
+class tree_constant;
+
+typedef Array<tree_constant> Octave_object;
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/

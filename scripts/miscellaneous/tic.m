c87d4d1da1898404619e4f3958b3c7a807af357e
jwe <unknown>
1994-11-09 18:04:15 +0000

    [project @ 1994-11-09 18:04:15 by jwe] Initial revision

---
+++ scripts/miscellaneous/tic.m
@@ -0,0 +1,35 @@
+# Copyright (C) 1994 John W. Eaton
+#
+# This file is part of Octave.
+#
+# Octave is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# Octave is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Octave; see the file COPYING.  If not, write to the Free
+# Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+function tic ()
+
+# usage: tic
+#
+# Set a wall-clock timer.
+#
+# See also: toc, clock, etime, cputime
+
+  if (nargin != 0)
+    warning ("tic: ignoring extra arguments");
+  endif
+
+  global _time_tic_called;
+
+  _time_tic_called = clock ();
+
+endfunction

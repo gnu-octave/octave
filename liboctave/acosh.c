c2cf81d5472614badf4253fe5ac0f7ed93e96b33
jwe <unknown>
1995-01-27 16:45:21 +0000

    [project @ 1995-01-27 16:45:21 by jwe] Initial revision

---
+++ liboctave/acosh.c
@@ -0,0 +1,46 @@
+/*
+
+Copyright (C) 1995 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef HAVE_ACOSH
+
+#include "f77-uscore.h"
+
+extern double F77_FCN (dacosh) (double*);
+
+double
+acosh (double x)
+{
+  return F77_FCN (dacosh) (&x);
+}
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/
---
+++ liboctave/lgamma.c
@@ -0,0 +1,58 @@
+/*
+
+Copyright (C) 1995 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef HAVE_LGAMMA
+
+#include "f77-uscore.h"
+
+/* If the system doesn't have lgamma, assume that it doesn't have
+   signgam either */
+
+int signgam;
+
+extern int F77_FCN (dlgams) (double*, double*, double*);
+
+double
+lgamma (double x)
+{
+  double result;
+  double sgngam;
+
+  F77_FCN (dlgams) (&x, &result, &sgngam);
+
+  signgam = (int) sgngam;
+
+  return result;
+}
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/

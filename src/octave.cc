ad1aefca4ab4cce5acf16b8ed40a20264b87c9b0
jwe <unknown>
1995-02-15 03:51:08 +0000

    [project @ 1995-02-15 03:51:08 by jwe]

--- src/octave.cc
+++ src/octave.cc
@@ -374,7 +374,7 @@ static void
 verbose_usage (void)
 {
   cout << "\n"
-       << OCTAVE_STARTUP_MESSAGE
+       << OCTAVE_NAME_VERSION_AND_COPYRIGHT
        << "\n\n"
        << "  usage: " << usage_string
        << "\n\n"
@@ -728,20 +728,20 @@ DEFUN ("warranty", Fwarranty, Swarranty, 0, 0,
   Octave_object retval;
 
   ostrstream output_buf;
-  output_buf << "\n    " OCTAVE_NAME_VERSION_AND_COPYRIGHT "\n\n\
-    This program is free software; you can redistribute it and/or modify\n\
-    it under the terms of the GNU General Public License as published by\n\
-    the Free Software Foundation; either version 2 of the License, or\n\
-    (at your option) any later version.\n\
+  output_buf << "\n" OCTAVE_NAME_VERSION_AND_COPYRIGHT "\n\n\
+This program is free software; you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation; either version 2 of the License, or\n\
+(at your option) any later version.\n\
 \n\
-    This program is distributed in the hope that it will be useful,\n\
-    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
-    GNU General Public License for more details.\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
 \n\
-    You should have received a copy of the GNU General Public License\n\
-    along with this program. If not, write to the Free Software\n\
-    Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program. If not, write to the Free Software\n\
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.\n\
 \n";
 
   output_buf << ends;

f4ef8edd18103543155663e24fdcf9a8b4223b93
jwe <unknown>
1995-03-30 04:50:34 +0000

    [project @ 1995-03-30 04:50:34 by jwe] Initial revision

---
+++ scripts/time/clock.m
@@ -0,0 +1,38 @@
+# Copyright (C) 1995 John W. Eaton
+# 
+# This file is part of Octave.
+# 
+# Octave is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+# 
+# Octave is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Octave; see the file COPYING.  If not, write to the Free
+# Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+function retval = clock ()
+
+# usage: clock ()
+#
+# Return current date and time in vector with elements
+#
+#   [ year, month, day-of-month, hour, minute, second ]
+
+  tm = localtime (time ());
+
+  retval = zeros (1, 6);
+
+  retval(1) = tm.tm_year + 1900;
+  retval(2) = tm.tm_mon + 1;
+  retval(3) = tm.tm_mday;
+  retval(4) = tm.tm_hour;
+  retval(5) = tm.tm_min;
+  retval(6) = tm.tm_sec + tm.tm_usec / 1e6;
+
+endfunction

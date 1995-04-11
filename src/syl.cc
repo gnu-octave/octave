f5c89b7a1c8b8fb08bb3a8d2acb156e00796b493
jwe <unknown>
1995-04-11 00:45:35 +0000

    [project @ 1995-04-11 00:45:35 by jwe]

--- src/syl.cc
+++ src/syl.cc
@@ -43,16 +43,16 @@ Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 extern "C"
 {
-  int F77_FCN (dtrsyl) (const char*, const char*, const int*,
-			const int*, const int*, const double*,
-			const int*, const double*, const int*,
-			const double*, const int*, double*, int*,
+  int F77_FCN (dtrsyl) (const char*, const char*, const int&,
+			const int&, const int&, const double*,
+			const int&, const double*, const int&,
+			const double*, const int&, double&, int&,
 			long, long);
  
-  int F77_FCN (ztrsyl) (const char*, const char*, const int*,
-			const int*, const int*, const Complex*,
-			const int*, const Complex*, const int*,
-			const Complex*, const int*, double*, int*,
+  int F77_FCN (ztrsyl) (const char*, const char*, const int&,
+			const int&, const int&, const Complex*,
+			const int&, const Complex*, const int&,
+			const Complex*, const int&, double&, int&,
 			long, long);
 }
 
@@ -146,12 +146,11 @@ DEFUN_DLD_BUILTIN ("syl", Fsyl, Ssyl, 4, 1,
   
 	double scale;
 	int info;
-	int one = 1;
   
-	F77_FCN (ztrsyl) ("N", "N", &one, &a_nr, &b_nr,
-			  sch_a.fortran_vec (), &a_nr,
-			  sch_b.fortran_vec (), &b_nr,
-			  cx.fortran_vec (), &a_nr, &scale, &info,
+	F77_FCN (ztrsyl) ("N", "N", 1, a_nr, b_nr,
+			  sch_a.fortran_vec (), a_nr,
+			  sch_b.fortran_vec (), b_nr,
+			  cx.fortran_vec (), a_nr, scale, info,
 			  1L, 1L);
 
 	cx = -ua * cx * ub.hermitian ();
@@ -196,12 +195,11 @@ DEFUN_DLD_BUILTIN ("syl", Fsyl, Ssyl, 4, 1,
   
 	double scale;
 	int info;
-	int one = 1;
 
-	F77_FCN (dtrsyl) ("N", "N", &one, &a_nr, &b_nr,
-			  sch_a.fortran_vec (), &a_nr, 
-			  sch_b.fortran_vec (), &b_nr,
-			  cx.fortran_vec (), &a_nr, &scale, &info,
+	F77_FCN (dtrsyl) ("N", "N", 1, a_nr, b_nr,
+			  sch_a.fortran_vec (), a_nr, 
+			  sch_b.fortran_vec (), b_nr,
+			  cx.fortran_vec (), a_nr, scale, info,
 			  1L, 1L);
 
 	if (info)

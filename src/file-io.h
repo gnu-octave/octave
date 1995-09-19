998da990239301930be0be395106d08874e49348
jwe <unknown>
1995-09-19 03:32:33 +0000

    [project @ 1995-09-19 03:32:33 by jwe]

--- src/file-io.h
+++ src/file-io.h
@@ -23,43 +23,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // Written by John C. Campbell <jcc@che.utexas.edu>.
 
-#if !defined (octave_files_h)
-#define octave_files_h 1
-
-#include <Pix.h>
-
-#include "oct-obj.h"
+#if !defined (octave_file_io_h)
+#define octave_file_io_h 1
 
 extern void initialize_file_io (void);
 
 extern void close_files (void);
-
-class
-file_info
-{
-public:
-  file_info (void);
-  file_info (int num, const char *nm, FILE *t, const char *md);
-  file_info (const file_info& f);
-
-  file_info& operator = (const file_info& f);
-
-  ~file_info (void);
-
-  int number (void) const;
-  const char *name (void) const;
-  FILE *fptr (void) const;
-  const char *mode (void) const;
-
-  int eof (void) const;
-  int error (void) const;
-
-private:
-  int file_number;
-  char *file_name;
-  FILE *file_fptr;
-  char *file_mode;
-};
 
 #endif
 

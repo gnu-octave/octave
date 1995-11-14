09c4842f8265204c49b5fd6361f4d739f314e157
jwe <unknown>
1995-11-14 20:27:50 +0000

    [project @ 1995-11-14 20:24:15 by jwe]

--- liboctave/dbleSVD.cc
+++ liboctave/dbleSVD.cc
@@ -29,6 +29,8 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <config.h>
 #endif
 
+#include <iostream.h>
+
 #include "dbleSVD.h"
 #include "f77-uscore.h"
 #include "mx-inlines.cc"

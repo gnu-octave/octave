307e55d2972d579a7f699d13c78a884e24bdac8a
jwe <unknown>
1995-12-20 06:43:29 +0000

    [project @ 1995-12-20 06:43:29 by jwe] Initial revision

---
+++ liboctave/oct-cmplx.h
@@ -0,0 +1,38 @@
+// oct-cmplx.h                                           -*- C++ -*-
+/*
+
+Copyright (C) 1995 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if !defined (octave_oct_cmplx_h)
+#define octave_oct_cmplx_h 1
+
+#include <complex>
+
+typedef complex<double> Complex;
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/

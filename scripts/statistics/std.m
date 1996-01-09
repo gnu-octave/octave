9989be92a9ed323d4dc9515c008d517edd4760a6
jwe <unknown>
1996-01-09 06:12:13 +0000

    [project @ 1996-01-09 06:12:13 by jwe]

--- scripts/statistics/std.m
+++ scripts/statistics/std.m
@@ -35,14 +35,14 @@ function retval = std (a)
   if (nc == 1 && nr == 1)
     retval = 0;
   elseif (nc == 1 || nr == 1)
-    tmp = sum (a);
     n = length (a);
-    retval = sqrt ((n * sumsq (a) - tmp .* tmp) / (n * (n - 1)));
+    retval = sqrt (sumsq (a - mean (a)) / (n - 1));
   elseif (nr > 1 && nc > 0)
-    tmp = sum (a);
-    retval = sqrt ((nr * sumsq (a) - tmp .* tmp) / (nr * (nr - 1)));
+    tmp = mean (a);
+    tmp = tmp (ones (nr, 1), :);
+    retval = sqrt (sumsq (a - tmp) / (nr - 1));
   else
-    error ("mean: invalid matrix argument");
+    error ("std: invalid matrix argument");
   endif
 
 endfunction

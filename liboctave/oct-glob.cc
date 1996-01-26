eefd006db82195b4893c1a5c4c715a140ad3f8e9
jwe <unknown>
1996-01-26 02:35:55 +0000

    [project @ 1996-01-26 02:35:55 by jwe] Initial revision

---
+++ liboctave/oct-glob.cc
@@ -0,0 +1,45 @@
+// oct-glob.cc                                           -*- C++ -*-
+/*
+
+Copyright (C) 1996 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <fnmatch.h>
+
+#include <string>
+
+#include "oct-glob.h"
+
+bool
+glob_match::match (const string& s)
+{
+  return fnmatch (pat.c_str (), s.c_str (), flags) != FNM_NOMATCH;
+}
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/
---
+++ liboctave/oct-glob.h
@@ -0,0 +1,76 @@
+// oct-glob.h                                              -*- C++ -*-
+/*
+
+Copyright (C) 1996 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if !defined (octave_oct_glob_h)
+#define octave_oct_glob_h 1
+
+#include <string>
+
+class
+glob_match
+{
+public:
+
+  enum opts
+    {
+      pathname = 1,  // No wildcard can ever match `/'.
+      noescape = 2,  // Backslashes don't quote special chars.
+      period = 4     // Leading `.' is matched only explicitly.
+   };
+
+  glob_match (const string& p = string (),
+	      unsigned int f = pathname|noescape|period)
+    : pat (p), flags (f) { }
+
+  glob_match (const glob_match& gm) : pat (gm.pat), flags (gm.flags) { }
+
+  glob_match& operator = (const glob_match& gm)
+    {
+      pat = gm.pat;
+      flags = gm.flags;
+      return *this;
+    }
+
+  ~glob_match (void) { }
+
+  void set_pattern (const string& p) { pat = p; }
+
+  bool match (const string&);
+
+private:
+
+  // Globbing pattern.
+  string pat;
+
+  // Option flags.
+  unsigned int flags;
+};
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/

9e9059f84eb6c147dab8f2d0ea6e41fce37ea84f
jwe <unknown>
1996-02-02 14:10:35 +0000

    [project @ 1996-02-02 14:10:35 by jwe] Initial revision

---
+++ src/SLList-tm.cc
@@ -0,0 +1,42 @@
+// SLList-tm.cc                                          -*- C++ -*-
+/*
+
+Copyright (C) 1996 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+// Instantiate Stacks of tree_matrix* values.
+
+#include "SLStack.h"
+#include "SLStack.cc"
+
+#include "pt-mat.h"
+
+template class SLNode<tree_matrix_row *>;
+template class SLList<tree_matrix_row *>;
+
+template class SLNode<tree_matrix *>;
+template class SLList<tree_matrix *>;
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; page-delimiter: "^/\\*" ***
+;;; End: ***
+*/

046061f35d40d5767cef57e65ff429549fdc23f0
jwe <unknown>
1996-03-03 20:06:08 +0000

    [project @ 1996-03-03 20:04:05 by jwe]

--- liboctave/Array.h
+++ liboctave/Array.h
@@ -28,8 +28,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #pragma interface
 #endif
 
-#define HEAVYWEIGHT_INDEXING 1
-
 #include <cassert>
 #include <cstdlib>
 

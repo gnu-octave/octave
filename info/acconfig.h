c5be1a0d18737453ac648dc40eb685a327b7a741
jwe <unknown>
1996-11-13 03:39:44 +0000

    [project @ 1996-11-13 03:38:08 by jwe]

--- info/acconfig.h
+++
@@ -1,22 +0,0 @@
-/* acconfig.h
-
-   Descriptive text for the C preprocessor macros that are needed by
-   Info.
-
-   Leave the following blank line there!!  Autoheader needs it.  */
-
-
-/* Define if you have sgtty.h. */
-#undef HAVE_SGTTY_H
-
-/* Define if you have termios.h. */
-#undef HAVE_TERMIOS_H
-
-/* Define if you have termio.h. */
-#undef HAVE_TERMIO_H
-
-
-/* Leave that blank line there!!  Autoheader needs it.
-   If you're adding to this file, keep in mind:
-   The entries are in sort -df order: alphabetical, case insensitive,
-   ignoring punctuation (such as underscores).  */

25e7f3e647fb18bf9f0192f35b6a70538b2b5cfd
jwe <unknown>
1996-12-02 02:56:43 +0000

    [project @ 1996-12-02 02:56:01 by jwe]

---
+++ scripts/strings/str2num.m
@@ -0,0 +1,38 @@
+## Copyright (C) 1996 Kurt Hornik
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+## 02111-1307, USA.
+
+## usage:  m = str2num (s)
+##
+## Convert the string S to a number.
+
+## Author: jwe
+
+function m = str2num (s)
+
+  if (nargin == 1 && isstr (s))
+    [nr, nc] = size (s);
+    sep = ";";
+    sep = sep (ones (nr, 1), 1);
+    s = sprintf ("m = [%s];", reshape ([s, sep]', 1, nr * (nc + 1)));
+    eval (s, "m = [];");
+  else
+    usage ("str2num (s)");
+  endif
+
+endfunction

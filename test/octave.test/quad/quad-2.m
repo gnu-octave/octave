59c0358783584fb5973f1d7f31911636c962dfae
jwe <unknown>
1997-02-25 00:19:52 +0000

    [project @ 1997-02-25 00:19:52 by jwe]

--- test/octave.test/quad/quad-2.m
+++ test/octave.test/quad/quad-2.m
@@ -1,6 +1,5 @@
 function y = f (x)
   y = x .* sin (1 ./ x) .* sqrt (abs (1 - x));
 endfunction
-[v, ier, nfun, err] = quad ("f", 0, 5);
-((ier == 0 || ier == 1)
- && v - 1.98194122455795 < sqrt (eps) && nfun > 0 && err < sqrt (eps))
+[v, ier, nfun, err] = quad ("f", 0, 3);
+(ier == 0 || ier == 1) && v - 1.98194122455795 < sqrt (eps) && nfun > 0

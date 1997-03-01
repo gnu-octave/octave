3d24ceac6ef033bced1f313cdda4f0af0813152c
jwe <unknown>
1997-03-01 07:28:23 +0000

    [project @ 1997-03-01 07:28:23 by jwe] strftime-1.m

--- test/octave.test/system/strftime-1.m
+++ test/octave.test/system/strftime-1.m
@@ -1,5 +1,5 @@
 (isstr (strftime ("%%%n%t%H%I%k%l", localtime (time ())))
  && isstr (strftime ("%M%p%r%R%s%S%T", localtime (time ())))
- && isstr (strftime ("%X%Z%a%A%b%B%c", localtime (time ())))
- && isstr (strftime ("%C%d%e%D%h%j%m", localtime (time ())))
- && isstr (strftime ("%U%w%W%x%y%Y", localtime (time ()))))
+ && isstr (strftime ("%X%Z%z%a%A%b%B", localtime (time ())))
+ && isstr (strftime ("%c%C%d%e%D%h%j", localtime (time ())))
+ && isstr (strftime ("%m%U%w%W%x%y%Y", localtime (time ()))))

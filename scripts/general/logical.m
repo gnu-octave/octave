824c7a0e0d62f014450233102fbfeadf501f28f5
jwe <unknown>
1997-03-24 22:50:24 +0000

    [project @ 1997-03-24 22:50:14 by jwe]

---
+++ scripts/general/logical.m
@@ -0,0 +1,40 @@
+## Copyright (C) 1996 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+## 02111-1307, USA.
+
+## usage: logical (arg)
+##
+## Convert ARG to a logical value.  For example,
+##
+##   logical ([-1, 0, 1])
+##
+## is equivalent to
+##
+##   [-1, 0, 1] != 0
+
+## Author: jwe
+
+function y = logical (x)
+
+  if (nargin == 1)
+    y = x != 0;
+  else
+    usage ("logical (x)");
+  endif
+
+endfunction

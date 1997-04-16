777c007c3f6afe26a895017d8bf24e54ee897dac
jwe <unknown>
1997-04-16 21:22:10 +0000

    [project @ 1997-04-16 21:22:05 by jwe]

--- glob/glob.h
+++ glob/glob.h
@@ -52,7 +52,7 @@ extern "C"
 			 GLOB_PERIOD|GLOB_ALTDIRFUNC|GLOB_BRACE|     \
 			 GLOB_NOMAGIC|GLOB_TILDE)
 
-#if !defined (_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 2 || defined (_BSD_SOURCE)
+#if !defined (_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 2 || defined (_GNU_SOURCE)
 #define	GLOB_MAGCHAR	(1 << 8)/* Set in gl_flags if any metachars seen.  */
 #define GLOB_ALTDIRFUNC	(1 << 9)/* Use gl_opendir et al functions.  */
 #define GLOB_BRACE	(1 << 10)/* Expand "{a,b}" to "a" "b".  */

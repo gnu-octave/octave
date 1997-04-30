8b1d1b4a42bac08ca1d7b3a54fb28abe1ce87e0c
jwe <unknown>
1997-04-30 20:38:28 +0000

    [project @ 1997-04-30 20:38:26 by jwe]

---
+++ src/op-fil-rec.cc
@@ -0,0 +1,78 @@
+/*
+
+Copyright (C) 1996, 1997 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if defined (__GNUG__)
+#pragma implementation
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream.h>
+
+#include "mach-info.h"
+
+#include "error.h"
+#include "oct-stream.h"
+#include "ops.h"
+#include "ov.h"
+#include "ov-file.h"
+#include "ov-struct.h"
+#include "ov-typeinfo.h"
+
+// file by struct ops.
+
+DEFBINOP (lshift, file, struct)
+{
+  CAST_BINOP_ARGS (const octave_file&, const octave_struct&);
+
+  octave_stream *oct_stream = v1.stream_value ();
+
+  if (oct_stream)
+    {
+      ostream *osp = oct_stream->output_stream ();
+
+      if (osp)
+	{
+	  ostream& os = *osp;
+
+	  v2.print_raw (os);
+	}
+      else
+	error ("invalid file specified for binary operator `<<'");
+    }
+
+  return octave_value (oct_stream, v1.stream_number ());
+}
+
+void
+install_fil_rec_ops (void)
+{
+  INSTALL_BINOP (lshift, octave_file, octave_struct, lshift);
+}
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; End: ***
+*/

3065fa93a854ac4616c40e94dbf3b5c9dbfc16b2
jwe <unknown>
1997-06-06 22:17:29 +0000

    [project @ 1997-06-06 22:17:29 by jwe]

---
+++ src/version.h
@@ -0,0 +1,48 @@
+/*
+
+Copyright (C) 1996, 1997 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if !defined (octave_version_h)
+#define octave_version_h 1
+
+#define OCTAVE_VERSION "2.1.0"
+
+#define OCTAVE_COPYRIGHT \
+  "Copyright (C) 1996, 1997 John W. Eaton."
+
+#define OCTAVE_NAME_AND_VERSION \
+  "Octave, version " OCTAVE_VERSION " (" TARGET_HOST_TYPE ")"
+
+#define OCTAVE_NAME_VERSION_AND_COPYRIGHT \
+  OCTAVE_NAME_AND_VERSION ".\n" OCTAVE_COPYRIGHT "\n\
+This is free software with ABSOLUTELY NO WARRANTY."
+
+#define OCTAVE_STARTUP_MESSAGE \
+  OCTAVE_NAME_VERSION_AND_COPYRIGHT "\n\
+For details, type `warranty'."
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; End: ***
+*/

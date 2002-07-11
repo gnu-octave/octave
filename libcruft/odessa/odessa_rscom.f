9e67ff1eec7fa04abde8bd2726ebc9eef0ace2ad
jwe <unknown>
2002-07-11 02:37:44 +0000

    [project @ 2002-07-11 02:36:25 by jwe]

---
+++ libcruft/odessa/odessa_rscom.f
@@ -0,0 +1,29 @@
+      SUBROUTINE ODESSA_RSCOM (RSAV, ISAV)
+C-----------------------------------------------------------------------
+C THIS ROUTINE RESTORES FROM RSAV AND ISAV THE CONTENTS OF COMMON BLOCKS
+C ODE001, ODE002 AND EH0001, WHICH ARE USED INTERNALLY IN THE ODESSSA
+C PACKAGE.  THIS PRESUMES THAT RSAV AND ISAV WERE LOADED BY MEANS
+C OF SUBROUTINE ODESSA_SVCOM OR THE EQUIVALENT.
+C-----------------------------------------------------------------------
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      DIMENSION RSAV(*), ISAV(*)
+      COMMON /ODE001/ RODE1(219), IODE1(39)
+      COMMON /ODE002/ RODE2(3), IODE2(11)
+      COMMON /EH0001/ IEH(2)
+      DATA LRODE1/219/, LIODE1/39/, LRODE2/3/, LIODE2/11/
+C
+      DO 10 I = 1,LRODE1
+ 10     RODE1(I) = RSAV(I)
+      DO 20 I = 1,LRODE2
+        J = LRODE1 + I
+ 20     RODE2(I) = RSAV(J)
+      DO 30 I = 1,LIODE1
+ 30     IODE1(I) = ISAV(I)
+      DO 40 I = 1,LODE2
+        J = LIODE1 + I
+ 40     IODE2(I) = ISAV(J)
+      IEH(1) = ISAV(LIODE1+LIODE2+1)
+      IEH(2) = ISAV(LIODE1+LIODE2+2)
+      RETURN
+C----------------------- END OF SUBROUTINE ODESSA_RSCOM -----------------------
+      END
---
+++ libcruft/odessa/odessa_svcom.f
@@ -0,0 +1,30 @@
+      SUBROUTINE ODESSA_SVCOM (RSAV, ISAV)
+C-----------------------------------------------------------------------
+C THIS ROUTINE STORES IN RSAV AND ISAV THE CONTENTS OF COMMON BLOCKS
+C ODE001, ODE002 AND EH0001, WHICH ARE USED INTERNALLY IN THE ODESSA
+C PACKAGE.
+C RSAV = REAL ARRAY OF LENGTH 222 OR MORE.
+C ISAV = INTEGER ARRAY OF LENGTH 52 OR MORE.
+C-----------------------------------------------------------------------
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      DIMENSION RSAV(*), ISAV(*)
+      COMMON /ODE001/ RODE1(219), IODE1(39)
+      COMMON /ODE002/ RODE2(3), IODE2(11)
+      COMMON /EH0001/ IEH(2)
+      DATA LRODE1/219/, LIODE1/39/, LRODE2/3/, LIODE2/11/
+C
+      DO 10 I = 1,LRODE1
+ 10     RSAV(I) = RODE1(I)
+      DO 20 I = 1,LRODE2
+        J = LRODE1 + I
+ 20     RSAV(J) = RODE2(I)
+      DO 30 I = 1,LIODE1
+ 30     ISAV(I) = IODE1(I)
+      DO 40 I = 1,LIODE2
+        J = LIODE1 + I
+ 40     ISAV(J) = IODE2(I)
+      ISAV(LIODE1+LIODE2+1) = IEH(1)
+      ISAV(LIODE1+LIODE2+2) = IEH(2)
+      RETURN
+C----------------------- END OF SUBROUTINE ODESSA_SVCOM -----------------------
+      END

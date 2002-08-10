a8a5e37304e363bd73a668ead9c4856177d773e8
jwe <unknown>
2002-08-10 06:14:28 +0000

    [project @ 2002-08-10 06:13:57 by jwe]

--- test/octave.test/arith/betainc-1.m
+++ test/octave.test/arith/betainc-1.m
@@ -1,8 +1,8 @@
 a=[1, 1.5, 2, 3];
 b=[4, 3, 2, 1];
-v1=betainc(a,b,1);
+v1=betainc(1,a,b);
 v2=[1,1,1,1];
 x = [.2, .4, .6, .8];
-v3=betainc(a, b, x);
-v4 = 1-betainc(b, a, 1.-x);
+v3=betainc(x, a, b);
+v4 = 1-betainc(1.-x, b, a);
 all(abs(v1-v2)<sqrt(eps)) && all(abs(v3-v4)<sqrt(eps))
--- test/octave.test/arith/gammainc-1.m
+++ test/octave.test/arith/gammainc-1.m
@@ -2,4 +2,4 @@ a = [.5 .5 .5 .5 .5];
 x = [0 1 2 3 4];
 v1 = sqrt(pi)*erf(x)./gamma(a);
 v3 = gammainc(x.*x,a);
-all (abs(v1 - v2) < sqrt(eps))
+all (abs(v1 - v3) < sqrt(eps))

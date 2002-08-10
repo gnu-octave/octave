94f9608401b839eace584facbd9bf4c09a7a772d
jwe <unknown>
2002-08-10 06:04:40 +0000

    [project @ 2002-08-10 06:02:12 by jwe]

--- test/octave.test/arith/gammainc-1.m
+++ test/octave.test/arith/gammainc-1.m
@@ -1,6 +1,5 @@
 a = [.5 .5 .5 .5 .5];
 x = [0 1 2 3 4];
 v1 = sqrt(pi)*erf(x)./gamma(a);
-v2 = gammai(a,x.*x);
 v3 = gammainc(x.*x,a);
-all (abs(v1 - v2) < sqrt(eps)) && all (abs(v2 - v3) < sqrt(eps))
+all (abs(v1 - v2) < sqrt(eps))

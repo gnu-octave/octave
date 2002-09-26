a37c6e0fd1045d1dcba4032f39a278d7d295d623
jwe <unknown>
2002-09-26 22:48:24 +0000

    [project @ 2002-09-26 22:48:24 by jwe]

--- scripts/statistics/base/var.m
+++ scripts/statistics/base/var.m
@@ -40,9 +40,9 @@ function y = var(x)
     y = 0;
   elseif ((nr == 1) || (nc == 1))
     n = length (x);
-    y = (sumsq (x) - sum(x)^2 / n) / (n - 1);
+    y = (sumsq (x) - abs(sum(x))^2 / n) / (n - 1);
   else
-    y = (sumsq (x) - sum(x).^2 / nr) / (nr - 1);
+    y = (sumsq (x) - abs(sum(x)).^2 / nr) / (nr - 1);
   endif
 
 endfunction

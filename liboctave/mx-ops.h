2adcc9f1403260c715b985757e8f11ea75d9adb5
jwe <unknown>
2002-12-04 00:33:34 +0000

    [project @ 2002-12-04 00:33:34 by jwe]

---
+++ liboctave/mx-ops.h
@@ -0,0 +1,76 @@
+/*
+
+Copyright (C) 2002 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if !defined (octave_mx_base_h)
+#define octave_mx_base_h 1
+
+// Declarations for operators that work on matrix objects.
+
+// ComplexDiagMatrix by X ops.
+
+mx-cdm-cm.h
+mx-cdm-cs.h
+mx-cdm-dm.h
+mx-cdm-m.h
+mx-cdm-s.h
+
+// ComplexMatrix by X ops.
+
+mx-cm-cdm.h
+mx-cm-dm.h
+mx-cm-m.h
+mx-cm-s.h
+
+// Complex scalar by X ops.
+
+mx-cs-cdm.h
+mx-cs-dm.h
+mx-cs-m.h
+
+// DiagMatrix by X ops.
+
+mx-dm-cdm.h
+mx-dm-cm.h
+mx-dm-cs.h
+mx-dm-m.h
+mx-dm-s.h
+
+// Matrix by X ops.
+
+mx-m-cdm.h
+mx-m-cm.h
+mx-m-cs.h
+mx-m-dm.h
+
+// Real scalar by X ops.
+
+mx-s-cdm.h
+mx-s-cm.h
+mx-s-dm.h
+
+#endif
+
+/*
+;;; Local Variables: ***
+;;; mode: C++ ***
+;;; End: ***
+*/

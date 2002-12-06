8f88aa84f378095e9a59a8aed194ca845a64144b
jwe <unknown>
2002-12-06 22:34:47 +0000

    [project @ 2002-12-06 22:34:47 by jwe]

--- src/toplev.cc
+++ src/toplev.cc
@@ -27,6 +27,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cassert>
 #include <cstdlib>
 #include <cstring>
+#include <new>
 
 #include <fstream>
 #include <iostream>

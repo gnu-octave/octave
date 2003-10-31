e832062c7b93e6895b8fef66efc9161a413969ff
jwe <unknown>
2003-10-31 14:11:47 +0000

    [project @ 2003-10-31 14:11:47 by jwe]

--- liboctave/file-ops.cc
+++ liboctave/file-ops.cc
@@ -30,6 +30,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cstring>
 
 #include <iostream>
+#include <memory>
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>

9b1625bc3a4d48c5aa4486bf17625be8f629e3e1
jwe <unknown>
2004-03-09 16:38:19 +0000

    [project @ 2004-03-09 16:38:19 by jwe]

--- src/syscalls.cc
+++ src/syscalls.cc
@@ -586,7 +586,7 @@ See stat.\n\
 
 DEFUN (mkfifo, args, ,
   "-*- texinfo -*-\n\
-@deftypefn {Built-in Function} {[@var{err}, @var{msg}] =} mkfifo (@var{name})\n\
+@deftypefn {Built-in Function} {[@var{err}, @var{msg}] =} mkfifo (@var{name}, @var{mode})\n\
 Create a @var{fifo} special file named @var{name} with file mode @var{mode}\n\\n\
 \n\
 If successful, @var{err} is 0 and @var{msg} is an empty string.\n\

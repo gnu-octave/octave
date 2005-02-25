60d2766d3760b0af11e0018cc3eba767d7b8a6ca
jwe <unknown>
2005-02-25 17:42:55 +0000

    [project @ 2005-02-25 17:42:55 by jwe]

--- src/version.h
+++ src/version.h
@@ -23,12 +23,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #if !defined (octave_version_h)
 #define octave_version_h 1
 
-#define OCTAVE_VERSION "2.1.64-cvs"
+#define OCTAVE_VERSION "2.1.cvs"
 
-#define OCTAVE_API_VERSION "api-v12-cvs"
+#define OCTAVE_API_VERSION "api-cvs"
 
 #define OCTAVE_COPYRIGHT \
-  "Copyright (C) 2004 John W. Eaton."
+  "Copyright (C) 2005 John W. Eaton."
 
 #define OCTAVE_NAME_AND_VERSION \
   "GNU Octave, version " OCTAVE_VERSION " (" OCTAVE_CANONICAL_HOST_TYPE ")"

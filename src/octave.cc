7c4eeaf4ad5fec97646a102ff88ba2c223f8d1c0
jwe <unknown>
2005-03-04 02:51:33 +0000

    [project @ 2005-03-04 02:51:33 by jwe]

--- src/octave.cc
+++ src/octave.cc
@@ -367,8 +367,8 @@ Options:\n\
 \n\
   --debug, -d             Enter parser debugging mode.\n\
   --echo-commands, -x     Echo commands as they are executed.\n\
-  --exec-path PATH        Set path for executing subprograms.\n\
   --eval CODE             Evaluate CODE and exit when done unless --persist.\n\
+  --exec-path PATH        Set path for executing subprograms.\n\
   --help, -h, -?          Print short help message and exit.\n\
   --info-file FILE        Use top-level info file FILE.\n\
   --info-program PROGRAM  Use PROGRAM for reading info files.\n\
@@ -385,7 +385,8 @@ Options:\n\
   --verbose, -V           Enable verbose output in some cases.\n\
   --version, -v           Print version number and exit.\n\
 \n\
-  FILE                    Execute commands from FILE.\n\
+  FILE                    Execute commands from FILE.  Exit when done\n\
+                          unless --persist is also specified\n\.
 \n"
 OCTAVE_WWW_STATEMENT "\n\
 \n"

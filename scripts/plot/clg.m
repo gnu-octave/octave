035ee28579a55c01c7e8c962cd45bfccba3f9a14
jwe <unknown>
2005-03-04 05:26:12 +0000

    [project @ 2005-03-04 05:25:35 by jwe]

---
+++ scripts/plot/clg.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2005 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+## 02111-1307, USA.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} clg
+## Clear the plot window and any titles or axis labels.  This function
+## is equivalent to @code{clearplot}.
+## @end deftypefn
+
+function clg ()
+
+  clearplot ();
+
+endfunction

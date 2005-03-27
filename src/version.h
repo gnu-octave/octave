31cba2a520f1a26c91680675e71b3d4315939fa5
jwe <unknown>
2005-03-27 12:06:59 +0000

    [project @ 2005-03-27 12:06:59 by jwe]

--- src/version.h
+++ src/version.h
@@ -23,7 +23,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #if !defined (octave_version_h)
 #define octave_version_h 1
 
-#define OCTAVE_VERSION "2.9.0"
+#define OCTAVE_VERSION "2.9.1"
 
 #define OCTAVE_API_VERSION "api-v14"
 

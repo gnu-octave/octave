326cd33e6e8fdad086bda32cbd4bab1db2f02097
jwe <unknown>
2005-06-02 16:01:25 +0000

    [project @ 2005-06-02 16:01:25 by jwe]

--- scripts/plot/meshgrid.m
+++ scripts/plot/meshgrid.m
@@ -18,11 +18,16 @@
 ## 02110-1301, USA.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{xx}, @var{yy}] =} meshgrid (@var{x}, @var{y})
+## @deftypefn {Function File} {[@var{xx}, @var{yy}, @var{zz}] =} meshgrid (@var{x}, @var{y}, @var{z})
+## @deftypefnx {Function File} {[@var{xx}, @var{yy}] =} meshgrid (@var{x}, @var{y})
 ## @deftypefnx {Function File} {[@var{xx}, @var{yy}] =} meshgrid (@var{x})
-## Given vectors of @var{x} and @var{y} coordinates, return two matrices corresponding
-## to the @var{x} and @var{y} coordinates of a mesh.  The rows of @var{xx} are copies of @var{x},
-## and the columns of @var{yy} are copies of @var{y}.
+## Given vectors of @var{x}, @var{y}, and @var{z} coordinates, return
+## three dimensional arrays corresponding to the @var{x}, @var{y}, and
+## @var{z} coordinates of a mesh.  Given only @var{x} and @var{y},
+## return matrices corresponding to the @var{x} and @var{y} coordinates
+## of a mesh.  The rows of @var{xx} are copies of @var{x}, and the
+## columns of @var{yy} are copies of @var{y}.  If @var{y} is omitted,
+## then it is assumed to be the same as @var{x}.
 ## @end deftypefn
 ##
 ## @seealso{sombrero, plot, semilogx, semilogy, loglog, polar, mesh,
@@ -30,7 +35,7 @@
 
 ## Author: jwe
 
-function [xx, yy] = meshgrid (x, y)
+function [xx, yy, zz] = meshgrid (x, y, z)
 
   if (nargin == 1)
     y = x;
@@ -42,8 +47,19 @@ function [xx, yy] = meshgrid (x, y)
     else
       error ("meshgrid: arguments must be vectors");
     endif
+  elseif (nargin == 3)
+   if (isvector (x) && isvector (y) && isvector (z))
+      lenx = length (x);
+      leny = length (y);
+      lenz = length (z);
+      xx = repmat (ones (leny, 1) * x(:).', [1, 1, lenz]);
+      yy = repmat (y(:) * ones (1, lenx), [1, 1, lenz]);
+      zz = reshape (repmat (z(:).', lenx*leny, 1)(:), leny, lenx, lenz);
+   else
+      error ("meshgrid: arguments must be vectors");
+   endif
   else
-    usage ("[xx, yy] = meshgrid (x, y)");
+    usage ("[xx, yy, zz] = meshgrid (x, y, z)");
   endif
 
 endfunction

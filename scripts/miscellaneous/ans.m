0c6654673c96fb523f7c354bd6cb2e902baedbeb
jwe <unknown>
2006-05-09 17:31:28 +0000

    [project @ 2006-05-09 17:30:57 by jwe]

---
+++ scripts/miscellaneous/ans.m
@@ -0,0 +1,17 @@
+## -*- texinfo -*-
+## @defvr {Automatic Variable} ans
+## The the most recently computed result that was not
+## explicitly assigned to a variable.  For example, after the expression
+## 
+## @example
+## 3^2 + 4^2
+## @end example
+## 
+## @noindent
+## is evaluated, the value returned by @code{ans} is 25.
+## @end defvr
+
+## FIXME -- we should be able to get formatted help for empty scripts,
+## not just functions.
+function ans ()
+endfunction

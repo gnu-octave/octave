b6c49ae915b902bde0eb945375a0f86e7287a0a3
jwe <unknown>
2006-08-22 05:37:06 +0000

    [project @ 2006-08-22 05:37:06 by jwe]

--- test/test_io.m
+++ test/test_io.m
@@ -60,7 +60,7 @@
 %!
 %!  ret = 0;
 %!
-%!  files = {"ascii.mat", "binary.mat", "mat5.mat", "mat7.mat"};
+%!  files = {"text.mat", "binary.mat", "mat5.mat", "mat7.mat"};
 %!  opts = {"-z -text", "-z -binary", "-z -mat", "-v7"};
 %!
 %!  vars = "a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20";

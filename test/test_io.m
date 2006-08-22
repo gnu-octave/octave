7602d268d78ea9528d765e4f7293688aee1b65a3
jwe <unknown>
2006-08-22 04:42:36 +0000

    [project @ 2006-08-22 04:42:36 by jwe]

--- test/test_io.m
+++ test/test_io.m
@@ -66,7 +66,7 @@
 %!  vars = "a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20";
 %!  if (! input)
 %!    for i = 1:length(files)
-%!      eval (["save " opts{i} " " files{i} " " vars]);
+%!      eval (sprintf ("save %s %s %s", opts{i}, files{i}, vars));
 %!    endfor
 %!  else
 %!    b1 = a1; b2 = a2; b3 = a3; b4 = a4; b5 = a5;

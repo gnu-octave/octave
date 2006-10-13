bb18f7aaa36f42744d09e10b6c4bdd90456258e4
jwe <unknown>
2006-10-13 20:50:08 +0000

    [project @ 2006-10-13 20:50:08 by jwe]

---
+++ scripts/plot/box.m
@@ -0,0 +1,37 @@
+## Copyright (C) 2006 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+## 02110-1301, USA.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} box (@var{arg})
+## @deftypefnx {Function File} {} grid (@var{h}, @dots{})
+## Control the display of a border around the plot.
+## The argument may be either @code{"on"} or @code{"off"}.  If it is
+## omitted, the the current box state is toggled.
+##
+## Note, this function currently has no effect.
+## @seealso{grid}
+## @end deftypefn
+
+## Author: jwe
+
+## PKG_ADD: mark_as_command box
+
+function box ()
+
+endfunction

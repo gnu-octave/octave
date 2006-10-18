475cce13282b9d4260668ea277c801e04cde8c81
jwe <unknown>
2006-10-18 02:21:22 +0000

    [project @ 2006-10-18 02:21:22 by jwe]

--- src/oct-map.h
+++ src/oct-map.h
@@ -24,6 +24,7 @@ Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #if !defined (octave_oct_map_h)
 #define octave_oct_map_h 1
 
+#include <algorithm>
 #include <map>
 
 #include "Cell.h"

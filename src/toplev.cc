3ed76d37a77dac2dc741078a85de616a3890d975
jwe <unknown>
2006-10-28 16:30:52 +0000

    [project @ 2006-10-28 16:30:52 by jwe]

--- src/toplev.cc
+++ src/toplev.cc
@@ -674,16 +674,12 @@ will print a message when Octave exits.\n\
 
   if (nargin == 1)
     {
-#if defined (HAVE_ATEXIT) || defined (HAVE_ON_EXIT)
       std::string arg = args(0).string_value ();
 
       if (! error_state)
 	octave_atexit_functions.push (arg);
       else
 	error ("atexit: argument must be a string");
-#else
-      gripe_not_supported ("atexit");
-#endif
     }
   else
     print_usage ();

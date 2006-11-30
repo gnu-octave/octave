4960693bd2aaf4bd185608c7cd4b461430527a4e
jwe <unknown>
2006-11-30 22:57:15 +0000

    [project @ 2006-11-30 22:57:14 by jwe]

---
+++ scripts/miscellaneous/mexext.m
@@ -0,0 +1,27 @@
+## Copyright (C) 2006 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+## 02110-1301, USA.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} mexext ()
+## Return the filename extension used for MEX files.
+## @end deftypefn
+
+function retval = mexext ()
+  retval = "mex";
+endfunction

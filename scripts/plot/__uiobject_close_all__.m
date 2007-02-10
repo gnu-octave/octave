f414d3020ddc1de3c8d1c4f2bb84f1ebb10ca243
jwe <unknown>
2007-02-10 20:00:56 +0000

    [project @ 2007-02-10 20:00:56 by jwe]

---
+++ scripts/plot/__uiobject_close_all__.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2007 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+## 02110-1301, USA.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} __uiobject_close_all__ ()
+## Internal function to close all figures.
+## @seealso{drawnow}
+## @end deftypefn
+
+## Author: jwe
+
+function __uiobject_close_all__ ()
+  fputs (stderr, "closing all figures\n");
+  close ("all");
+endfunction

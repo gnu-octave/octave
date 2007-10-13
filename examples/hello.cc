44dc7d36ad141b1cbd1822228e2daa0691d2f137
jwe <unknown>
2007-10-13 01:42:21 +0000

    [project @ 2007-10-13 01:42:20 by jwe]

--- examples/hello.cc
+++ examples/hello.cc
@@ -1,3 +1,25 @@
+/*
+
+Copyright (C) 1996, 1997, 1999, 2002, 2003, 2007 John W. Eaton
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+Octave is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
 // hello.cc -- example of a dynamically linked function for Octave.
 
 // To use this file, your version of Octave must support dynamic

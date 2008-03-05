58fe0e735ccd11c5882efe0ffdb3d14e3127465b
Unknown <bill@denney.ws>
2008-03-04 22:58:05 -0500

    allchild.m, findall.m: new functions

---
+++ scripts/plot/allchild.m
@@ -0,0 +1,50 @@
+## Copyright (C) 2008 Bill Denney
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{h} =} allchild (@var{handles})
+## Find all children including hidden children of an object.
+##
+## This function is similar to @code{get (h, "children")}, but also
+## returns includes hidden objects.  If @var{handles} is a scalar,
+## @var{h} will be a vector, and if not, @var{h} will be a cell matrix
+## with the same size as @var{handles} and each cell will contain a
+## vector of handles.
+## @seealso{get, set, findall, findobj}
+## @end deftypefn
+
+## Author: Bill Denney <bill@denney.ws>
+
+function h = allchild (ha)
+
+  unwind_protect
+    shh = get (0, "showhiddenhandles");
+    set (0, "showhiddenhandles", "on");
+    if (isscalar (ha))
+      h = get (ha, "children");
+    else
+      h = cell (size (ha));
+      for i = 1:numel (ha)
+        h{i} = get (ha, "children");
+      endfor
+    endif
+  unwind_protect_cleanup
+    set (0, "showhiddenhandles", shh);
+  end_unwind_protect
+
+endfunction
---
+++ scripts/plot/findall.m
@@ -0,0 +1,44 @@
+## Copyright (C) 2008 Bill Denney
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{h} =} findall ()
+## @deftypefnx {Function File} {@var{h} =} findall (@var{propName}, @var{propValue})
+## @deftypefnx {Function File} {@var{h} =} findall (@var{h}, @dots{})
+## @deftypefnx {Function File} {@var{h} =} findall (@var{h}, "-depth", @var{d}, @dots{})
+## Find object with specified property values including hidden handles.
+##
+## This function performs the same function as @code{findobj}, but it
+## includes hidden objects in its search.  For full documentation, see
+## @code{findobj}.
+## @seealso{get, set, findobj, allchild}
+## @end deftypefn
+
+## Author: Bill Denney <bill@denney.ws>
+
+function h = findall (varargin)
+
+  unwind_protect
+    shh = get (0, "showhiddenhandles");
+    set (0, "showhiddenhandles", "on");
+    h = findobj (varargin{:});
+  unwind_protect_cleanup
+    set (0, "showhiddenhandles", shh);
+  end_unwind_protect
+
+endfunction

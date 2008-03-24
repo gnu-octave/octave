eb6c6ee389424bf67418512e9d858eabb9a72b71
David Bateman <dbateman@free.fr>
2008-03-24 15:54:50 -0400

    Add the namelengthmax function

---
+++ scripts/miscellaneous/namelengthmax.m
@@ -0,0 +1,39 @@
+## Copyright (C) 2008  David Bateman
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} namelengthmax ()
+## Returns the matlab compatible maximum variable name length. Octave is
+## capable of storing strings up to 
+## @iftex
+## @tex
+## $2^{31} - 1$
+## @end tex
+## @end iftext
+## @ifnottex
+## @code{2 ^ 31 - 1}
+## @end ifnottex
+## in length. However for matlab compatibility all variable, function
+## and structure field names should be shorter than the length supplied by
+## @code{namelengthmax}. In particular variables stored to a matlab file
+## format will have their names truncated to this length.
+## @end deftypefn
+
+function n = namelengthmax ()
+  n = 63;
+endfunction

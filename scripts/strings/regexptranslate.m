1b1661524fb2bd4aa1b7751e53924544d5e50f93
David Bateman <dbateman@free.fr>
2008-03-24 15:55:40 -0400

    Add the regexptranslate function

---
+++ scripts/strings/regexptranslate.m
@@ -0,0 +1,48 @@
+## Copyright (C) 2008  David Bateman
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} realsqrt (@var{x})
+## Return the real natural logarithm of @var{x}. If any element results in the
+## return value being complex @code{reallog} produces an error.
+## @seealso{log, realsqrt, realpow}
+## @end deftypefn
+
+function y = regexptranslate (op, x)
+  
+  if (ischar (op))
+    op = tolower (op);
+    if (strcmp ("wildcard", op))
+      y = regexprep (regexprep (regexprep (x, "\\.", "\\."), "\\*",
+				".*"), "\\?", ".");
+    elseif (strcmp ("escape", op))
+      ch = {'\$', '\.', '\?', '\[', '\]'};
+      y = x;
+      for i = 1 : length (ch)
+	y = regexprep (y, ch{i}, ch{i});
+      endfor
+    else
+      error ("regexptranslate: unexpected operation");
+    endif
+  else
+    error ("regexptranslate: expecting operation to be a string");
+  endif
+endfunction
+
+%!assert (regexptranslate ("wildcard", "/a*b?c."), "/a.*b.c\\.")
+%!assert (regexptranslate ("escape", '$.?[]'), '\$\.\?\[\]')

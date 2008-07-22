ae040954fe539cdad3216f15045624ef51a7f528
John W. Eaton <jwe@octave.org>
2008-07-22 11:40:48 -0400

    3.1.51 snapshot

--- src/version.h
+++ src/version.h
@@ -24,11 +24,11 @@ along with Octave; see the file COPYING.  If not, see
 #if !defined (octave_version_h)
 #define octave_version_h 1
 
-#define OCTAVE_VERSION "3.1.50+"
+#define OCTAVE_VERSION "3.1.51"
 
 #define OCTAVE_API_VERSION "api-v33+"
 
-#define OCTAVE_RELEASE_DATE "2008-07-18"
+#define OCTAVE_RELEASE_DATE "2008-07-21"
 
 #define OCTAVE_COPYRIGHT "Copyright (C) 2008 John W. Eaton and others."
 

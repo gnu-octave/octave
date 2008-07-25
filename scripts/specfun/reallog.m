09a8b501085a023111eef17890812637f8f9813e
Ben Abbott <bpabbott@mac.com>
2008-07-24 20:16:11 -0400

    Fix help string.

--- scripts/specfun/reallog.m
+++ scripts/specfun/reallog.m
@@ -17,7 +17,7 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {} realsqrt (@var{x})
+## @deftypefn {Function File} {} reallog (@var{x})
 ## Return the real natural logarithm of @var{x}. If any element results in the
 ## return value being complex @code{reallog} produces an error.
 ## @seealso{log, realsqrt, realpow}

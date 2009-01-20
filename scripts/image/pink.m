22187472d956e2bc9089e28062c89f00ce018bdc
John W. Eaton <jwe@octave.org>
2009-01-20 11:02:56 -0500

    pink.m: spelling fix

--- scripts/image/pink.m
+++ scripts/image/pink.m
@@ -18,8 +18,8 @@
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {} pink (@var{n})
-## Create color colormap. This colormap gives a sephia tone on black and
-## white images. The argument @var{n} should be a scalar.  If it
+## Create color colormap.  This colormap gives a sepia tone on black and
+## white images.  The argument @var{n} should be a scalar.  If it
 ## is omitted, the length of the current colormap or 64 is assumed.
 ## @seealso{colormap}
 ## @end deftypefn

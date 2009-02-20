4314d5419dbdeec4c2290201cc476e4f5e72f2ea
John W. Eaton <jwe@octave.org>
2009-02-20 01:23:04 -0500

    ov-base-mat.cc: correctly compute all-scalar index for assignment

--- src/ov-base-mat.cc
+++ src/ov-base-mat.cc
@@ -356,7 +356,7 @@ octave_base_matrix<MT>::assign (const octave_value_list& idx,
                 octave_idx_type k = 1, j = 0;
                 for (octave_idx_type i = 0; i < n_idx; i++)
                   {
-                    j += i * k;
+                    j += idx_vec(i)(0) * k;
                     k *= dv (i);
                   }
                 matrix(j) = rhs;

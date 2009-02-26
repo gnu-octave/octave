f4f3ace392ea408d2f1d63d5774ccf24ed63f232
John W. Eaton <jwe@octave.org>
2009-02-26 03:53:37 -0500

    append + to version numbers

--- src/version.h
+++ src/version.h
@@ -24,9 +24,9 @@ along with Octave; see the file COPYING.  If not, see
 #if !defined (octave_version_h)
 #define octave_version_h 1
 
-#define OCTAVE_VERSION "3.1.53"
+#define OCTAVE_VERSION "3.1.53+"
 
-#define OCTAVE_API_VERSION "api-v35"
+#define OCTAVE_API_VERSION "api-v35+"
 
 #define OCTAVE_RELEASE_DATE "2009-02-25"
 

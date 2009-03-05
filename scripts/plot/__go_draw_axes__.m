e0bb9c22725b02f93414c486f606923d5ca3cd32
Ben Abbott <bpabbott@mac.com>
2009-03-05 00:55:56 -0500

    __go_draw_axes__.m: Preserve column vector orientation for the axes children while removing the axis label & title handles.

--- scripts/plot/__go_draw_axes__.m
+++ scripts/plot/__go_draw_axes__.m
@@ -294,8 +294,8 @@ function __go_draw_axes__ (h, plot_stream, enhanced, mono, implicit_margin)
     fputs (plot_stream, "set clip two;\n");
 
     kids = axis_obj.children;
-    kids = setdiff (kids, [axis_obj.xlabel, axis_obj.ylabel, ...
-                           axis_obj.zlabel, axis_obj.title]);
+    kids = setdiff (kids, [axis_obj.xlabel; axis_obj.ylabel; ...
+                           axis_obj.zlabel; axis_obj.title]);
 
     if (nd == 3)
       fputs (plot_stream, "set parametric;\n");

aa1cfb528b2e0a8c4eec7ea7a5b7dc6fa4651e63
Ben Abbott <bpabbott@mac.com>
2009-04-30 18:45:42 -0400

    __gnuplot_has_feature__.m: Change version for "x11_figure_position", >4.2.4 to >=4.3.0.

--- scripts/plot/__gnuplot_has_feature__.m
+++ scripts/plot/__gnuplot_has_feature__.m
@@ -34,8 +34,8 @@ function res = __gnuplot_has_feature__ (feature)
 
   if (isempty (has_features))
     gnuplot_version = __gnuplot_version__ ();
-    versions = {"4.2.4", "4.3", "4.3", "4.2", "4.2", "4.3", "4.3"};
-    operators = {">", ">=", ">=", ">=", ">=", ">=", ">="};
+    versions = {"4.3", "4.3", "4.3", "4.2", "4.2", "4.3", "4.3"};
+    operators = {">=", ">=", ">=", ">=", ">=", ">=", ">="};
     have_features = logical (zeros (size (features)));
     for n = 1 : numel (have_features)
       has_features(n) = compare_versions (gnuplot_version, versions{n}, operators{n});

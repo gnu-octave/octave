d1490d2c37e8a941cde18ede2531cc241b5352ca
Ben Abbott <bpabbott@mac.com>
2009-06-16 06:43:56 +0200

    quiver.m: Add 'clf' to demos.

--- scripts/plot/quiver.m
+++ scripts/plot/quiver.m
@@ -82,6 +82,7 @@ function retval = quiver (varargin)
 endfunction
 
 %!demo
+%! clf
 %! [x,y] = meshgrid(1:2:20);
 %! h = quiver(x,y,sin(2*pi*x/10),sin(2*pi*y/10));
 %! set (h, "maxheadsize", 0.33);

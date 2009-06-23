60f414221041ee46f3bfeb65436beddf072caa88
Robert T. Short <octave@phaselockedsystems.com>
2009-06-23 14:13:47 -0400

    * run-octave.in: Exclude @-files from path.  Remove CVS exclusions.   * test: Add class directories for testing.   * ov-class.cc: Add tests.

---
+++ test/@Gork/subsasgn.m
@@ -0,0 +1,9 @@
+function g = subsasgn(g, s, x)
+
+  switch s.type
+  case '.'
+    switch s.subs
+    case 'gyrk'
+      g.gyrk = x; 
+  end
+end
---
+++ test/@Gork/subsref.m
@@ -0,0 +1,9 @@
+function x = subsref(g, s)
+
+  switch s.type
+  case '.'
+    switch s.subs
+    case 'gyrk'
+      x = g.gyrk;
+  end
+end

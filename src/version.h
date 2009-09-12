d95ba69a2050861897e3b9695e20e7a022540ce6
John W. Eaton <jwe@octave.org>
2009-09-12 10:15:38 -0400

    append + to version and api after snapshot

--- src/version.h
+++ src/version.h
@@ -25,9 +25,9 @@ along with Octave; see the file COPYING.  If not, see
 #if !defined (octave_version_h)
 #define octave_version_h 1
 
-#define OCTAVE_VERSION "3.3.50"
+#define OCTAVE_VERSION "3.3.50+"
 
-#define OCTAVE_API_VERSION "api-v38"
+#define OCTAVE_API_VERSION "api-v38+"
 
 #define OCTAVE_RELEASE_DATE "2009-09-12n"
 

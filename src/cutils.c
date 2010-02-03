8546e313bf7de71e33f3176247e15c1bb5ecd804
John W. Eaton <jwe@octave.org>
2010-02-03 05:18:25 -0500

    liboctave/cutils.c (octave_usleep): implement with nanosleep

--- src/cutils.c
+++ src/cutils.c
@@ -24,87 +24,31 @@ along with Octave; see the file COPYING.  If not, see
 #include <config.h>
 #endif
 
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
-#if defined (__WIN32__) && ! defined (_POSIX_VERSION)
-
-#include <windows.h>
-
-#else
-
-#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#ifdef HAVE_POLL_H
-#include <poll.h>
-#elif HAVE_SYS_POLL_H
-#include <sys/poll.h>
-#endif
-
-#endif
 
 void
 octave_sleep (unsigned int seconds)
 {
-#if defined (__WIN32__) && ! defined (_POSIX_VERSION)
-  Sleep (1000 * seconds);
-#else
   sleep (seconds);
-#endif
 }
 
 void
 octave_usleep (unsigned int useconds)
 {
+  struct timespec delay;
+  struct timespec remaining;
+
   unsigned int sec = useconds / 1000000;
   unsigned int usec = useconds % 1000000;
 
-  if (sec > 0)
-    octave_sleep (sec);
+  delay.tv_sec = sec;
+  delay.tv_nsec = usec * 1000;
 
-#if defined (__WIN32__) && ! defined (_POSIX_VERSION)
-
-  /* Round to the nearest millisecond, with a minimum of 1 millisecond
-     if usleep was called with a a non-zero value.  */
-
-  if (usec > 500)
-    Sleep ((usec+500)/1000);
-  else if (usec > 0)
-    Sleep (1);
-  else
-    Sleep (0);
-
-#elif defined (HAVE_USLEEP)
-
-  usleep (usec);
-
-#elif defined (HAVE_SELECT)
-
-  {
-    struct timeval delay;
-
-    delay.tv_sec = 0;
-    delay.tv_usec = usec;
-
-    select (0, 0, 0, 0, &delay);
-  }
-
-#elif defined (HAVE_POLL)
-
-  {
-    struct pollfd pfd;
-
-    int delay = usec / 1000;
-
-    if (delay > 0)
-      poll (&pfd, 0, delay);
-  }
-
-#endif
+  nanosleep (&delay, &remaining);
 }
 
 int

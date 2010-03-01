09337250a296b01f8466c1d54317b95ea88240bb
John W. Eaton <jwe@octave.org>
2010-02-28 23:20:53 -0500

    Makefile.am rules for rebuilding .mk files

--- liboctave/config-ops.sh
+++ liboctave/config-ops.sh
@@ -4,32 +4,43 @@ set -e
 
 : ${AWK=awk}
 
-VX_INC=$($AWK -f mk-ops.awk prefix=vx list_h_files=1 vx-ops)
+if [ $# -eq 1 ]; then
+  top_srcdir="$1";
+else
+  top_srcdir=".."
+fi
 
-MX_INC=$($AWK -f mk-ops.awk prefix=mx list_h_files=1 mx-ops)
+liboctave_dir="$top_srcdir/liboctave"
 
-SMX_INC=$($AWK -f sparse-mk-ops.awk prefix=smx list_h_files=1 sparse-mx-ops)
+mk_ops="$liboctave_dir/mk-ops.awk"
+sparse_mk_ops="$liboctave_dir/sparse-mk-ops.awk"
 
-VX_SRC=$($AWK -f mk-ops.awk prefix=vx list_cc_files=1 vx-ops)
+VX_INC=$($AWK -f $mk_ops prefix=vx list_h_files=1 $liboctave_dir/vx-ops)
 
-MX_SRC=$($AWK -f mk-ops.awk prefix=mx list_cc_files=1 mx-ops)
+MX_INC=$($AWK -f $mk_ops prefix=mx list_h_files=1 $liboctave_dir/mx-ops)
 
-SMX_SRC=$($AWK -f sparse-mk-ops.awk prefix=smx list_cc_files=1 sparse-mx-ops)
+SMX_INC=$($AWK -f $sparse_mk_ops prefix=smx list_h_files=1 $liboctave_dir/sparse-mx-ops)
 
-echo "VX_OP_INC = $(echo $VX_INC)" > vx-op-inc.mk-t
-../move-if-change vx-op-inc.mk-t vx-op-inc.mk
+VX_SRC=$($AWK -f $mk_ops prefix=vx list_cc_files=1 $liboctave_dir/vx-ops)
 
-echo "MX_OP_INC = $(echo $MX_INC)" > mx-op-inc.mk-t
-../move-if-change mx-op-inc.mk-t mx-op-inc.mk
+MX_SRC=$($AWK -f $mk_ops prefix=mx list_cc_files=1 $liboctave_dir/mx-ops)
 
-echo "SMX_OP_INC = $(echo $SMX_INC)" > smx-op-inc.mk-t
-../move-if-change smx-op-inc.mk-t smx-op-inc.mk
+SMX_SRC=$($AWK -f $sparse_mk_ops prefix=smx list_cc_files=1 $liboctave_dir/sparse-mx-ops)
 
-echo "VX_OP_SRC = $(echo $VX_SRC)" > vx-op-src.mk-t
-../move-if-change vx-op-src.mk-t vx-op-src.mk
+echo "VX_OP_INC = $(echo $VX_INC)" > $liboctave_dir/vx-op-inc.mk-t
+$top_srcdir/move-if-change $liboctave_dir/vx-op-inc.mk-t $liboctave_dir/vx-op-inc.mk
 
-echo "MX_OP_SRC = $(echo $MX_SRC)" > mx-op-src.mk-t
-../move-if-change mx-op-src.mk-t mx-op-src.mk
+echo "MX_OP_INC = $(echo $MX_INC)" > $liboctave_dir/mx-op-inc.mk-t
+$top_srcdir/move-if-change $liboctave_dir/mx-op-inc.mk-t $liboctave_dir/mx-op-inc.mk
 
-echo "SMX_OP_SRC = $(echo $SMX_SRC)" > smx-op-src.mk-t
-../move-if-change smx-op-src.mk-t smx-op-src.mk
+echo "SMX_OP_INC = $(echo $SMX_INC)" > $liboctave_dir/smx-op-inc.mk-t
+$top_srcdir/move-if-change $liboctave_dir/smx-op-inc.mk-t $liboctave_dir/smx-op-inc.mk
+
+echo "VX_OP_SRC = $(echo $VX_SRC)" > $liboctave_dir/vx-op-src.mk-t
+$top_srcdir/move-if-change $liboctave_dir/vx-op-src.mk-t $liboctave_dir/vx-op-src.mk
+
+echo "MX_OP_SRC = $(echo $MX_SRC)" > $liboctave_dir/mx-op-src.mk-t
+$top_srcdir/move-if-change $liboctave_dir/mx-op-src.mk-t $liboctave_dir/mx-op-src.mk
+
+echo "SMX_OP_SRC = $(echo $SMX_SRC)" > $liboctave_dir/smx-op-src.mk-t
+$top_srcdir/move-if-change $liboctave_dir/smx-op-src.mk-t $liboctave_dir/smx-op-src.mk
--- src/DLD-FUNCTIONS/config-module.sh
+++ src/DLD-FUNCTIONS/config-module.sh
@@ -4,6 +4,14 @@ set -e
 
 : ${AWK=awk}
 
-$AWK -f config-module.awk < module-files > module.mk-t
+if [ $# -eq 1 ]; then
+  top_srcdir="$1";
+else
+  top_srcdir="../.."
+fi
 
-../../move-if-change module.mk-t module.mk
+dld_dir=$top_srcdir/src/DLD-FUNCTIONS
+
+$AWK -f $dld_dir/config-module.awk < $dld_dir/module-files > $dld_dir/module.mk-t
+
+$top_srcdir/move-if-change $dld_dir/module.mk-t $dld_dir/module.mk

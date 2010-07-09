18e326789a2551462ab821996c721222aeac5853
Jaroslav Hajek <highegg@gmail.com>
2010-07-09 14:12:59 +0200

    implement proper CGELSD/ZGELSD RWORK size formulas, as found in lapack SVN

--- liboctave/CMatrix.cc
+++ liboctave/CMatrix.cc
@@ -2646,10 +2646,9 @@ ComplexMatrix::lssolve (const ComplexMatrix& b, octave_idx_type& info,
       if (nlvl < 0)
         nlvl = 0;
 
-      nlvl += 1; // FIXME: workaround for bug in LAPACK 3.1.1
-
       octave_idx_type lrwork = minmn*(10 + 2*smlsiz + 8*nlvl)
-        + 3*smlsiz*nrhs + (smlsiz+1)*(smlsiz+1);
+        + 3*smlsiz*nrhs + std::max ((smlsiz+1)*(smlsiz+1),
+                                    n*(1+nrhs) + 2*nrhs);
       if (lrwork < 1)
         lrwork = 1;
       Array<double> rwork (lrwork, 1);

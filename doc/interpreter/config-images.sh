6a9982d3f913b35710569e95a981619a3f0213d3
Rik <octave@nomad.inbox5.com>
2010-08-09 11:21:00 -0700

    Add documentation Makefile dependency on images.mk

--- doc/interpreter/config-images.sh
+++ doc/interpreter/config-images.sh
@@ -4,5 +4,14 @@ set -e
 
 : ${AWK=awk}
 
-$AWK -f images.awk images > images.mk-t
-../../move-if-change images.mk-t images.mk
+if [ $# -eq 1 ]; then
+  top_srcdir="$1";
+else
+  top_srcdir="../.."
+fi
+
+interp_dir=$top_srcdir/doc/interpreter
+
+$AWK -f $interp_dir/images.awk < $interp_dir/images > $interp_dir/images.mk-t
+
+$top_srcdir/move-if-change $interp_dir/images.mk-t $interp_dir/images.mk

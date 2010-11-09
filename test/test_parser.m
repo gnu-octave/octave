74fcdc97d80dbb573422d40a58416663fda04354
John W. Eaton <jwe@octave.org>
2010-11-09 18:21:14 -0500

    add omitted file from previous change

---
+++ test/test_parser.m
@@ -0,0 +1,29 @@
+## Copyright (C) 2010 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Tests for parser problems belong in this file.  We need many more
+## tests here!
+
+%!assert ({1 2 {3 4}}, {1,2,{3,4}})
+%!assert ({1, 2 {3 4}}, {1,2,{3,4}})
+%!assert ({1 2, {3 4}}, {1,2,{3,4}})
+%!assert ({1 2 {3, 4}}, {1,2,{3,4}})
+%!assert ({1, 2, {3 4}}, {1,2,{3,4}})
+%!assert ({1 2,{3 4}}, {1,2,{3,4}})
+%!assert ({1 2,{3,4}}, {1,2,{3,4}})
+%!assert ({1,2,{3 4}}, {1,2,{3,4}})

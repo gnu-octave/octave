ee33d6c13b3eb3418f8ff407deb2de89c177f8e7
John W. Eaton <jwe@octave.org>
2011-01-04 16:57:19 -0500

    improve display of percentage full when printing sparse matrices

--- src/ov-base-sparse.cc
+++ src/ov-base-sparse.cc
@@ -321,7 +321,31 @@ octave_base_sparse<T>::print_raw (std::ostream& os,
   double dnel = matrix.numel ();
 
   if (dnel > 0)
-    os << " [" << std::setprecision (2) << (nz / dnel * 100) << "%]";
+    {
+      double pct = (nz / dnel * 100);
+
+      int prec = 2;
+
+      // Display at least 2 significant figures and up to 4 as we
+      // approach 100%.  Avoid having limited precision of the display
+      // result in reporting 100% for matrices that are not actually
+      // 100% full.
+
+      if (pct == 100)
+        prec = 3;
+      else
+        {
+          if (pct > 99.9)
+            prec = 4;
+          else if (pct > 99)
+            prec = 3;
+
+          if (pct > 99.99)
+            pct = 99.99;
+        }
+
+      os << " [" << std::setprecision (prec) << pct << "%]";
+    }
 
   os << ")\n";
 

5a36ddda7f7a25efa13537b6a33eb84707994531
John W. Eaton <jwe@octave.org>
2011-01-25 04:31:33 -0500

    autogen.sh: check for required files instead of having automake do it

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,23 @@
 
 set -e
 
-AUTOMAKE="automake --warnings=no-portability"
+## Use --foreign since we auto-generate the AUTHORS file and the default
+## --gnu strictness level doesn't like it if the AUTHORS file is missing.
+
+AUTOMAKE="automake --foreign --warnings=no-portability"
+export AUTOMAKE
+
+## Check for files that automake --gnu would normally look for, except
+## AUTHORS, which we autogenerate from the documentation files along with
+## building the rest of Octave, and INSTALL, which is linked from
+## gnulib/doc/INSTALL by the bootstrap script.
+
+for f in NEWS README ChangeLog COPYING; do
+  if ! test -f $f; then
+    echo "required file $f is missing" 2>&1
+    exit 1
+  fi
+done
 
 echo "generating source lists for liboctave/Makefile..."
 

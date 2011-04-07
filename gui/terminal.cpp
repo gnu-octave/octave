e9a0186ac916c8b08533d3229ef63c2a3bb5159f
Jacob Dawid <jacob.dawid@googlemail.com>
2011-04-07 10:55:33 +0200

    Loading octave on startup.

--- gui/terminal.cpp
+++ gui/terminal.cpp
@@ -4,14 +4,19 @@ Terminal::Terminal()
     : QMdiSubWindow(),
       m_terminalWidget(0) {
     setWindowTitle("Terminal Session");
+    resize(800, 400);
     launchTerminal();
 }
 
 void Terminal::launchTerminal() {
     delete m_terminalWidget;
-    m_terminalWidget = new QTermWidget(1, this);
+    m_terminalWidget = new QTermWidget(0, this);
     m_terminalWidget->setScrollBarPosition(QTermWidget::ScrollBarRight);
     setWidget(m_terminalWidget);
 
+    QString programName = "octave";
+    m_terminalWidget->setShellProgram(programName);
+    m_terminalWidget->startShellProgram();
+    setFocus();
     connect(m_terminalWidget, SIGNAL(finished()), this, SLOT(launchTerminal()));
 }

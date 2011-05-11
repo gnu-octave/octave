904a09bfcf5589e58b343697ffd4a34da81d0cdb
Jordi Gutiérrez Hermoso <jordigh@gmail.com>
2011-05-11 00:23:46 -0500

    Fix passing compiler options to mkoctfile
    * mkoctfile.in: Only pass the part after -W to the compiler, plus add
      a comma for consistency with -Wl option.
    
    * mkoctfile.m: Document the change.

--- scripts/miscellaneous/mkoctfile.m
+++ scripts/miscellaneous/mkoctfile.m
@@ -51,12 +51,13 @@
 ## Add the run-time path to the link command.
 ##
 ## @item -Wl,@dots{}
-## Pass flags though the linker like "-Wl,-rpath=@dots{}".
+## Pass flags through the linker e.g. "-Wl,-rpath=@dots{}".
 ## The quotes are needed since commas are interpreted as command
 ## separators.
 ##
-## @item -W@dots{}
-## Pass flags though the compiler like "-Wa,OPTION".
+## @item -W,@dots{}
+## Pass flags through the compiler, e.g. "-W,-O2". The quotes are needed
+## since commas are interpreted as command separators.
 ##
 ## @item -c
 ## Compile but do not link.

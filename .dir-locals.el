26c90218d5c1832566ab402564756c7a49951819
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2011-09-06 12:48:40 -0500

    Make .h files open in c++-mode in Emacs; make sure gnu style is set for C mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
-((nil . ((indent-tabs-mode . nil)
-         (fill-column . 72)))
- (c-default-mode "gnu")
+((nil .
+      ((c-file-style . "gnu")
+       (indent-tabs-mode . nil)
+       (fill-column . 72)
+       (eval . (when (string-match "\\.h\\'" (buffer-file-name))
+                   (c++-mode)
+                   (c-set-style "gnu")))))
  (change-log-mode . ((indent-tabs-mode . t)))
  (makefile-mode . ((indent-tabs-mode . t))))

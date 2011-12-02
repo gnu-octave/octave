4b40eb27d513bb08e76e4717adf58f3ef021833b
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2011-12-02 18:50:56 -0500

    Fix error reporting in urlwrite (bug #34971)

--- src/DLD-FUNCTIONS/urlwrite.cc
+++ src/DLD-FUNCTIONS/urlwrite.cc
@@ -805,8 +805,8 @@ urlwrite (\"http://www.google.com/search\", \"search.html\",\n\
 
   frame.add_fcn (cleanup_urlwrite, filename);
 
-  bool res;
-  curl_handle curl = curl_handle (url, method, param, ofile, res);
+  bool ok;
+  curl_handle curl = curl_handle (url, method, param, ofile, ok);
 
   ofile.close ();
 
@@ -817,7 +817,7 @@ urlwrite (\"http://www.google.com/search\", \"search.html\",\n\
 
   if (nargout > 0)
     {
-      if (res)
+      if (ok)
         {
           retval(2) = std::string ();
           retval(1) = true;
@@ -831,7 +831,7 @@ urlwrite (\"http://www.google.com/search\", \"search.html\",\n\
         }
     }
 
-  if (nargout < 2 && res)
+  if (nargout < 2 && ! ok)
     error ("urlwrite: curl: %s", curl.lasterror ().c_str ());
 
 #else
@@ -939,18 +939,18 @@ s = urlread (\"http://www.google.com/search\", \"get\",\n\
 
   std::ostringstream buf;
 
-  bool res;
-  curl_handle curl = curl_handle (url, method, param, buf, res);
+  bool ok;
+  curl_handle curl = curl_handle (url, method, param, buf, ok);
 
   if (nargout > 0)
     {
       retval(0) = buf.str ();
-      retval(1) = res;
+      retval(1) = ok;
       // Return empty string if no error occured.
-      retval(2) = res ? "" : curl.lasterror ();
+      retval(2) = ok ? "" : curl.lasterror ();
     }
 
-  if (nargout < 2 && !res)
+  if (nargout < 2 && ! ok)
     error ("urlread: curl: %s", curl.lasterror().c_str());
 
 #else

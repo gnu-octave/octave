1daf7d32c976587912e8d7caa1ed3f76c503e022
Ben Abbott <bpabbott@mac.com>
2011-12-21 19:19:39 -0500

    slice.m: Add clf() to demos.

--- scripts/plot/slice.m
+++ scripts/plot/slice.m
@@ -182,11 +182,13 @@ endfunction
 
 
 %!demo
+%! clf
 %! [x, y, z] = meshgrid (linspace (-8, 8, 32));
 %! v = sin (sqrt (x.^2 + y.^2 + z.^2)) ./ (sqrt (x.^2 + y.^2 + z.^2));
 %! slice (x, y, z, v, [], 0, []);
 
 %!demo
+%! clf
 %! [x, y, z] = meshgrid (linspace (-8, 8, 32));
 %! v = sin (sqrt (x.^2 + y.^2 + z.^2)) ./ (sqrt (x.^2 + y.^2 + z.^2));
 %! [xi, yi] = meshgrid (linspace (-7, 7));

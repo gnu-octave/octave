a2ee0d0fd6bbfd59eb1e738fa6bde2ed52728e8a
John W. Eaton <jwe@octave.org>
2011-12-31 13:53:14 -0500

    eliminate duplicate indexing tests
    * test_index-wfi-t.m: Delete
    * test_index.m: Rename from test_index-wfi-f.m.
    Flush comments marking origin of tests from old DejaGNU scripts.

--- test/test_index.m
+++ test/test_index.m
@@ -16,212 +16,176 @@
 ## along with Octave; see the file COPYING.  If not, see
 ## <http://www.gnu.org/licenses/>.
 
-%% test/octave.test/index-wfi-f/s-1.m
 %!test
 %! a = [];
 %! assert(isempty (a));
 
-%% test/octave.test/index-wfi-f/s-2.m
 %!test
 %! a = 1;
 %! assert(a(1),1);
 
-%% test/octave.test/index-wfi-f/s-3.m
 %!test
 %! a = 1;
 %! assert(a(:),1);
 
-%% test/octave.test/index-wfi-f/s-4.m
 %!test
 %! a = 1;
 %! assert(a(:,:),1);
 
-%% test/octave.test/index-wfi-f/s-5.m
 %!test
 %! a = 1;
 %! assert(a(1,:),1);
 
-%% test/octave.test/index-wfi-f/s-6.m
 %!test
 %! a = 1;
 %! assert(a(:,1),1);
 
-%% test/octave.test/index-wfi-f/s-7.m
 %!test
 %! a = 1;
 %! assert(isempty (a(logical (0))));
 
-%% test/octave.test/index-wfi-f/s-8.m
 %!test
 %! a = 1;
 %! fail("a(-1)");
 
-%% test/octave.test/index-wfi-f/s-9.m
 %!test
 %! a = 1;
 %! fail("a(2);");
 
-%% test/octave.test/index-wfi-f/s-10.m
 %!test
 %! a = 1;
 %! fail("a(2,:);");
 
-%% test/octave.test/index-wfi-f/s-11.m
 %!test
 %! a = 1;
 %! fail("a(:,2);");
 
-%% test/octave.test/index-wfi-f/s-12.m
 %!test
 %! a = 1;
 %! fail("a(-1,:);");
 
-%% test/octave.test/index-wfi-f/s-13.m
 %!test
 %! a = 1;
 %! fail("a(:,-1);");
 
-%% test/octave.test/index-wfi-f/s-14.m
 %!test
 %! a = 1;
 %! fail("a([1,2,3]);");
 
-%% test/octave.test/index-wfi-f/s-15.m
 %!test
 %! a = 1;
 %! fail("a([1;2;3]);");
 
-%% test/octave.test/index-wfi-f/s-16.m
 %!test
 %! a = 1;
 %! fail("a([1,2;3,4]);");
 
-%% test/octave.test/index-wfi-f/s-17.m
 %!test
 %! a = 1;
 %! fail("a([0,1]);");
 
-%% test/octave.test/index-wfi-f/s-18.m
 %!test
 %! a = 1;
 %! fail("a([0;1]);");
 
-%% test/octave.test/index-wfi-f/s-19.m
 %!test
 %! a = 1;
 %! fail("a([-1,0]);");
 
-%% test/octave.test/index-wfi-f/s-20.m
 %!test
 %! a = 1;
 %! fail("a([-1;0]);");
 
-%% test/octave.test/index-wfi-f/v-1.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(a(1),4);
 
-%% test/octave.test/index-wfi-f/v-2.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(a(2),3);
 
-%% test/octave.test/index-wfi-f/v-3.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(all (a(:) == a_prime));
 
-%% test/octave.test/index-wfi-f/v-4.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(all (a(1,:) == a));
 
-%% test/octave.test/index-wfi-f/v-5.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(a(:,3),2);
 
-%% test/octave.test/index-wfi-f/v-6.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(all (a(:,:) == a));
 
-%% test/octave.test/index-wfi-f/v-7.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(all (a(logical ([0,1,1,0])) == mid_a));
 
-%% test/octave.test/index-wfi-f/v-8.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! fail("a(0);");
 
-%% test/octave.test/index-wfi-f/v-9.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! fail("a(5);");
 
-%% test/octave.test/index-wfi-f/v-10.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! fail("a(0,1);");
 
-%% test/octave.test/index-wfi-f/v-11.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(isempty (a(logical (0),:)));
 
-%% test/octave.test/index-wfi-f/v-12.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! fail("a(:,0);");
 
-%% test/octave.test/index-wfi-f/v-13.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(isempty (a([])));
 
-%% test/octave.test/index-wfi-f/v-14.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(isempty (a([],:)));
 
-%% test/octave.test/index-wfi-f/v-15.m
 %!test
 %! a = [4,3,2,1];
 %! a_prime = [4;3;2;1];
 %! mid_a = [3,2];
 %! assert(isempty (a(:,[])));
 
-%% test/octave.test/index-wfi-f/m-1.m
 %!test
 %! a = [1,2;3,4];
 %! a_fvec = [1;3;2;4];
@@ -231,7 +195,6 @@
 %! a_row_2 = [3,4];
 %! assert(all (all (a(:,:) == a)));
 
-%% test/octave.test/index-wfi-f/m-2.m
 %!test
 %! a = [1,2;3,4];
 %! a_fvec = [1;3;2;4];
@@ -241,7 +204,6 @@
 %! a_row_2 = [3,4];
 %! assert(all (a(:) == a_fvec));
 
-%% test/octave.test/index-wfi-f/m-3.m
 %!test
 %! a = [1,2;3,4];
 %! a_fvec = [1;3;2;4];
@@ -251,7 +213,6 @@
 %! a_row_2 = [3,4];
 %! fail("a(0);");
 
-%% test/octave.test/index-wfi-f/m-4.m
 %!test
 %! a = [1,2;3,4];
 %! a_fvec = [1;3;2;4];

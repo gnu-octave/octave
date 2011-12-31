f79bf4bc15f14a2b79debf78f1ebf79f95d88c92
John W. Eaton <jwe@octave.org>
2011-12-31 13:50:40 -0500

    eliminate duplicate logical indexing tests
    * test_logical-wfi-t.m: Delete
    * test_logical_indexing.m: Rename from test_logical-wfi-f.m.
    Flush comments marking origin of tests from old DejaGNU scripts.

--- test/test_logical_index.m
+++ test/test_logical_index.m
@@ -16,223 +16,179 @@
 ## along with Octave; see the file COPYING.  If not, see
 ## <http://www.gnu.org/licenses/>.
 
-%% test/octave.test/logical-wfi-t/s-1.m
 %!test
 %! a = [];
 %! fail("a(0);");
 
-%% test/octave.test/logical-wfi-t/s-2.m
 %!test
 %! a = 2;
 %! assert(a(1) == 2);
 
-%% test/octave.test/logical-wfi-t/s-3.m
 %!test
 %! a = 2;
 %! assert(a(1) == 2);
 
-%% test/octave.test/logical-wfi-t/s-4.m
 %!test
 %!shared a
-%! a = 2;
+%!  a = 2;
 %!error id=Octave:index-out-of-bounds a(logical ([1,1]));
 
-%% test/octave.test/logical-wfi-t/v-1.m
 %!test
 %! a = [9,8,7,6];
 %! assert(isempty (a(logical ([0,0,0,0]))));
 
-%% test/octave.test/logical-wfi-t/v-2.m
 %!test
 %! a = [9,8,7,6];
 %! assert(all (a(logical ([1,1,1,1])) == [9,8,7,6]));
 
-%% test/octave.test/logical-wfi-t/v-3.m
 %!test
 %! a = [9,8,7,6];
 %! assert(all (a(logical ([0,1,1,0])) == [8,7]));
 
-%% test/octave.test/logical-wfi-t/v-4.m
 %!test
 %! a = [9,8,7,6];
 %! assert(all (a(logical ([1,1])) == [9,8]));
 
-%% test/octave.test/logical-wfi-t/m-1.m
 %!test
 %! a = [9,8;7,6];
-%! isempty (a(logical ([0,0,0,0])));
+%! assert(isempty (a(logical ([0,0,0,0]))));
 
-%% test/octave.test/logical-wfi-t/m-2.m
 %!test
 %! a = [9,8;7,6];
-%! all (a(logical ([1,1,1,1])) == [9,7,8,6]);
+%! assert(all (a(logical ([1,1,1,1])) == [9,7,8,6]));
 
-%% test/octave.test/logical-wfi-t/m-3.m
 %!test
 %! a = [9,8;7,6];
-%! all (a(logical ([0,1,1,0])) == [7,8]);
+%! assert(all (a(logical ([0,1,1,0])) == [7,8]));
 
-%% test/octave.test/logical-wfi-t/m-4.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical (0:1),logical (0:1)) == 6);
 
-%% test/octave.test/logical-wfi-t/m-5.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical (0:1),2:-1:1) == [6,7]));
 
-%% test/octave.test/logical-wfi-t/m-6.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical (0:1),logical ([0,1])) == 6);
 
-%% test/octave.test/logical-wfi-t/m-7.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical (0:1),[2,1]) == [6,7]));
 
-%% test/octave.test/logical-wfi-t/m-8.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical (0:1),:) == [7,6]));
 
-%% test/octave.test/logical-wfi-t/m-9.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical (0:1),1) == 7);
 
-%% test/octave.test/logical-wfi-t/m-10.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical (0:1),logical ([1,1])) == [7,6]));
 
-%% test/octave.test/logical-wfi-t/m-11.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(2:-1:1,logical (0:1)) == [6;8]));
 
-%% test/octave.test/logical-wfi-t/m-12.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(2:-1:1,logical ([0,1])) == [6;8]));
 
-%% test/octave.test/logical-wfi-t/m-13.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(2:-1:1,logical ([1,1])) == [7,6;9,8])));
 
-%% test/octave.test/logical-wfi-t/m-14.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical ([0,1]),logical (0:1)) == 6);
 
-%% test/octave.test/logical-wfi-t/m-15.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([0,1]),2:-1:1) == [6,7]));
 
-%% test/octave.test/logical-wfi-t/m-16.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical ([0,1]),logical ([0,1])) == 6);
 
-%% test/octave.test/logical-wfi-t/m-17.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([0,1]),[2,1]) == [6,7]));
 
-%% test/octave.test/logical-wfi-t/m-18.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([0,1]),:) == [7,6]));
 
-%% test/octave.test/logical-wfi-t/m-19.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(logical ([0,1]),1) == 7);
 
-%% test/octave.test/logical-wfi-t/m-20.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([0,1]),logical ([1,1])) == [7,6]));
 
-%% test/octave.test/logical-wfi-t/m-21.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a([2,1],logical (0:1)) == [6;8]));
 
-%% test/octave.test/logical-wfi-t/m-22.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a([2,1],logical ([0,1])) == [6;8]));
 
-%% test/octave.test/logical-wfi-t/m-23.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a([2,1],logical ([1,1])) == [7,6;9,8])));
 
-%% test/octave.test/logical-wfi-t/m-24.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(:,logical (0:1)) == [8;6]));
 
-%% test/octave.test/logical-wfi-t/m-25.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(:,logical ([0,1])) == [8;6]));
 
-%% test/octave.test/logical-wfi-t/m-26.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(:,logical ([1,1])) == [9,8;7,6])));
 
-%% test/octave.test/logical-wfi-t/m-27.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(1,logical (0:1)) == 8);
 
-%% test/octave.test/logical-wfi-t/m-28.m
 %!test
 %! a = [9,8;7,6];
 %! assert(a(1,logical ([0,1])) == 8);
 
-%% test/octave.test/logical-wfi-t/m-29.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(1,logical ([1,1])) == [9,8]));
 
-%% test/octave.test/logical-wfi-t/m-30.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([1,1]),logical (0:1)) == [8;6]));
 
-%% test/octave.test/logical-wfi-t/m-31.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(logical ([1,1]),2:-1:1) == [8,9;6,7])));
 
-%% test/octave.test/logical-wfi-t/m-32.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([1,1]),logical ([0,1])) == [8;6]));
 
-%% test/octave.test/logical-wfi-t/m-33.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(logical ([1,1]),[2,1]) == [8,9;6,7])));
 
-%% test/octave.test/logical-wfi-t/m-34.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(logical ([1,1]),:) == [9,8;7,6])));
 
-%% test/octave.test/logical-wfi-t/m-35.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (a(logical ([1,1]),1) == [9;7]));
 
-%% test/octave.test/logical-wfi-t/m-36.m
 %!test
 %! a = [9,8;7,6];
 %! assert(all (all (a(logical ([1,1]),logical ([1,1])) == [9,8;7,6])));

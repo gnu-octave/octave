6a844a1891051b24d5fc6bbdede06494a7b1347d
Jacob Dawid <jacob.dawid@googlemail.com>
2012-01-24 20:02:09 +0100

    Added similar script to retrieve libqirc.

--- gui/get-qirc.sh
+++ gui/get-qirc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-qterminal_dir="qterminal";
-echo "Updating qterminal..";
+qirc_dir="qirc";
+echo "Updating qirc..";
 
 # Check whether we have already cloned the repository:
-if [ -d $qterminal_dir ]; then
+if [ -d $qirc_dir ]; then
 	# Yes, so just pull any changes and rebuild.
-	cd qterminal
+	cd qirc
 	git pull
-	qmake-qt4 qterminal.pro
+	qmake-qt4 qirc.pro
 	cd ..
 else
 	# No, clone the repository, checkout the stable branch
 	# and build it.
-	git clone https://code.google.com/p/qterminal/
+	git clone https://code.google.com/p/qirc/
 	git checkout master
-	cd qterminal
-	qmake-qt4 qterminal.pro
+	cd qirc
+	qmake-qt4 qirc.pro
 	make
 	cd ..
 fi

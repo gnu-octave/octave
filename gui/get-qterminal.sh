0384a024763bbe2600854dcc7d174f6059854e30
Jacob Dawid <jacob.dawid@googlemail.com>
2012-01-24 19:57:32 +0100

    Improved clone script for qterminal. *(get-qterminal.sh): Now pulling instead of cloning when directory exists.

--- gui/get-qterminal.sh
+++ gui/get-qterminal.sh
@@ -1,6 +1,22 @@
-git clone https://code.google.com/p/qterminal/
-git checkout master
-cd qterminal
-qmake-qt4 qterminal.pro
-make
-cd ..
+#!/bin/bash
+
+qterminal_dir="qterminal";
+echo "Updating qterminal..";
+
+# Check whether we have already cloned the repository:
+if [ -d $qterminal_dir ]; then
+	# Yes, so just pull any changes and rebuild.
+	cd qterminal
+	git pull
+	qmake-qt4
+	cd ..
+else
+	# No, clone the repository, checkout the stable branch
+	# and build it.
+	git clone https://code.google.com/p/qterminal/
+	git checkout master
+	cd qterminal
+	qmake-qt4 qterminal.pro
+	make
+	cd ..
+fi

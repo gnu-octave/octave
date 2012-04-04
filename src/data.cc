c554b76e7c439a4091dea0f89cf24da5b2261360
Rik <octave@nomad.inbox5.com>
2012-04-03 20:44:05 -0700

    test: remove obsolete warning test for linspace.
    data.cc: delete test that depended on array-to-vector warning being on.

--- src/data.cc
+++ src/data.cc
@@ -4719,9 +4719,7 @@ if fewer than two values are requested.\n\
 %! assert (size (x2) == [1, 10] && x2(1) == 1 && x2(10) == 2);
 %! assert (size (x3) == [1, 10] && x3(1) == 1 && x3(10) == -2);
 
-%#assert (linspace ([1, 2; 3, 4], 5, 6), linspace (1, 5, 6))
-
-%!fail ("linspace ([1, 2; 3, 4], 5, 6)", "warning")
+%assert (linspace ([1, 2; 3, 4], 5, 6), linspace (1, 5, 6))
 
 %!error linspace ()
 %!error linspace (1, 2, 3, 4)

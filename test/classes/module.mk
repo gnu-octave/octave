efa6b4311a395731b5d8a724a29394e12c23ab2d
Julien Bect <julien.bect@supelec.fr>
2013-02-23 12:55:16 +0100

    Add to test/classes/module.mk new files from changeset e603ce23f20c
    * test/classes/module.mk: Add new files from changeset e603ce23f20c.
      Rewrite for clarity using auxiliary variables (one for each test
      class).

--- test/classes/module.mk
+++ test/classes/module.mk
@@ -1,14 +1,18 @@
-classes_FCN_FILES = \
+class_Blork_FCN_FILES = \
   classes/@Blork/Blork.m \
   classes/@Blork/bleek.m \
   classes/@Blork/display.m \
   classes/@Blork/get.m \
-  classes/@Blork/set.m \
+  classes/@Blork/set.m
+
+class_Cork_FCN_FILES = \
   classes/@Cork/Cork.m \
   classes/@Cork/click.m \
   classes/@Cork/display.m \
   classes/@Cork/get.m \
-  classes/@Cork/set.m \
+  classes/@Cork/set.m
+
+class_Dork_FCN_FILES = \
   classes/@Dork/Dork.m \
   classes/@Dork/bling.m \
   classes/@Dork/display.m \
@@ -16,7 +20,9 @@ classes_FCN_FILES = \
   classes/@Dork/get.m \
   classes/@Dork/getStash.m \
   classes/@Dork/private/myStash.m \
-  classes/@Dork/set.m \
+  classes/@Dork/set.m
+
+class_Gork_FCN_FILES = \
   classes/@Gork/Gork.m \
   classes/@Gork/cork.m \
   classes/@Gork/display.m \
@@ -24,16 +30,22 @@ classes_FCN_FILES = \
   classes/@Gork/get.m \
   classes/@Gork/set.m \
   classes/@Gork/subsasgn.m \
-  classes/@Gork/subsref.m \
+  classes/@Gork/subsref.m
+
+class_Pork_FCN_FILES = \
   classes/@Pork/Pork.m \
   classes/@Pork/bling.m \
   classes/@Pork/display.m \
   classes/@Pork/get.m \
   classes/@Pork/gurk.m \
   classes/@Pork/private/myStash.m \
-  classes/@Pork/set.m \
+  classes/@Pork/set.m
+
+class_Sneetch_FCN_FILES = \
   classes/@Sneetch/Sneetch.m \
-  classes/@Sneetch/display.m \
+  classes/@Sneetch/display.m
+
+class_Snork_FCN_FILES = \
   classes/@Snork/Snork.m \
   classes/@Snork/cack.m \
   classes/@Snork/display.m \
@@ -68,7 +80,9 @@ classes_FCN_FILES = \
   classes/@Snork/times.m \
   classes/@Snork/uminus.m \
   classes/@Snork/uplus.m \
-  classes/@Snork/vertcat.m \
+  classes/@Snork/vertcat.m
+
+class_Spork_FCN_FILES = \
   classes/@Spork/Spork.m \
   classes/@Spork/cack.m \
   classes/@Spork/display.m \
@@ -78,7 +92,32 @@ classes_FCN_FILES = \
   classes/@Spork/loadobj.m \
   classes/@Spork/private/myStash.m \
   classes/@Spork/saveobj.m \
-  classes/@Spork/set.m \
+  classes/@Spork/set.m
+
+class_CPrecedenceTester1_FCN_FILES = \
+  classes/@CPrecedenceTester1/CPrecedenceTester1.m \
+  classes/@CPrecedenceTester1/tattack.m
+
+class_CPrecedenceTester2_FCN_FILES = \
+  classes/@CPrecedenceTester2/CPrecedenceTester2.m \
+  classes/@CPrecedenceTester2/tattack.m
+
+class_CPrecedenceTester3_FCN_FILES = \
+  classes/@CPrecedenceTester3/CPrecedenceTester3.m \
+  classes/@CPrecedenceTester3/tattack.m
+
+classes_FCN_FILES = \
+  $(class_Blork_FCN_FILES) \
+  $(class_Cork_FCN_FILES) \
+  $(class_Dork_FCN_FILES) \
+  $(class_Gork_FCN_FILES) \
+  $(class_Pork_FCN_FILES) \
+  $(class_Sneetch_FCN_FILES) \
+  $(class_Snork_FCN_FILES) \
+  $(class_Spork_FCN_FILES) \
+  $(class_CPrecedenceTester1_FCN_FILES) \
+  $(class_CPrecedenceTester2_FCN_FILES) \
+  $(class_CPrecedenceTester3_FCN_FILES) \
   classes/classes.tst
 
 FCN_FILES += $(classes_FCN_FILES)

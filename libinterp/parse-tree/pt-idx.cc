af52807af0b925d2d5f59b12438759f1ab6508a0
John W. Eaton <jwe@octave.org>
2013-05-14 05:24:19 -0400

    require that dynamic field names have only one row
    * pt-idx.cc (tree_index_expression::get_struct_index):
    Require field name to have one row.

--- libinterp/parse-tree/pt-idx.cc
+++ libinterp/parse-tree/pt-idx.cc
@@ -217,7 +217,7 @@ tree_index_expression::get_struct_index
 
           if (! error_state)
             {
-              if (t.is_string ())
+              if (t.is_string () && t.rows () == 1)
                 fn = t.string_value ();
               else
                 error ("dynamic structure field names must be character strings");

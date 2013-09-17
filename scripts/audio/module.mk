29cb3cb543d3f388d34dad84a38cb0485dc3648f
Vytautas Jančauskas <unaudio@gmail.com>
2013-09-18 00:16:28 +0300

    maint: moved @audiorecorder and @audioplayer folders inside the audio folder

--- scripts/audio/module.mk
+++ scripts/audio/module.mk
@@ -9,7 +9,36 @@ audio_FCN_FILES = \
   audio/saveaudio.m \
   audio/setaudio.m \
   audio/wavread.m \
-  audio/wavwrite.m
+  audio/wavwrite.m \
+  audio/@audioplayer/__get_properties__.m \
+  audio/@audioplayer/audioplayer.m \
+  audio/@audioplayer/display.m  \
+  audio/@audioplayer/get.m  \
+  audio/@audioplayer/isplaying.m  \
+  audio/@audioplayer/pause.m  \
+  audio/@audioplayer/play.m  \
+  audio/@audioplayer/playblocking.m  \
+  audio/@audioplayer/resume.m  \
+  audio/@audioplayer/set.m \
+  audio/@audioplayer/stop.m \
+  audio/@audioplayer/subsasgn.m \
+  audio/@audioplayer/subsref.m \
+  audio/@audiorecorder/__get_properties__.m \
+  audio/@audiorecorder/audiorecorder.m \
+  audio/@audiorecorder/display.m \
+  audio/@audiorecorder/get.m \
+  audio/@audiorecorder/getaudiodata.m \
+  audio/@audiorecorder/getplayer.m \
+  audio/@audiorecorder/isrecording.m \
+  audio/@audiorecorder/pause.m \
+  audio/@audiorecorder/play.m \
+  audio/@audiorecorder/record.m \
+  audio/@audiorecorder/recordblocking.m \
+  audio/@audiorecorder/resume.m \
+  audio/@audiorecorder/set.m \
+  audio/@audiorecorder/stop.m  \
+  audio/@audiorecorder/subsasgn.m  \
+  audio/@audiorecorder/susbref.m
 
 FCN_FILES += $(audio_FCN_FILES)
 

2f1c012f9a979a6238b7a68d2e68f14dff43ad2f
Mike Miller <mtmiller@ieee.org>
2013-10-01 23:36:37 -0400

    Add Octave copyright header block to audio project files
    * libinterp/dldfcn/__player_audioplayer__.cc,
    libinterp/dldfcn/__recorder_audiorecorder__.cc,
    libinterp/dldfcn/audiodevinfo.cc, libinterp/dldfcn/audioinfo.cc,
    libinterp/dldfcn/audioread.cc, libinterp/dldfcn/audiowrite.cc,
    libinterp/dldfcn/player_class.cc, libinterp/dldfcn/player_class.h,
    libinterp/dldfcn/recorder_class.cc, libinterp/dldfcn/recorder_class.h,
    scripts/audio/@audioplayer/__get_properties__.m,
    scripts/audio/@audioplayer/audioplayer.m,
    scripts/audio/@audioplayer/display.m, scripts/audio/@audioplayer/get.m,
    scripts/audio/@audioplayer/isplaying.m, scripts/audio/@audioplayer/pause.m,
    scripts/audio/@audioplayer/play.m,
    scripts/audio/@audioplayer/playblocking.m,
    scripts/audio/@audioplayer/resume.m, scripts/audio/@audioplayer/set.m,
    scripts/audio/@audioplayer/stop.m, scripts/audio/@audioplayer/subsasgn.m,
    scripts/audio/@audioplayer/subsref.m,
    scripts/audio/@audiorecorder/__get_properties__.m,
    scripts/audio/@audiorecorder/audiorecorder.m,
    scripts/audio/@audiorecorder/display.m, scripts/audio/@audiorecorder/get.m,
    scripts/audio/@audiorecorder/getaudiodata.m,
    scripts/audio/@audiorecorder/getplayer.m,
    scripts/audio/@audiorecorder/isrecording.m,
    scripts/audio/@audiorecorder/pause.m, scripts/audio/@audiorecorder/play.m,
    scripts/audio/@audiorecorder/record.m,
    scripts/audio/@audiorecorder/recordblocking.m,
    scripts/audio/@audiorecorder/resume.m, scripts/audio/@audiorecorder/set.m,
    scripts/audio/@audiorecorder/stop.m,
    scripts/audio/@audiorecorder/subsasgn.m,
    scripts/audio/@audiorecorder/subsref.m: Add Octave copyright header.

--- scripts/audio/@audiorecorder/__get_properties__.m
+++ scripts/audio/@audiorecorder/__get_properties__.m
@@ -1,3 +1,21 @@
+## Copyright (C) 2013 Vytautas Jančauskas
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
 function properties = __get_properties__ (recorder)
   properties.BitsPerSample = __recorder_get_nbits__ (struct(recorder).recorder);
   properties.CurrentSample = __recorder_get_sample_number__ (struct(recorder).recorder);
--- scripts/audio/@audiorecorder/set.m
+++ scripts/audio/@audiorecorder/set.m
@@ -1,3 +1,21 @@
+## Copyright (C) 2013 Vytautas Jančauskas
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
 ## -*- texinfo -*-
 ## @deftypefn{Function File} set (@var{recorderObj}, @var{Name}, @var{Value})
 ## Set the value of property specified by @var{Name} to a given @var{Value}.

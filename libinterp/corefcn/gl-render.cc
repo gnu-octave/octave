dcba81aac46f89f1d83fb96ee8a0153d0b8b1567
Rik <rik@octave.org>
2013-10-03 16:42:42 -0700

    Don't plot NaN values in cdata (bug #40114).
    * libinterp/corefcn/gl-render.cc(draw_surface): Check colordata for NaNs in addition
    to x,y,z data and set clip() matrix appropriately.

--- libinterp/corefcn/gl-render.cc
+++ libinterp/corefcn/gl-render.cc
@@ -1600,6 +1600,9 @@ opengl_renderer::draw_surface (const surface::properties& props)
 
   i1 = i2 = j1 = j2 = 0;
 
+  if ((fc_mode > 0 && fc_mode < 3) || ec_mode > 0)
+    c = props.get_color_data ().array_value ();
+
   boolMatrix clip (z.dims (), false);
 
   for (int i = 0; i < zr; i++)
@@ -1613,11 +1616,10 @@ opengl_renderer::draw_surface (const surface::properties& props)
             j1 = j;
 
           clip(i,j) = is_nan_or_inf (x(i1,j), y(i,j1), z(i,j));
+          if (fc_mode == 1 || fc_mode == 2)
+            clip(i,j) |= (xisnan (c(i,j)) || xisinf (c(i,j)));
         }
     }
-
-  if ((fc_mode > 0 && fc_mode < 3) || ec_mode > 0)
-    c = props.get_color_data ().array_value ();
 
   if (fa_mode > 0 || ea_mode > 0)
     {

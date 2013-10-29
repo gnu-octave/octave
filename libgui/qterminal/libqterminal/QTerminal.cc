b0e09d1b5f59b679fb2f9ae08c446e982a3da182
Rik <rik@octave.org>
2013-10-29 12:31:57 -0700

    maint: Fix various problems with GPL copyright statements.
    * libgui/qterminal/libqterminal/QTerminal.cc,
    libgui/qterminal/libqterminal/QTerminal.h,
    libgui/qterminal/libqterminal/win32/QTerminalColors.cpp,
    libgui/qterminal/libqterminal/win32/QTerminalColors.h,
    libgui/qterminal/libqterminal/win32/QWinTerminalImpl.cpp,
    libgui/qterminal/libqterminal/win32/QWinTerminalImpl.h:
    Replace "Foobar" with "This program" in GPL statement.
    
    * scripts/linear-algebra/linsolve.m: Replace "This program" with "Octave" in
    * GPL statement.
    
    * scripts/miscellaneous/getappdata.m, scripts/miscellaneous/isappdata.m,
    scripts/miscellaneous/rmappdata.m, scripts/miscellaneous/setappdata.m:
    Add "This file is part of Octave" line.  Replace "This program" with "Octave"
    in GPL statement.  Update GPL version to 3.
    
    * scripts/plot/util/copyobj.m, scripts/plot/util/hdl2struct.m,
    scripts/plot/util/struct2hdl.m, scripts/polynomial/ppder.m,
    scripts/polynomial/ppint.m, scripts/polynomial/ppjumps.m,
    scripts/sparse/bicg.m, scripts/strings/untabify.m:
    Replace "This program" with "Octave" in GPL statement.

--- libgui/qterminal/libqterminal/QTerminal.cc
+++ libgui/qterminal/libqterminal/QTerminal.cc
@@ -5,18 +5,19 @@ Copyright (C) 2012-2013 Jacob Dawid.
 
 This file is part of QTerminal.
 
-Foobar is free software: you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-QTerminal is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not,
+see <http://www.gnu.org/licenses/>.
 
 */
 

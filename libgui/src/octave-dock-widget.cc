c75f5a05ea392f6238d316355c0d3845a16acbaa
John W. Eaton <jwe@octave.org>
2013-11-13 11:27:46 -0500

    * octave-dock-widget.cc: Include config.h.

--- libgui/src/octave-dock-widget.cc
+++ libgui/src/octave-dock-widget.cc
@@ -21,6 +21,10 @@ along with Octave; see the file COPYING.  If not, see
 
 */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <QApplication>
 #include <QToolBar>
 #include <QToolButton>

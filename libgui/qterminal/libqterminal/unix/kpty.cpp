e98a58330857c67a9326067ef3bc3c4d0b8cacb7
Mike Miller <mtmiller@ieee.org>
2014-01-12 17:55:29 -0500

    Fix build failure on Unix systems without PATH_MAX (bug #41212)
    * kpty.cpp (PATH_MAX): Define to fallback values on systems that do not
    provide PATH_MAX. Based on a change made to kdelibs after qterminal was
    made based on it.

--- libgui/qterminal/libqterminal/unix/kpty.cpp
+++ libgui/qterminal/libqterminal/unix/kpty.cpp
@@ -145,6 +145,14 @@ extern "C" {
 
 #define TTY_GROUP "tty"
 
+#ifndef PATH_MAX
+# ifdef MAXPATHLEN
+#  define PATH_MAX MAXPATHLEN
+# else
+#  define PATH_MAX 1024
+# endif
+#endif
+
 ///////////////////////
 // private functions //
 ///////////////////////

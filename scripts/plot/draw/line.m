002b7c9fd6a485598b5eefee25236eb5e9bd63f6
Rik <rik@octave.org>
2014-07-01 09:41:50 -0700

    line.m: Add new %!demo of different linestyles.
    * line.m: Add new %!demo of different linestyles.

--- scripts/plot/draw/line.m
+++ scripts/plot/draw/line.m
@@ -64,6 +64,16 @@ endfunction
 
 %!demo
 %! clf
+%! line ([0 1], [0.8 0.8], 'linestyle', '-', 'color', 'b');
+%! line ([0 1], [0.6 0.6], 'linestyle', '--', 'color', 'g');
+%! line ([0 1], [0.4 0.4], 'linestyle', ':', 'color', 'r');
+%! line ([0 1], [0.2 0.2], 'linestyle', '-.', 'color', 'k');
+%! ylim ([0 1]);
+%! title ('line() with various linestyles');
+%! legend ('"-"', '"--"', '":"', '"-."', 'location', 'eastoutside');
+
+%!demo
+%! clf
 %! x = 0:0.3:10;
 %! y1 = cos (x);
 %! y2 = sin (x);

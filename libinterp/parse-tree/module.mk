79481ca847b4d69ffc2323dd33c14d74f4d54bb8
John W. Eaton <jwe@octave.org>
2014-08-01 14:06:35 -0400

    fix botched merge

--- libinterp/parse-tree/module.mk
+++ libinterp/parse-tree/module.mk
@@ -87,7 +87,7 @@ parse-tree/oct-gperf.h: parse-tree/octave.gperf
 
 parse-tree/oct-parse.yy: parse-tree/oct-parse.in.yy
 	case "$(BISON_API_PREFIX_DECL_STYLE)" in \
-	  *api*) api_prefix_decl='%define api.prefix "octave-"'; ;; \
+	  *api*) api_prefix_decl='%define api.prefix "octave_"'; ;; \
 	  *name*) api_prefix_decl='%name-prefix="octave_"'; ;; \
 	esac; \
 	case "$(BISON_PUSH_PULL_DECL_STYLE)" in \

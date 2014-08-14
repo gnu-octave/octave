8d1144dd81b3e86a2d46ec35c7fc10b6899fba1f
Torsten <ttl@justmail.de>
2014-08-14 09:16:16 +0200

    fix crash when mru list of editor is empty (regression from cset 12462638ab20)
    * file-editor.cc (handle_mru_add_file): only check first element of mru
      list if available

--- libgui/src/m-editor/file-editor.cc
+++ libgui/src/m-editor/file-editor.cc
@@ -835,7 +835,7 @@ file_editor::request_completion (bool)
 void
 file_editor::handle_mru_add_file (const QString& file_name)
 {
-  if (_mru_files.at (0) == file_name)
+  if (_mru_files.count () && _mru_files.at (0) == file_name)
     return;  // the first entry is already the actual file name
 
   _mru_files.removeAll (file_name);

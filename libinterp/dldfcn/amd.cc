2ae07af982fab7fa82328d8d5c0c7e97b96434de
Mike Miller <mtmiller@ieee.org>
2014-10-03 23:43:58 -0400

    Use %!testif HAVE_AMD for amd tests added in cset e99d7a2e7367
    * amd.cc: Use %!testif HAVE_AMD for amd tests that were added in cset
    e99d7a2e7367. Add regexp to match the appropriate error message when
    building without SuiteSparse.

--- libinterp/dldfcn/amd.cc
+++ libinterp/dldfcn/amd.cc
@@ -209,14 +209,14 @@ The author of the code itself is Timothy A. Davis\n\
 %! A = ones (20, 30);
 %! A2 = ones (30, 30);
 %!
-%!test
-%!error <matrix S must be square> amd (A)
-%!error amd (A2, 2)
-%!error <matrix S is corrupted> amd([])
-%!test
+%!testif HAVE_AMD
 %! assert(amd (A2), [1:30])
 %! opts.dense = 25;
 %! assert(amd (A2, opts), [1:30])
 %! opts.aggressive = 1;
 %! assert(amd (A2, opts), [1:30])
+
+%!error <matrix S must be square|not available in this version> amd (A)
+%!error amd (A2, 2)
+%!error <matrix S is corrupted|not available in this version> amd ([])
 */

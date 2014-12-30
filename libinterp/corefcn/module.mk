a011293648b88bec3b89d31e75875b147c677694
Rik <rik@octave.org>
2014-12-30 14:04:53 -0800

    Don't distribute mxarray.h in tarball (bug #43805).
    * libinterp/corefcn/module.mk: Remove mxarray.h from COREFCN_INC include files.

--- libinterp/corefcn/module.mk
+++ libinterp/corefcn/module.mk
@@ -76,7 +76,6 @@ COREFCN_INC = \
   corefcn/ls-utils.h \
   corefcn/mex.h \
   corefcn/mexproto.h \
-  corefcn/mxarray.h \
   corefcn/oct-errno.h \
   corefcn/oct-fstrm.h \
   corefcn/oct-handle.h \

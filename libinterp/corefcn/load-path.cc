4262b4a984d6fd43a35f87277ba6b2242266d4c6
John W. Eaton <jwe@octave.org>
2015-01-07 18:02:17 -0500

    avoid indexing before beginning of string
    * load-path.cc (load_path::do_find_dir): Avoid indexing before
    beginning of string.

--- libinterp/corefcn/load-path.cc
+++ libinterp/corefcn/load-path.cc
@@ -1383,7 +1383,7 @@ load_path::do_find_dir (const std::string& dir) const
 
           size_t dir_len = dir.length ();
 
-          if (dname_len >= dir_len
+          if (dname_len > dir_len
               && file_ops::is_dir_sep (dname[dname_len - dir_len - 1])
               && dir.compare (dname.substr (dname_len - dir_len)) == 0)
             {

a5b591c0d0ff3993b6f5b182e7980d69fadb1cc9
Mike Miller <mtmiller@ieee.org>
2015-02-18 22:35:04 -0500

    Fix cholmod initialization with SuiteSparse >= 4.3 (bug #44285)
    * oct-sparse.h (SUITESPARSE_NAME): New macro.
    (SUITESPARSE_ASSIGN_FPTR2): Use it when initializing divcomplex and hypot
    for SuiteSparse >= 4.3.

--- liboctave/util/oct-sparse.h
+++ liboctave/util/oct-sparse.h
@@ -98,8 +98,9 @@ along with Octave; see the file COPYING.  If not, see
 //
 #if defined (SUITESPARSE_VERSION)
 # if SUITESPARSE_VERSION >= SUITESPARSE_VER_CODE (4, 3)
+#  define SUITESPARSE_NAME(name) SuiteSparse_ ## name
 #  define SUITESPARSE_ASSIGN_FPTR(f_name, f_var, f_assign) (SuiteSparse_config.f_name = f_assign)
-#  define SUITESPARSE_ASSIGN_FPTR2(f_name, f_var, f_assign) (SuiteSparse_config.f_name = CHOLMOD_NAME (f_assign))
+#  define SUITESPARSE_ASSIGN_FPTR2(f_name, f_var, f_assign) (SuiteSparse_config.f_name = SUITESPARSE_NAME (f_assign))
 # else
 #  define SUITESPARSE_ASSIGN_FPTR(f_name, f_var, f_assign) (f_var = f_assign)
 #  define SUITESPARSE_ASSIGN_FPTR2(f_name, f_var, f_assign) (f_var = CHOLMOD_NAME (f_assign))

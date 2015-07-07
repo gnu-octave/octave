c27bb2b75caa7acf4c90935fe4f8e1b024da26bc
John W. Eaton <jwe@octave.org>
2015-07-07 13:22:44 -0400

    fix dependencies for generated overload operator tests
    * test/module.mk: Make $(GENERATED_BC_OVERLOADS_FILES) depend on
    test/bc-overload-tests.stamp.

--- test/module.mk
+++ test/module.mk
@@ -282,6 +282,8 @@ GENERATED_BC_OVERLOADS_DIRS := \
 GENERATED_BC_OVERLOADS_FILES := \
   $(shell $(srcdir)/test/build-bc-overload-tests.sh test --list-files)
 
+$(GENERATED_BC_OVERLOADS_FILES): test/bc-overload-tests.stamp
+
 test/bc-overload-tests.stamp: test/build-bc-overload-tests.sh test/bc-overloads-expected
 	$(AM_V_GEN)rm -f $@ && \
 	$(srcdir)/test/build-bc-overload-tests.sh test $(srcdir)/test/bc-overloads-expected && \

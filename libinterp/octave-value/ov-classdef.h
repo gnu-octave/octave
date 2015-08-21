edb3b1e55ade3a2b2ca797b9c47922a02d416fa8
akira noda <you.akira.noda@gmail.com>
2015-08-21 13:05:52 -0400

    Fix compilation of classdef with the clang compiler (bug #41178)
    * ov-classdef.cc, ov-classdef.h (cdef_object_scalar::mark_as_constructed):
    Move function definition out of header file to avoid dependency on incomplete
    type, compilation error with clang.

--- libinterp/octave-value/ov-classdef.h
+++ libinterp/octave-value/ov-classdef.h
@@ -438,7 +438,7 @@ public:
 
   void mark_as_constructed (void) { ctor_list.clear (); }
 
-  void mark_as_constructed (const cdef_class& cls) { ctor_list.erase (cls); }
+  void mark_as_constructed (const cdef_class& cls);
 
   bool is_constructed (void) const { return ctor_list.empty (); }
 

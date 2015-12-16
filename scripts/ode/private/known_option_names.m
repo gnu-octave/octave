2ac7bb86e2c37cb67a0a1524d84a4a9cfd2a39fa
Carlo de Falco <carlo.defalco@polimi.it>
2015-12-16 19:39:59 +0100

    add file missing from commit
    * scripts/ode/private/known_option_names.m: new file.

---
+++ scripts/ode/private/known_option_names.m
@@ -0,0 +1,35 @@
+## Copyright (C) 2015, Carlo de Falco
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {} {@var{val} =} known_option_names ()
+## Return a list of known names for ode options.
+## @seealso{odeset,odeget}
+## @end deftypefn
+
+function ret = known_option_names ()
+
+ret = {"AbsTol"; "BDF"; "Events"; "InitialSlope";
+       "InitialStep"; "Jacobian"; "JConstant"; "JPattern";
+       "Mass"; "MassConstant"; "MassSingular"; "MaxOrder";
+       "MaxStep"; "MStateDependence"; "MvPattern";
+       "NonNegative"; "NormControl"; "OutputFcn"; "OutputSel";
+       "Refine"; "RelTol"; "Stats"; "Vectorized";
+       "TimeStepSize"; "TimeStepNumber"};
+
+endfunction

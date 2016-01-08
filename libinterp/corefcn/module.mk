e40e2b0473b549985a9737f9c0d4abd3185d29ff
John W. Eaton <jwe@octave.org>
2016-01-08 15:26:18 -0500

    revert accidental change made in changeset ebc439187d29
    * libinterp/corefcn/module.mk (COREFCN_SRC):
    Restore colloc.cc to the list.

--- libinterp/corefcn/module.mk
+++ libinterp/corefcn/module.mk
@@ -123,6 +123,7 @@ COREFCN_SRC = \
   libinterp/corefcn/c-file-ptr-stream.cc \
   libinterp/corefcn/cdisplay.c \
   libinterp/corefcn/cellfun.cc \
+  libinterp/corefcn/colloc.cc \
   libinterp/corefcn/coct-hdf5-types.c \
   libinterp/corefcn/comment-list.cc \
   libinterp/corefcn/conv2.cc \

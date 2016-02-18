aa5bef5c44d9e924ecf9e04abb35db52f56271f9
Mike Miller <mtmiller@octave.org>
2016-02-18 12:10:34 -0800

    maint: Fix doxygen build system integration
    * doc/module.mk (doxyhtml): Add Doxyfile as a prerequisite.
    (doxyhtml-maintainer-clean): Simplify rule to clean doxyhtml directory.
    
    * Doxyfile.in (PROJECT_LOGO): Use correct path to icon.
    (STRIP_FROM_PATH, INCLUDE_PATH, INPUT, EXAMPLE_PATH): Use abs_top_srcdir.
    (PREDEFINED): Add HAVE_OPENGL and HAVE_QSCINTILLA to the list, sort.

--- doc/module.mk
+++ doc/module.mk
@@ -51,13 +51,12 @@ doc_EXTRA_DIST += \
   doc/doxyhtml/Doxyfile.in \
   doc/doxyhtml/README
 
-doxyhtml: | doc/doxyhtml/$(octave_dirstamp)
+doxyhtml: doc/doxyhtml/Doxyfile | doc/doxyhtml/$(octave_dirstamp)
 	doxygen doc/doxyhtml/Doxyfile
 
 doxyhtml-maintainer-clean:
 	rm -f doc/doxygen_sqlite3.db
-	cd doc/doxyhtml && \
-	rm -rf `ls | $(GREP) -v Doxyfile.in | $(GREP) -v README`
+	rm -rf `ls -d doc/doxyhtml/* 2>/dev/null | $(GREP) -v 'Doxyfile\.in\|README'`
 
 DIRSTAMP_FILES += doc/doxyhtml/$(octave_dirstamp)
 

bbc47acbbba7e02ccfc018a4bb3960e4c1e63ced
Rik <rik@octave.org>
2016-08-25 08:50:37 -0700

    Fix changed fingerprint in BIST test (bug #48842).
    * __osmesa_print__.cc: Change fingerprint in 2nd BIST test to match current
    results.

--- libinterp/dldfcn/__osmesa_print__.cc
+++ libinterp/dldfcn/__osmesa_print__.cc
@@ -235,7 +235,7 @@ instead.
 %!   unwind_protect
 %!     plot (sin (0:0.1:2*pi));
 %!     __osmesa_print__ (hf, fn, "svgis2d");
-%!     assert (stat (fn).size, 7438, -0.1);
+%!     assert (stat (fn).size, 6276, -0.1);
 %!     img = __osmesa_print__ (hf);
 %!     assert (size (img), [get(hf, "position")([4, 3]), 3]);
 %!     ## Use pixel sum per RGB channel as fingerprint

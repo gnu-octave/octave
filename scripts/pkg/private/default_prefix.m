bfe74673553f39be74fef450f988740d20e48574
Philip Nienhuis <prnienhuis@users.sf.net>
2016-11-17 21:54:55 +0100

    (pkg): restore accidentally dropped 'desc' arg (bug #49635)

--- scripts/pkg/private/default_prefix.m
+++ scripts/pkg/private/default_prefix.m
@@ -21,7 +21,7 @@
 ## Undocumented internal function.
 ## @end deftypefn
 
-function [prefix, archprefix] = default_prefix (global_install)
+function [prefix, archprefix] = default_prefix (global_install, desc)
 
   if (global_install)
     prefix = fullfile (OCTAVE_HOME (), "share", "octave", "packages");

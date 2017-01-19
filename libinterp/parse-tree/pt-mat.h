fc81e196c85b80ed78f734ee58e3363c5032c4a4
John W. Eaton <jwe@octave.org>
2017-01-19 14:01:44 -0500

    delete duplicate function declarations
    * pt-mat.h (get_concat_class, maybe_warn_string_concat):
    Delete repeated declarations.

--- libinterp/parse-tree/pt-mat.h
+++ libinterp/parse-tree/pt-mat.h
@@ -81,11 +81,5 @@ get_concat_class (const std::string& c1, const std::string& c2);
 extern void
 maybe_warn_string_concat (bool all_dq_strings_p, bool all_sq_strings_p);
 
-extern std::string
-get_concat_class (const std::string& c1, const std::string& c2);
-
-extern void
-maybe_warn_string_concat (bool all_dq_strings_p, bool all_sq_strings_p);
-
 #endif
 

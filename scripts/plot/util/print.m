fee2e8c0178aa492f8f3a8757cf0a49cb3732b29
Avinoam Kalma <a.kalma@gmail.com>
2017-01-30 21:42:38 +0100

    Workaround segfault when an error occurs while printing (bug #49779)
    * print.m: check early that the requested directory exists and the requested
      file is writable
    * __print_parse_opts__.m: error out if the specified device/extension is unknown

--- scripts/plot/util/print.m
+++ scripts/plot/util/print.m
@@ -314,6 +314,18 @@ function print (varargin)
 
   opts = __print_parse_opts__ (varargin{:});
 
+  folder = fileparts (opts.name);
+  if (! isempty (folder) && ! exist (folder,"dir"))
+    error ("print: directory %s does not exist", folder);
+  endif
+
+  fid = fopen (opts.name, "w+");
+  if (fid == -1)
+    error ("print: cannot open file %s for writing", opts.name);
+  endif
+  fclose (fid);
+  unlink (opts.name);
+  
   opts.pstoedit_cmd = @pstoedit;
   opts.fig2dev_cmd = @fig2dev;
   opts.latex_standalone = @latex_standalone;

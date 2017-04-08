14181cfebf5fecd9218d5430c1553883bfc68140
Mike Miller <mtmiller@octave.org>
2017-04-08 16:44:40 -0700

    install +containers.Map to the right directory (bug #50763)
    * scripts/+containers/module.mk (scripts_containersdir): Fix definition to
    install +containers.Map.m correctly.

--- scripts/+containers/module.mk
+++ scripts/+containers/module.mk
@@ -3,7 +3,7 @@ FCN_FILE_DIRS += scripts/+containers
 scripts_containers_FCN_FILES = \
   scripts/+containers/Map.m
 
-scripts_containersdir = $(fcnfiledir)/strings
+scripts_containersdir = $(fcnfiledir)/+containers
 
 scripts_containers_DATA = $(scripts_containers_FCN_FILES)
 

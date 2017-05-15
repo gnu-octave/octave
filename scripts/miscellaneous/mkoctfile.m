e7c84d40ab15418a78711de2e1132b57ecb4588f
Carnë Draug <carandraug@octave.org>
2017-05-15 14:09:38 +0100

    doc: fix typo 'to the linker' not 'though the linker'

--- scripts/miscellaneous/mkoctfile.m
+++ scripts/miscellaneous/mkoctfile.m
@@ -54,7 +54,7 @@
 ## Add the run-time path to the link command.
 ##
 ## @item @nospell{-Wl,@dots{}}
-## Pass flags though the linker like @nospell{"-Wl,-rpath=@dots{}"}.
+## Pass options to the linker like @nospell{"-Wl,-rpath=@dots{}"}.
 ## The quotes are needed since commas are interpreted as command
 ## separators.
 ##

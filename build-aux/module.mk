7ab2a9a832f3549928b0e850580ee09012d9104f
John W. Eaton <jwe@octave.org>
2017-06-09 15:32:33 -0400

    move build-aux file lists and rules to build-aux/module.mk
    * build-aux/module.mk: Move build-aux file lists and rules here from
      Makefile.am.

--- build-aux/module.mk
+++ build-aux/module.mk
@@ -1,7 +1,48 @@
-## utility rules to aid development
-
 EXTRA_DIST += \
+  %reldir%/OctJavaQry.class \
+  %reldir%/OctJavaQry.java \
+  %reldir%/changelog.tmpl \
+  %reldir%/check-subst-vars.in.sh \
+  %reldir%/find-defun-files.sh \
+  %reldir%/find-files-with-tests.sh \
+  %reldir%/mk-builtins.pl \
+  %reldir%/mk-default-qt-settings.in.sh \
+  %reldir%/mk-f77-def.in.sh \
+  %reldir%/mk-hg-id.sh \
+  %reldir%/mk-mxarray-h.in.sh \
+  %reldir%/mk-octave-config-h.sh \
+  %reldir%/mk-opts.pl \
+  %reldir%/mk-version-h.in.sh \
+  %reldir%/move-if-change \
+  %reldir%/stl_algo.h-fixed \
+  %reldir%/subst-config-vals.in.sh \
+  %reldir%/subst-cross-config-vals.in.sh \
+  %reldir%/subst-default-vals.in.sh \
+  %reldir%/subst-f77-isnan-macro.in.sh \
+  %reldir%/subst-script-vals.in.sh \
   %reldir%/update-bug-status.sh
+
+GEN_CONFIG_SHELL = \
+  %reldir%/mk-default-qt-settings.sh \
+  %reldir%/mk-f77-def.sh \
+  %reldir%/mk-mxarray-h.sh \
+  %reldir%/mk-version-h.sh \
+  %reldir%/subst-config-vals.sh \
+  %reldir%/subst-cross-config-vals.sh \
+  %reldir%/subst-default-vals.sh \
+  %reldir%/subst-f77-isnan-macro.sh \
+  %reldir%/subst-script-vals.sh
+
+$(GEN_CONFIG_SHELL) : %.sh : %.in.sh config.status
+	$(AM_V_GEN)$(SHELL) config.status $@-tmp $@
+
+GEN_CONFIG_INC = \
+  oct-conf-post.h
+
+$(GEN_CONFIG_INC) : %.h : %.in.h config.status
+	$(AM_V_GEN)$(SHELL) config.status $@-tmp $@
+
+### utility rules to aid development
 
 ALL_TEST_FILES = \
   $(addprefix $(srcdir)/, $(LIBINTERP_TST_FILES_SRC)) \

ae42786a55f75c7c8839bf14407b9cce31aa7ba3
John W. Eaton <jwe@octave.org>
2017-06-26 11:15:33 -0400

    * mxarray.in.h: Update version for deprecated function.

--- libinterp/corefcn/mxarray.in.h
+++ libinterp/corefcn/mxarray.in.h
@@ -288,8 +288,7 @@ protected:
 
   mxArray_base (const mxArray_base&) { }
 
-  // FIXME: Deprecated in 4.2, remove in 4.6
-  OCTAVE_DEPRECATED (4.4, "use 'err_invalid_type' instead")
+  OCTAVE_DEPRECATED (4.2, "use 'err_invalid_type' instead")
   void invalid_type_error (void) const
   {
     error ("invalid type for operation");

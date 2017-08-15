825c8ef7b7040181066d4182a78289e6688a37bd
John W. Eaton <jwe@octave.org>
2017-08-15 08:58:03 -0400

    * dos.m: Ensure outputs are defined.

--- scripts/miscellaneous/dos.m
+++ scripts/miscellaneous/dos.m
@@ -39,7 +39,9 @@ function [status, text] = dos (command, echo_arg)
 
   if (nargin < 1 || nargin > 2)
     print_usage ();
-  elseif (! isunix ())
+  elseif (isunix ())
+    status = text = [];
+  else
     [status, text] = system (command);
     if (nargin > 1 || nargout == 0)
       printf ("%s\n", text);

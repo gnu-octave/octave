94c91878d8e0f7cdad74da01eadc5abf6f5bb9b5
Markus Mützel <markus.muetzel@gmx.de>
2017-08-26 09:39:00 +0200

    assert.m: Increase tolerance in BIST because of bug in mingw-w64 (bug #48365).

--- scripts/testfun/assert.m
+++ scripts/testfun/assert.m
@@ -624,7 +624,9 @@ endfunction
 %! x = [-40:0];
 %! y1 = (10.^x).*(10.^x);
 %! y2 = 10.^(2*x);
-%! assert (y1, y2, eps (y1));
+%! ## Increase tolerance from eps (y1) to 4*eps (y1) because of an upstream bug
+%! ## in mingw-w64: https://sourceforge.net/p/mingw-w64/bugs/466/
+%! assert (y1, y2, 4*eps (y1));
 %! fail ("assert (y1, y2 + eps*1e-70, eps (y1))");
 
 ## Multiple tolerances

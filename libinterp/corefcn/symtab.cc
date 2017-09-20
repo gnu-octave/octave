e7ff31fb573dedaaa449faebcc0b03c255057430
John W. Eaton <jwe@octave.org>
2017-09-20 17:45:25 -0400

    only cache parent function in anonymous function scopes
    * symtab.cc (symbol_table::scope::set_parent): Only cache parent
    function in anonymous function scopes.  See also changeset
    ef85638c605a.

--- libinterp/corefcn/symtab.cc
+++ libinterp/corefcn/symtab.cc
@@ -1825,10 +1825,15 @@ namespace octave
         // If m_parent is the top-level scope, there will be no parent
         // function.
 
-        octave_function *ov_fcn = m_parent->function ();
+        octave_function *current_fcn = function ();
 
-        if (ov_fcn)
-          m_parent_fcn = octave_value (ov_fcn, true);
+        if (current_fcn && current_fcn->is_anonymous_function ())
+          {
+            octave_function *parent_fcn = m_parent->function ();
+
+            if (parent_fcn)
+              m_parent_fcn = octave_value (parent_fcn, true);
+          }
       }
   }
 

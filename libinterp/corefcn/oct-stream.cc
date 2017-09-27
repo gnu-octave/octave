f0524bac419b96ae9bd504d88b43b7994226a31d
Daniel J Sebald <daniel.sebald@ieee.org>
2017-09-27 03:58:28 -0500

    Properly restore the input stream pointer at end of textscan (bug #52116 et al.)
    * oct-stream.cc (delimited_stream::refresh_buf): Do not update stream pointer
      buf_in_file from the current input stream position unless the delimited
      stream buffer is completely empty, e.g., at start of processing.

--- libinterp/corefcn/oct-stream.cc
+++ libinterp/corefcn/oct-stream.cc
@@ -1468,6 +1468,8 @@ namespace octave
         buf_in_file += (idx - buf);
         memmove (buf, idx, old_remaining);
       }
+    else
+      buf_in_file = i_stream.tellg ();    // record for destructor
 
     progress_marker -= idx - buf;         // where original idx would have been
     idx = buf;
@@ -1475,7 +1477,6 @@ namespace octave
     int gcount;   // chars read
     if (! i_stream.eof ())
       {
-        buf_in_file = i_stream.tellg ();   // record for destructor
         i_stream.read (buf + old_remaining, bufsize - old_remaining);
         gcount = i_stream.gcount ();
       }

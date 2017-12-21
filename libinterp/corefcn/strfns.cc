4588b74d8651c87be0799ff1f3a13b52502f05bb
Rik <rik@octave.org>
2017-12-20 17:19:11 -0800

    doc: Fix inconsistencies with char, strvcat, strread docstrings (bug #52702.
    * strfns.cc (Fchar, Fstrvcat): Correct number of spaces in results of example
    code in docstring.
    
    * strread.m: Use @itemx to associate one explanation with multiple items in
    table.

--- libinterp/corefcn/strfns.cc
+++ libinterp/corefcn/strfns.cc
@@ -66,13 +66,13 @@ converted through @code{char} can mostly be converted back with
 @example
 @group
 char ([97, 98, 99], "", @{"98", "99", 100@}, "str1", ["ha", "lf"])
-   @result{} ["abc    "
-       "       "
-       "98     "
-       "99     "
-       "d      "
-       "str1   "
-       "half   "]
+   @result{} ["abc  "
+       "     "
+       "98   "
+       "99   "
+       "d    "
+       "str1 "
+       "half "]
 @end group
 @end example
 @seealso{strvcat, cellstr}
@@ -192,12 +192,12 @@ converted through @code{strvcat} can mostly be converted back with
 @example
 @group
 strvcat ([97, 98, 99], "", @{"98", "99", 100@}, "str1", ["ha", "lf"])
-      @result{} ["abc    "
-          "98     "
-          "99     "
-          "d      "
-          "str1   "
-          "half   "]
+      @result{} ["abc "
+          "98  "
+          "99  "
+          "d   "
+          "str1"
+          "half"]
 @end group
 @end example
 @seealso{char, strcat, cstrcat}

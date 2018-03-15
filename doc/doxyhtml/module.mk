8ffe2bb13bfa4967a3993d4f86f851bd08ee316b
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-03-15 15:01:03 +0100

    Enable build of a reduced Doxygen version for compressed Qt help file.
    Currently a doxygen build is about 2 GB, certainly too much for any file
    distribution.  Thus `make doxyqt` now creates a complete Doxygen build of about
    70 MB without dot graphics and Javascript search tools.
    
    * doc/doxyhtml/Doxyfile.in: Update year.  Build Javascipt search engine by
    default HTML generation.
    
    * doc/doxyhtml/DoxyfileQt.patch: A patch for Doxyfile.in to make the reduced
    build for compressed Qt help files.
    
    * doc/doxyhtml/module.mk: Add target `doxyqt` for reduced build in subfolder
    `doxyhtml/doxyqt`.  Add new files to build system.

--- doc/doxyhtml/module.mk
+++ doc/doxyhtml/module.mk
@@ -8,16 +8,23 @@ DOXYGEN_PAGES = \
   %reldir%/pages/macros.dox \
   %reldir%/pages/README.md
 
+doxyqt: %reldir%/Doxyfile $(DOXYGEN_PAGES) | %reldir%/$(octave_dirstamp)
+	cp %reldir%/Doxyfile %reldir%/DoxyfileQt
+	cat $(srcdir)/%reldir%/DoxyfileQt.patch >> %reldir%/DoxyfileQt
+	doxygen %reldir%/DoxyfileQt
+
 doxyhtml: %reldir%/Doxyfile $(DOXYGEN_PAGES) | %reldir%/$(octave_dirstamp)
 	doxygen %reldir%/Doxyfile
 
+# This target is important for in source tree builds.
 doxyhtml-maintainer-clean:
-	rm -f %reldir%/pages/README.md
-	rm -rf `ls -d %reldir%/* 2>/dev/null | $(GREP) -v 'module\.mk\|Doxyfile\.in\|README\|pages$$'`
+	$(RM) %reldir%/pages/README.md
+	$(RM) -r `ls -d %reldir%/* 2>/dev/null | $(GREP) -v 'module\.mk\|Doxyfile\.in\|DoxyfileQt\.patch\|README\|pages$$'`
 
 doc_EXTRA_DIST += \
   $(DOXYGEN_PAGES) \
   %reldir%/Doxyfile.in \
+  %reldir%/DoxyfileQt.patch \
   %reldir%/README
 
 DIRSTAMP_FILES += %reldir%/$(octave_dirstamp)

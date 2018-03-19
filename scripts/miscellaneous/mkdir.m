5eff6f90ff14faed83a5b6509fee07166d474862
Rik <rik@octave.org>
2018-03-19 14:36:20 -0700

    mkdir.m: Fix GPL header block to match rest of Octave.
    mkdir.m: Fix GPL header block to match rest of Octave.

--- scripts/miscellaneous/mkdir.m
+++ scripts/miscellaneous/mkdir.m
@@ -1,7 +1,7 @@
 ## Copyright (C) 2016-2017 Lachlan Andrew
 ## Copyright (C) 2012 Carnë Draug
 ##
-## This program is free software: you can redistribute it and/or modify it
+## Octave is free software: you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.

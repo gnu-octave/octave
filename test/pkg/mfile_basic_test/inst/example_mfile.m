9a30020f2736738a897fbc22e3f3e8d5acf15157
John W. Eaton <jwe@octave.org>
2018-04-09 11:55:22 -0400

    update copyright dates in files in new test/pkg directory
    * example-package.txi, macros.texi, example_mfile.m, example_mfile.m:
    Update copyright dates.

--- test/pkg/mfile_basic_test/inst/example_mfile.m
+++ test/pkg/mfile_basic_test/inst/example_mfile.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2017 Olaf Till <i7tiol@t-online.de>
+## Copyright (C) 2017-2018 Olaf Till <i7tiol@t-online.de>
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as

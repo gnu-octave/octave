c020bcc4ec3b0aca8cbe59e5ef2a870f6c6fdcd3
John W. Eaton <jwe@octave.org>
2018-05-15 14:53:41 -0400

    back out changes attempting to automatically call handle class destructor
    These changes proved to cause too much trouble for stable.  Making
    Octave call destructors for handle class objects will have to be done
    on the default branch.
    
    The following changesets on the stable branch were backed out:
    
      changeset:   b328ff3ce0f7
      user:        Piotr Held <pjheld@gmail.com>
      date:        Thu Nov 02 10:27:11 2017 -0700
      summary:     call delete method for handle class objects (bug #46497)
    
      changeset:   2205c0ca02e7
      parent:      0548e32e6b27
      user:        John W. Eaton <jwe@octave.org>
      date:        Fri May 11 16:46:27 2018 -0400
      summary:     improve test for handle class destructor
    
      changeset:   97f1d513aaf6
      parent:      2205c0ca02e7
      user:        John W. Eaton <jwe@octave.org>
      date:        Mon May 14 14:47:19 2018 -0400
      summary:     defer deletion of temporaries in argument lists (bug #53844)
    
      changeset:   2f3a66d7cf8a
      parent:      c8f49ee7a687
      user:        John W. Eaton <jwe@octave.org>
      date:        Mon May 14 20:41:59 2018 -0400
      summary:     also preserve temporaries in indexing expressions (bug #53844)

--- libinterp/parse-tree/pt-eval.h
+++ libinterp/parse-tree/pt-eval.h
@@ -67,54 +67,6 @@ namespace octave
       ECHO_ALL = 4
     };
 
-    class deferred_delete_stack
-    {
-    public:
-
-      deferred_delete_stack (void) = default;
-
-      deferred_delete_stack (const deferred_delete_stack&) = default;
-
-      deferred_delete_stack& operator = (const deferred_delete_stack&) = default;
-
-      ~deferred_delete_stack (void) = default;
-
-      // An undefined value on the stack marks the boundary of the
-      // current frame.
-
-      void mark (void) { push (octave_value ()); }
-
-      void push (const octave_value& val) { m_stack.push (val); }
-
-      void pop_frame (void)
-      {
-        while (! m_stack.empty ())
-          {
-            octave_value val = val_pop ();
-
-            if (val.is_undefined ())
-              break;
-          }
-      }
-
-      void clear (void)
-      {
-        while (! m_stack.empty ())
-          m_stack.pop ();
-      }
-
-    private:
-
-      std::stack<octave_value> m_stack;
-
-      octave_value val_pop (void)
-      {
-        octave_value retval = m_stack.top ();
-        m_stack.pop ();
-        return retval;
-      }
-    };
-
     template <typename T>
     class value_stack
     {
@@ -173,8 +125,8 @@ namespace octave
     tree_evaluator (interpreter& interp)
       : m_interpreter (interp), m_result_type (RT_UNDEFINED),
         m_expr_result_value (), m_expr_result_value_list (),
-        m_deferred_delete_stack (), m_lvalue_list_stack (),
-        m_nargout_stack (), m_call_stack (interp), m_profiler (),
+        m_lvalue_list_stack (), m_nargout_stack (),
+        m_call_stack (interp), m_profiler (),
         m_max_recursion_depth (256), m_silent_functions (false),
         m_string_fill_char (' '), m_PS4 ("+ "), m_echo (ECHO_OFF),
         m_echo_state (false), m_echo_file_name (), m_echo_file_pos (1),
@@ -314,11 +266,6 @@ namespace octave
 
     // TRUE means we are evaluating some kind of looping construct.
     static bool in_loop_command;
-
-    void defer_deletion (const octave_value& val)
-    {
-      m_deferred_delete_stack.push (val);
-    }
 
     Matrix ignored_fcn_outputs (void) const;
 
@@ -539,8 +486,6 @@ namespace octave
     result_type m_result_type;
     octave_value m_expr_result_value;
     octave_value_list m_expr_result_value_list;
-
-    deferred_delete_stack m_deferred_delete_stack;
 
     value_stack<const std::list<octave_lvalue>*> m_lvalue_list_stack;
 

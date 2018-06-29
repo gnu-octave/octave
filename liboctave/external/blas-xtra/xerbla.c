034a0220f28538f98596886ec0a289ac34434774
Rik <rik@octave.org>
2018-06-29 14:15:08 -0700

    Use %ld format code in xerbla printf (bug #54206).
    * xerbla.c: F77_INT is cast to (long) in printf and needs to use %ld format
    code.

--- liboctave/external/blas-xtra/xerbla.c
+++ liboctave/external/blas-xtra/xerbla.c
@@ -61,7 +61,7 @@ F77_FUNC (xerbla, XERBLA) (F77_CONST_CHAR_ARG_DEF (s_arg, len),
   const char *s = F77_CHAR_ARG_USE (s_arg);
   int slen = F77_CHAR_ARG_LEN_USE (s_arg, len);
 
-  fprintf (stderr, "%.*s: parameter number %d is invalid\n", slen, s,
+  fprintf (stderr, "%.*s: parameter number %ld is invalid\n", slen, s,
            (long) (*info));
 
    if (xerbla_handler)

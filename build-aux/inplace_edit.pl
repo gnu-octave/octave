2d52f9b0573d14e0a165827ebbb4b27e2f4776cf
Rik <rik@octave.org>
2018-07-01 21:29:09 -0700

    build: Work around bug in perl 5.28.0 (bug #54202).
    * build-aux/inplace_edit.pl: New Perl script to replace using 'perl -i -pe'
    
    * doc/interpreter/module.mk: Replace instance of 'perl -i -pe' with call to
    inplace_edit.pl.

---
+++ build-aux/inplace_edit.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+################################################################################
+## File: inplace_edit.pl
+## Usage: perl inplace_edit.pl 'PERL_CODE' file1 [file2] [...]
+## Purpose: Run snippet of PERL_CODE on each line in a file and replace
+## existing line with the results of running the code.
+## This replaces perl -i -pe 'PERL_CODE' file1 [file2] ...
+## due to a problem in Perl 5.28 which restricts the number of files
+################################################################################
+
+## Create Perl code from first argument (-e CODE)
+eval "sub per_line_code { $ARGV[0]; }";
+shift @ARGV;
+
+## Loop over each file
+foreach $fname (@ARGV)
+{
+  rename ($fname, "$fname.$$") or die "Rename failed:$fname:$!"; 
+  open (my $FHI, "<", "$fname.$$") or die "Open failed:$fname.$$:$!";  
+  open (my $FHO, ">", "$fname") or die "Open failed:$fname:$!";  
+
+  ## Loop over each line
+  while (<$FHI>)
+  {
+    per_line_code ();
+    print $FHO $_;      
+  }
+
+  close ($FHI);
+  close ($FHO);
+  unlink "$fname.$$" or die "Delete failed:$fname.$$:$!";
+}

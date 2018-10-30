01a5b3790f210366701cdc706fb764285f5f7867
John W. Eaton <jwe@octave.org>
2018-10-30 19:44:01 -0400

    fix typo from changeset d85049849e8
    * graphics.cc (axes::properties::update_position): Fix typo in
    calculation of outer_height.

--- libinterp/corefcn/graphics.cc
+++ libinterp/corefcn/graphics.cc
@@ -7281,7 +7281,7 @@ axes::properties::update_position (void)
   double outer_top = inner_top + top_margin;
 
   double outer_width = outer_right - outer_left;
-  double outer_height = outer_top = outer_bottom;
+  double outer_height = outer_top - outer_bottom;
 
   Matrix outerbox (1, 4);
 

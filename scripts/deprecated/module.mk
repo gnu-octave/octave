65786997bde47d5a55e3d63f69be3b89a99a1235
Rik <rik@octave.org>
2018-12-20 21:13:02 -0800

    maint: Remove scripts scheduled for deprecation in version 6.0.
    * scripts/deprecated/module.mk: Remove deprecated scripts from build system.
    
    * scripts/deprecated/chop.m, scripts/deprecated/comma.m,
    scripts/deprecated/desktop.m, scripts/deprecated/java2mat.m,
    scripts/deprecated/paren.m, scripts/deprecated/semicolon.m,
    scripts/deprecated/tmpnam.m, scripts/deprecated/toascii.m:
    Remove deprecated scripts

--- scripts/deprecated/module.mk
+++ scripts/deprecated/module.mk
@@ -1,14 +1,6 @@
 FCN_FILE_DIRS += scripts/deprecated
 
-%canon_reldir%_FCN_FILES = \
-  %reldir%/chop.m \
-  %reldir%/comma.m \
-  %reldir%/desktop.m \
-  %reldir%/java2mat.m \
-  %reldir%/paren.m \
-  %reldir%/semicolon.m \
-  %reldir%/tmpnam.m \
-  %reldir%/toascii.m
+%canon_reldir%_FCN_FILES =
 
 %canon_reldir%dir = $(fcnfiledir)/deprecated
 

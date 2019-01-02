90306fd08c0e75b8640835aac296b4dc4d33eeb4
Rik <rik@octave.org>
2019-01-02 14:06:56 -0800

    chol.cc: Fix static analyzer detected issues (bug #55347).
    * chol.cc (chol2inv_internal): Remove unnecessary conditional test on info
    which is always 0.

--- liboctave/numeric/chol.cc
+++ liboctave/numeric/chol.cc
@@ -57,39 +57,38 @@ chol2inv_internal (const Matrix& r, bool is_upper = true)
     (*current_liboctave_error_handler) ("chol2inv requires square matrix");
 
   F77_INT n = octave::to_f77_int (r_nc);
-  F77_INT info = 0;
+  F77_INT info;
 
   Matrix tmp = r;
   double *v = tmp.fortran_vec ();
 
-  if (info == 0)
+  if (is_upper)
+    F77_XFCN (dpotri, DPOTRI, (F77_CONST_CHAR_ARG2 ("U", 1), n,
+                               v, n, info
+                               F77_CHAR_ARG_LEN (1)));
+  else
+    F77_XFCN (dpotri, DPOTRI, (F77_CONST_CHAR_ARG2 ("L", 1), n,
+                               v, n, info
+                               F77_CHAR_ARG_LEN (1)));
+
+  // FIXME: Should we check info exit value and possible report an error?
+
+  // If someone thinks of a more graceful way of doing this
+  // (or faster for that matter :-)), please let me know!
+
+  if (n > 1)
     {
       if (is_upper)
-        F77_XFCN (dpotri, DPOTRI, (F77_CONST_CHAR_ARG2 ("U", 1), n,
-                                   v, n, info
-                                   F77_CHAR_ARG_LEN (1)));
+        for (octave_idx_type j = 0; j < r_nc; j++)
+          for (octave_idx_type i = j+1; i < r_nr; i++)
+            tmp.xelem (i, j) = tmp.xelem (j, i);
       else
-        F77_XFCN (dpotri, DPOTRI, (F77_CONST_CHAR_ARG2 ("L", 1), n,
-                                   v, n, info
-                                   F77_CHAR_ARG_LEN (1)));
-
-      // If someone thinks of a more graceful way of doing this (or
-      // faster for that matter :-)), please let me know!
-
-      if (n > 1)
-        {
-          if (is_upper)
-            for (octave_idx_type j = 0; j < r_nc; j++)
-              for (octave_idx_type i = j+1; i < r_nr; i++)
-                tmp.xelem (i, j) = tmp.xelem (j, i);
-          else
-            for (octave_idx_type j = 0; j < r_nc; j++)
-              for (octave_idx_type i = j+1; i < r_nr; i++)
-                tmp.xelem (j, i) = tmp.xelem (i, j);
-        }
-
-      retval = tmp;
+        for (octave_idx_type j = 0; j < r_nc; j++)
+          for (octave_idx_type i = j+1; i < r_nr; i++)
+            tmp.xelem (j, i) = tmp.xelem (i, j);
     }
+
+  retval = tmp;
 
   return retval;
 }
@@ -106,39 +105,38 @@ chol2inv_internal (const FloatMatrix& r, bool is_upper = true)
     (*current_liboctave_error_handler) ("chol2inv requires square matrix");
 
   F77_INT n = octave::to_f77_int (r_nc);
-  F77_INT info = 0;
+  F77_INT info;
 
   FloatMatrix tmp = r;
   float *v = tmp.fortran_vec ();
 
-  if (info == 0)
+  if (is_upper)
+    F77_XFCN (spotri, SPOTRI, (F77_CONST_CHAR_ARG2 ("U", 1), n,
+                               v, n, info
+                               F77_CHAR_ARG_LEN (1)));
+  else
+    F77_XFCN (spotri, SPOTRI, (F77_CONST_CHAR_ARG2 ("L", 1), n,
+                               v, n, info
+                               F77_CHAR_ARG_LEN (1)));
+
+  // FIXME: Should we check info exit value and possible report an error?
+
+  // If someone thinks of a more graceful way of doing this (or
+  // faster for that matter :-)), please let me know!
+
+  if (n > 1)
     {
       if (is_upper)
-        F77_XFCN (spotri, SPOTRI, (F77_CONST_CHAR_ARG2 ("U", 1), n,
-                                   v, n, info
-                                   F77_CHAR_ARG_LEN (1)));
+        for (octave_idx_type j = 0; j < r_nc; j++)
+          for (octave_idx_type i = j+1; i < r_nr; i++)
+            tmp.xelem (i, j) = tmp.xelem (j, i);
       else
-        F77_XFCN (spotri, SPOTRI, (F77_CONST_CHAR_ARG2 ("L", 1), n,
-                                   v, n, info
-                                   F77_CHAR_ARG_LEN (1)));
-
-      // If someone thinks of a more graceful way of doing this (or
-      // faster for that matter :-)), please let me know!
-
-      if (n > 1)
-        {
-          if (is_upper)
-            for (octave_idx_type j = 0; j < r_nc; j++)
-              for (octave_idx_type i = j+1; i < r_nr; i++)
-                tmp.xelem (i, j) = tmp.xelem (j, i);
-          else
-            for (octave_idx_type j = 0; j < r_nc; j++)
-              for (octave_idx_type i = j+1; i < r_nr; i++)
-                tmp.xelem (j, i) = tmp.xelem (i, j);
-        }
-
-      retval = tmp;
+        for (octave_idx_type j = 0; j < r_nc; j++)
+          for (octave_idx_type i = j+1; i < r_nr; i++)
+            tmp.xelem (j, i) = tmp.xelem (i, j);
     }
+
+  retval = tmp;
 
   return retval;
 }

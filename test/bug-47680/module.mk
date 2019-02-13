eefd9648361b939003b52f524f260b10fa6c66b3
John W. Eaton <jwe@octave.org>
2019-02-13 13:03:20 +0000

    * test/bug-47680/module.mk (bug_47680_TEST_FILES): Correct file names.

--- test/bug-47680/module.mk
+++ test/bug-47680/module.mk
@@ -1,6 +1,6 @@
 bug_47680_TEST_FILES = \
   %reldir%/bug-47680.tst \
-  %reldir%/sub_class47680.m \
-  %reldir%/super_class47680.m
+  %reldir%/sub_bug47680.m \
+  %reldir%/super_bug47680.m
 
 TEST_FILES += $(bug_47680_TEST_FILES)

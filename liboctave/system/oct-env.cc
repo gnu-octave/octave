747ff185cd174c863ea2b67ab5a0f305dcb7f17c
Markus Mützel <markus.muetzel@gmx.de>
2019-03-03 19:48:55 +0100

    make_absolute_filename: Support file URI scheme (bug #36677).
    * oct-env.cc (do_absolute_pathname): File URIs are absolute.

--- liboctave/system/oct-env.cc
+++ liboctave/system/oct-env.cc
@@ -326,6 +326,10 @@ namespace octave
         return true;
 #endif
 
+      // check if using file URI scheme
+      if (len > 4 && s.compare (0, 5, "file:") == 0)
+        return true;
+
       return false;
     }
 

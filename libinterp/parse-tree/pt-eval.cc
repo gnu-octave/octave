83b631ddcc7326d65089e9151604a9c6e428f563
John W. Eaton <jwe@octave.org>
2019-09-25 10:27:33 -0400

    avoid shadowed variable warning introduced in previous change (bug #56948)
    * pt-eval.cc (debugger::repl): Capture evmgr by reference instead of
    passing it in argument list of lambda expression.

--- libinterp/parse-tree/pt-eval.cc
+++ libinterp/parse-tree/pt-eval.cc
@@ -195,10 +195,10 @@ namespace octave
 
             evmgr.set_workspace ();
 
-            frame.add ([fcn_nm, curr_debug_line] (event_manager& evmgr) {
+            frame.add ([&evmgr, fcn_nm, curr_debug_line] (void) {
                          evmgr.execute_in_debugger_event (fcn_nm,
                                                           curr_debug_line);
-                       }, std::ref (evmgr));
+                       });
 
             if (! silent)
               {

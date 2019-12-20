9b535246d65cde82cd32f744915b479660e459e9
Rik <rik@octave.org>
2019-12-20 14:36:03 -0800

    Create equally-spaced floating point values for rand ("double").
    * randmtzig.cc (randu53): Test and exclude value of 0 (left endpoint of range).
    Remove offset addition of 0.4 when constructing output which causes first
    interval to be unequally sized compared to all others (which are of size
    eps/2).

--- liboctave/numeric/randmtzig.cc
+++ liboctave/numeric/randmtzig.cc
@@ -393,9 +393,16 @@ namespace octave
   /* generates a random number on (0,1) with 53-bit resolution */
   static double randu53 (void)
   {
-    const uint32_t a = randi32 () >> 5;
-    const uint32_t b = randi32 () >> 6;
-    return (a*67108864.0+b+0.4) * (1.0/9007199254740992.0);
+    int32_t a, b;
+
+    do
+      {
+        a = randi32 () >> 5;
+        b = randi32 () >> 6;
+      }
+    while (a == 0 && b == 0);
+
+    return (a*67108864.0 + b) * (1.0/9007199254740992.0);
   }
 
   /* Determine mantissa for uniform doubles */

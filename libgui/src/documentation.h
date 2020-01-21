6969c6d2c87bf1e4399e68c22f0878df43ed8ff2
Torsten Lilge <ttl-octave@mailbox.org>
2020-01-21 19:52:17 +0100

    * documentation.h: fix spelling error

--- libgui/src/documentation.h
+++ libgui/src/documentation.h
@@ -90,7 +90,7 @@ namespace octave
   };
 
 
-  //! The documenztation main class derived from QSplitter
+  //! The documentation main class derived from QSplitter
 
   class documentation : public QSplitter
   {

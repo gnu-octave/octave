e560c1776c9f5c57adbbcdd9a79869c4b2ab3417
Torsten Lilge <ttl-octave@mailbox.org>
2020-05-10 09:27:53 +0200

    run slelected editor code as block, not line by line (bug #58323)
    * octave-qscintilla.cc (octave_qscintilla) update connection of signal when
      running selected code has finished due to new arguments;
      (contextmenu_run): collect all selected lines into a temporary script
      file, display all lines to be executed and add the executed lines
      to the history afterwards avoiding the temporary function for displaying
      and adding to history line by line;
      (ctx_menu_run_finished): this slots now also adds the executed lines
      to the history where a possible error during execution is taken into
      account
    
    * octave-qscintilla.h: new arguments in ctx_menu_run_finished_signal
      and its related slot ctx_menu_run_finished

--- libgui/src/m-editor/octave-qscintilla.h
+++ libgui/src/m-editor/octave-qscintilla.h
@@ -91,14 +91,12 @@ namespace octave
     void context_menu_break_condition_signal (int);
     void context_menu_break_once (int);
     void interpreter_event (const meth_callback& meth);
-    void ctx_menu_run_finished_signal (bool, QTemporaryFile*,
-                                       QTemporaryFile*, QTemporaryFile*);
+    void ctx_menu_run_finished_signal (bool, int, QTemporaryFile*, QTemporaryFile*);
     void focus_console_after_command_signal (void);
 
   private slots:
 
-    void ctx_menu_run_finished (bool, QTemporaryFile*,
-                                QTemporaryFile*, QTemporaryFile*);
+    void ctx_menu_run_finished (bool, int, QTemporaryFile*, QTemporaryFile*);
 
     void contextmenu_help (bool);
     void contextmenu_doc (bool);

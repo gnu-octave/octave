c7b087171fc4b3a532555625967bdc11f04b6b93
Rik <rik@octave.org>
2020-06-13 20:12:50 -0700

    Make gzip/bzip2 more Matlab compatible when directory is first argument (bug #58547).
    * gzip.cc (xzip): Strip source path from basename of source file.

--- libinterp/dldfcn/gzip.cc
+++ libinterp/dldfcn/gzip.cc
@@ -556,7 +556,10 @@ namespace octave
     const std::function<std::string(const std::string&)> mk_dest_path
       = [&out_dir, &ext] (const std::string& source_path) -> std::string
       {
-        const std::string basename = sys::env::base_pathname (source_path);
+        // Strip any relative path (bug #58547)
+        size_t pos = source_path.find_last_of (sys::file_ops::dir_sep_str ());
+        const std::string basename =
+          (pos == std::string::npos ? source_path : source_path.substr (pos+1));
         return sys::file_ops::concat (out_dir, basename + ext);
       };
 

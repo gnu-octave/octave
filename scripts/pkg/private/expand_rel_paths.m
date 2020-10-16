78c39ce6dea5349f57875b0b8284c18b8316ff6c
Rik <rik@octave.org>
2020-10-15 21:24:06 -0700

    maint: Use identical Copyright text in scripts/.
    * setstr.m, expand_rel_paths.m, betainc.m, gammainc.m,
    gammaincinv.m: Use identical Copyright text in scripts/ directory.

--- scripts/pkg/private/expand_rel_paths.m
+++ scripts/pkg/private/expand_rel_paths.m
@@ -5,18 +5,23 @@
 ## See the file COPYRIGHT.md in the top-level directory of this
 ## distribution or <https://octave.org/copyright/>.
 ##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
+## This file is part of Octave.
+##
+## Octave is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+## along with Octave; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
+##
+########################################################################
 
 ## -*- texinfo -*-
 ## @deftypefn {} {@var{pkg_list} =} expand_rel_paths (@var{pkg_list})

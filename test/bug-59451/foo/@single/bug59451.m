d22fe09694b4ad5213487b677b38ea03cd42d453
Markus Mützel <markus.muetzel@gmx.de>
2020-11-14 09:47:41 +0100

    test: Add BIST for bug #59451.
    * test/bug-59451/bug-59451.tst, test/bug-59451/bug59451.m,
      test/bug-59451/foo/@double/bug59451.m, test/bug-59451/foo/@single/bug59451.m,
      test/bug-59451/module.mk: Add new files.
    * test/module.mk: Include new module.

---
+++ test/bug-59451/foo/@single/bug59451.m
@@ -0,0 +1,3 @@
+function s = bug59451 (x, y)
+  s = "single";
+endfunction
---
+++ test/bug-59451/module.mk
@@ -0,0 +1,7 @@
+bug_59451_TEST_FILES = \
+  %reldir%/bug-59451.tst \
+  %reldir%/bug59451.m \
+  %reldir%/foo/@double/bug59451.m \
+  %reldir%/foo/@single/bug59451.m
+
+TEST_FILES += $(bug_59451_TEST_FILES)

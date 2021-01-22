f2237ad255c30067debdd4bf8404a5a435a61428
Rik <rik@octave.org>
2021-01-21 19:37:19 -0800

    uniquetol.m: Update to use more Octave conventions (bug #59850).
    * uniquetol.m: Review documentation.  Emit error if input A is not a floating
    point array rather than just emitting a warning.  Replace if/elseif/else tree
    with one call to ifelse().  Capitalize variables in error messages.  Verify
    that input A is a 2-D array when "ByRows" is true.  Add input validation
    for "DataScale" input.  For performance, avoid calculating data_scale variable
    if it has been given explicitly.  Make 3 BIST tests into xtests for behaviors
    which are not Matlab-compatible.  Add more BIST tests for input validation.

--- scripts/set/uniquetol.m
+++ scripts/set/uniquetol.m
@@ -24,30 +24,29 @@
 ########################################################################
 
 ## -*- texinfo -*-
-## @deftypefn  {} {@var{C} =} uniquetol (@var{A}, @var{tol})
-## @deftypefnx {} {@var{C} =} uniquetol (@var{A})
-## @deftypefnx {} {@var{C} =} uniquetol (@dots{}, @var{property}, @var{value}, @dots{})
-## @deftypefnx {} {[@var{C}, @var{IA}, @var{IC}] =} uniquetol (@dots{})
-## Return the unique elements of @var{A} using tolerance @var{tol}.
+## @deftypefn  {} {@var{c} =} uniquetol (@var{A})
+## @deftypefnx {} {@var{c} =} uniquetol (@var{A}, @var{tol})
+## @deftypefnx {} {@var{c} =} uniquetol (@dots{}, @var{property}, @var{value})
+## @deftypefnx {} {[@var{c}, @var{ia}, @var{ic}] =} uniquetol (@dots{})
+## Return the unique elements of @var{A} within tolerance @var{tol}.
 ##
-## Two values, @var{u} and @var{v}, are within tolerance if
-## @code {abs (@var{u} - @var{v}) <= @var{tol} * max (abs (@var{A}(:)))}.
+## Two values, @var{x} and @var{y}, are within tolerance if
+## @code{abs (@var{x} - @var{y}) <= @var{tol} * max (abs (@var{A}(:)))}.
 ##
 ## If it is unspecified, the default tolerance is 1e-12 for double precision
 ## input or 1e-6 for single precision input, respectively.
 ##
-## If the input @var{A} is not a floating point type, @code{unique} is called
-## without any additional options and a warning is issued.
+## The input input @var{A} must be a floating point type (double or single).
 ##
-## If the input @var{A} is a column vector, a column vector is returned.
-## Otherwise, a row vector is returned.
+## The output @var{C} is a row vector if the input @var{A} is a row vector.
+## For all other cases, a column vector is returned.
 ##
 ## @var{IA} and @var{IC} are column index vectors such that
 ## @code{@var{C} = @var{A}(@var{IA})} and @code{@var{A} = @var{C}(@var{IC})}.
 ##
-## Additionally, the function can be called with the following optional property
-## value pairs. Property value pairs must be passed after the other input
-## arguments:
+## Additionally, the function can be called with the following optional
+## property value pairs. Property value pairs must be passed after the other
+## input arguments:
 ##
 ## @table @asis
 ## @item @qcode{"ByRows"}, @code{true}
@@ -55,10 +54,11 @@
 ##
 ## @item @qcode{"DataScale"}, @var{DS}
 ## The tolerance test is changed to
-## @code{abs (@var{u}-@var{v}) <= @var{tol}*@var{DS}} where @var{DS} is a scalar
-## unless the property @qcode{"ByRows"} is set to @code{true}. In that case,
+## @code{abs (@var{x}-@var{y}) <= @var{tol}*@var{DS}} where @var{DS} is a scalar
+## unless the property @qcode{"ByRows"} is set to @code{true}.  In that case,
 ## @var{DS} can be a scalar or a vector with a length equal to the number of
-## rows in @var{A}.
+## rows in @var{A}.  Using a value of 1.0 for @var{DS} will change the
+## tolerance from a relative one to an absolute tolerance.
 ##
 ## @item @qcode{"OutputAllIndices"}, @code{true}
 ## @var{IA} is a cell array that contains the indices for all elements in
@@ -85,72 +85,77 @@
 ## @end deftypefn
 
 
-function [C, IA, IC] = uniquetol (A, varargin)
+function [c, ia, ic] = uniquetol (A, varargin)
 
   if (nargin < 1)
     print_usage ();
   endif
 
   if (isempty (A))
-    C = A;
-    IA = [];
-    IC = [];
+    c = A;
+    ia = [];
+    ic = [];
     return;
   endif
 
   if (! isfloat (A))
-    warning ("Octave:uniquetol:unsupported-type", ...
-             ["uniquetol: A is not a numeric, double or single precision " ...
-              "array. Calling unique without any additional options."]);
-    [C, IA, IC] = unique (A);
-    return;
+    error ("Octave:uniquetol:unsupported-type",
+           "uniquetol: A must be a double or single precision array");
   endif
 
-  by_rows = false;
-  output_all_indices = false;
-  data_scale = max (abs (A(! isinf (A))(:)));
-  size_A = size (A);
-
-  if (nargin == 1 || ! (isnumeric (varargin{1}) && isscalar (varargin{1})))
-    if (isa (A, "double"))
-      tol = 1e-12;
-    elseif (isa (A, "single"))
-      tol = 1e-6;
-    else
-      ## FIXME: Is there a float type that is not "double" or "single"?
-      error (["uniquetol: A must be a double or single precision floating ", ...
-              "point type"]);
-    endif
+  if (nargin == 1 || ischar (varargin{1}))
+    tol = ifelse (isa (A, "double"), 1e-12, 1e-6);
+  elseif (! (isfloat (varargin{1}) && isscalar (varargin{1})))
+    error ("Octave:uniquetol:unsupported-type",
+           "uniquetol: TOL must be a double or single precision scalar");
   else
     tol = varargin{1};
     varargin(1) = [];
   endif
 
-  if mod (numel (varargin), 2)
-    error ("uniquetol: property value arguments must be passed in pairs");
+  if (mod (numel (varargin), 2))
+    error ("uniquetol: PROPERTY/VALUE arguments must be passed in pairs");
   endif
+
+  by_rows = false;
+  output_all_indices = false;
+  data_scale = [];
 
   for k = 1:2:numel (varargin)
     if (! ischar (varargin{k}))
-      error ("uniquetol: property must be a string");
+      error ("uniquetol: PROPERTY must be a string");
     endif
 
     if (strcmpi (varargin{k}, "ByRows"))
       by_rows = logical (varargin{k+1});
+      if (by_rows && ndims (A) > 2)
+        error ('uniquetol: A must be a 2-D array when "ByRows" is true');
+      endif
     elseif (strcmpi (varargin{k}, "OutputAllIndices"))
       output_all_indices = logical (varargin{k+1});
     elseif (strcmpi (varargin{k}, "DataScale"))
       data_scale = varargin{k+1}(:).';
+      if (! isfloat (data_scale) || any (data_scale(:) < 0)
+          || any (isnan (data_scale(:))))
+        error ("uniquetol: DataScale must be a non-NaN, positive floating point scalar or vector");
+      endif
       cols_data_scale = columns (data_scale);
-      if (! (cols_data_scale == 1 || cols_data_scale == size_A(2)))
-        error ("uniquetol: incorrect size of data scale");
+      if (cols_data_scale != 1 && cols_data_scale != columns (A))
+        error ("uniquetol: invalid DataScale size");
       endif
     else
       error ("uniquetol: unknown property '%s'", varargin{k});
     endif
   endfor
 
-  if (! by_rows)
+  if (isempty (data_scale))
+    data_scale = max (abs (A(! isinf (A))(:)));
+  endif
+
+  if (by_rows)
+    isrowvec = false;
+  else
+    isrowvec = isrow (A);
     A = A(:);
   endif
 
@@ -158,7 +163,7 @@ function [C, IA, IC] = uniquetol (A, varargin)
   d = columns (A);
   Iall = zeros (points, 1);
   I = NaN (d, 1);
-  IA = {};
+  ia = {};
   J = NaN (d, 1);
   j = 1;
   ii = 0;
@@ -171,12 +176,12 @@ function [C, IA, IC] = uniquetol (A, varargin)
       equ = all (abs (A - A(i,:)) <= tol_data_scale, 2);
       equ(i,1) = equ(i,1) || any (! isfinite (A(i,:)), 2);
       sumeq = sum (equ);
-      ia = find (equ);
+      ia_tmp = find (equ);
       if (output_all_indices)
-        IA{end+1} = ia;
+        ia{end+1} = ia_tmp;
       endif
-      Iall(ii+(1:sumeq)) = ia;
-      I(j) = ia(1);
+      Iall(ii+(1:sumeq)) = ia_tmp;
+      I(j) = ia_tmp(1);
       J(equ) = j;
       ii += sumeq;
       j += 1;
@@ -185,54 +190,64 @@ function [C, IA, IC] = uniquetol (A, varargin)
 
   I(isnan (I)) = [];
   J(isnan (J)) = [];
-  C = A(I,:);
+  c = A(I,:);
 
-  if (size_A(1) == 1 && size_A(2) > 1 && ! by_rows)
-    C = C.';
+  ## Matlab-compatible orientation of output
+  if (isrowvec)
+    c = c.';
     I = I.';
     J = J.';
   endif
 
   if (! output_all_indices)
-    IA = I(1:j-1);
+    ia = I(1:j-1);
   endif
-  IC = J;
+  ic = J;
 
 endfunction
 
 
 %!assert (uniquetol ([1 1 2; 1 2 1; 1 1 2]), [1;2])
-%!assert (uniquetol ([1 1 2; 1 0 1; 1 1 2], 1e-12, "byrows", true), [1 1 2; 1 0 1])
+%!assert (uniquetol ([1 1 2; 1 0 1; 1 1 2], 1e-12, "byrows", true),
+%!        [1 1 2; 1 0 1])
 %!assert (uniquetol ([]), [])
 %!assert (uniquetol ([1]), [1])
-%!assert (uniquetol ([1, 2]), [1, 2])
+%!xtest <59850>
+%! ## FIXME: Matlab returns values sorted
+%! assert (uniquetol ([2, 1]), [1, 2]);
 %!assert (uniquetol ([1; 2]), [1; 2])
-%!assert (uniquetol ([1, NaN, Inf, NaN, Inf]), [1, NaN, Inf, NaN, Inf])
-%!assert (uniquetol (zeros (1,0)), zeros (1,0))
+%!xtest <59850>
+%! ## FIXME: Matlab returns only one unique value for Inf.
+%! assert (uniquetol ([1, NaN, Inf, NaN, Inf]), [1, Inf, NaN, NaN]);
+%!xtest <59850>
+%! ## FIXME: Matlab returns empty column vectors.
+%! ##        Do we want to bother with that?
+%! assert (uniquetol (zeros (1,0)), zeros (0,1));
 %!assert (uniquetol (zeros (1,0), 1e-12, "byrows", true), zeros (1,0))
 %!assert (uniquetol ([1,2,2,3,2,4], 1e-12, "byrows", true), [1,2,2,3,2,4])
 %!assert (uniquetol ([1,2,2,3,2,4]), [1,2,3,4])
 %!assert (uniquetol ([1,2,2,3,2,4].', 1e-12, "byrows", true), [1;2;3;4])
-%!assert (uniquetol (sparse ([2,0;2,0])), sparse([2;0]))
-%!assert (uniquetol (sparse ([1,2;2,3])), sparse([1;2;3]))
+%!assert (uniquetol (sparse ([2,0;2,0])), sparse ([2;0]))
+%!assert (uniquetol (sparse ([1,2;2,3])), sparse ([1;2;3]))
 %!assert (uniquetol ([1,2,2,3,2,4]', 1e-12, "byrows", true), [1;2;3;4])
-%!assert (uniquetol (single ([1,2,2,3,2,4]), 1e-12, "byrows", true), single ([1,2,2,3,2,4]))
+%!assert (uniquetol (single ([1,2,2,3,2,4]), 1e-12, "byrows", true),
+%!        single ([1,2,2,3,2,4]))
 %!assert (uniquetol (single ([1,2,2,3,2,4])), single ([1,2,3,4]))
-%!assert (uniquetol (single ([1,2,2,3,2,4].'), 1e-12, "byrows", true), single ([1;2;3;4]))
-%!assert (unique (uint8 ([1,2,2,3,2,4])), uint8 ([1,2,3,4]))
+%!assert (uniquetol (single ([1,2,2,3,2,4].'), 1e-12, "byrows", true),
+%!        single ([1;2;3;4]))
 
 %!test
-%! [y, i, j] = uniquetol ([1,1,2,3,3,3,4]);
-%! assert (y, [1,2,3,4]);
-%! assert (i, [1;3;4;7]);
-%! assert (j, [1;1;2;3;3;3;4]);
+%! [c, ia, ic] = uniquetol ([1,1,2,3,3,3,4]);
+%! assert (c, [1,2,3,4]);
+%! assert (ia, [1;3;4;7]);
+%! assert (ic, [1;1;2;3;3;3;4]);
 
 %!test
 %! A = [2, 3, 4; 2, 3, 4];
-%! [y, i, j] = uniquetol (A, "byrows", true);
-%! assert (y, [2, 3, 4]);
-%! assert (A(i,:), y);
-%! assert (y(j,:), A);
+%! [c, ia, ic] = uniquetol (A, "byrows", true);
+%! assert (c, [2, 3, 4]);
+%! assert (A(ia,:), c);
+%! assert (c(ic,:), A);
 
 %!test
 %! x = (2:7)'*pi;
@@ -240,27 +255,38 @@ endfunction
 %! C = uniquetol ([x; y]);
 %! assert (C, x);
 
+## Test "ByRows" Property
 %!test
 %! A = [0.06, 0.21, 0.38; 0.38, 0.21, 0.39; 0.54, 0.56, 0.41; 0.46, 0.52, 0.95];
 %! B = log (exp (1).^A);
 %! C = uniquetol ([A; B], "ByRows", true);
 %! assert (C, A);
 
+## Test "OutputAllIndices" Property
 %!test
 %! A = [.1 .2 .3 10];
-%! [C, IA, IC] = uniquetol (A, .1, "OutputAllIndices", true);
+%! [C, ia, ic] = uniquetol (A, .1, "OutputAllIndices", true);
 %! assert (C, [.1, 10]);
-%! assert (IA, {(1:3)', 4});
-%! assert (IC, [1; 1; 1; 2]);
+%! assert (ia, {(1:3)', 4});
+%! assert (ic, [1; 1; 1; 2]);
 
+## Test "DataScale" Property
 %!test
 %! x = 10^11;
 %! C = uniquetol ([x, exp(log(x))], 1e-6, "DataScale", 1);
 %! assert (C, [x, exp(log(x))]);
 
-%!error uniquetol ()
-%!error <in pairs> uniquetol (1, 2, "byrows")
-%!error <unknown property> uniquetol (1, "foo", "bar")
-%!error <unknown property> uniquetol (1, 2, "foo", "bar")
-%!error <must be a string> uniquetol (1, 2, 3, "foo")
-%!error <incorrect size> uniquetol (1, "DataScale", [1 2])
+## Test input validation
+%!error <Invalid call> uniquetol ()
+%!error <A must be a double or single precision array> uniquetol (int8 (1))
+%!error <TOL must be a double .* precision> uniquetol (1, int8 (1))
+%!error <TOL must be a .* scalar> uniquetol (1, [1, 2])
+%!error <arguments must be passed in pairs> uniquetol (1, 2, "byrows")
+%!error <PROPERTY must be a string> uniquetol (1, 2, 3, "bar")
+%!error <A must be a 2-D array> uniquetol (ones(2,2,2), "byrows", true)
+%!error <DataScale must be a .* floating point> uniquetol (1, "DataScale", '1')
+%!error <DataScale must be a .* positive> uniquetol (1, "DataScale", -1)
+%!error <DataScale must be a non-NaN> uniquetol (1, "DataScale", NaN)
+%!error <invalid DataScale size> uniquetol (1, "DataScale", [1 2])
+%!error <unknown property 'foo'> uniquetol (1, "foo", "bar")
+%!error <unknown property 'foo'> uniquetol (1, 2, "foo", "bar")

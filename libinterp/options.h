adb7f12f4eb179ae4ad935091494b37a3faf7be5
Markus Mützel <markus.muetzel@gmx.de>
2021-07-19 15:55:33 +0200

    options.h: Use C++ nullptr instead of 0.
    * libinterp/options.h (long_opts): Use C++ nullptr instead of 0. Remove gcc
      diagnostic pragmas that turned off the respective warning.

--- libinterp/options.h
+++ libinterp/options.h
@@ -30,19 +30,10 @@
 
 #include <iosfwd>
 
-// #include "version.h"
-
 // This is here so that it's more likely that the usage message and
 // the real set of options will agree.  Note: the '+' must come first
 // to prevent getopt from permuting arguments!
 static const char *short_opts = "+HWVdfhip:qvx";
-
-
-#if defined (HAVE_PRAGMA_GCC_DIAGNOSTIC)
-   // Disable warning temporarily.
-#  pragma GCC diagnostic push
-#  pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
-#endif
 
 // Long options.  See the comments in getopt.h for the meanings of the
 // fields in this structure.
@@ -70,48 +61,43 @@ static const char *short_opts = "+HWVdfhip:qvx";
 #define TRADITIONAL_OPTION 22
 struct octave_getopt_options long_opts[] =
 {
-  { "braindead",                octave_no_arg,       0, TRADITIONAL_OPTION },
-  { "built-in-docstrings-file", octave_required_arg, 0, BUILT_IN_DOCSTRINGS_FILE_OPTION },
-  { "debug",                    octave_no_arg,       0, 'd' },
-  { "debug-jit",                octave_no_arg,       0, DEBUG_JIT_OPTION },
-  { "doc-cache-file",           octave_required_arg, 0, DOC_CACHE_FILE_OPTION },
-  { "echo-commands",            octave_no_arg,       0, 'x' },
-  { "eval",                     octave_required_arg, 0, EVAL_OPTION },
-  { "exec-path",                octave_required_arg, 0, EXEC_PATH_OPTION },
-  { "experimental-terminal-widget", octave_no_arg,   0, EXPERIMENTAL_TERMINAL_WIDGET_OPTION },
-  { "force-gui",                octave_no_arg,       0, GUI_OPTION },
-  { "gui",                      octave_no_arg,       0, GUI_OPTION },
-  { "help",                     octave_no_arg,       0, 'h' },
-  { "image-path",               octave_required_arg, 0, IMAGE_PATH_OPTION },
-  { "info-file",                octave_required_arg, 0, INFO_FILE_OPTION },
-  { "info-program",             octave_required_arg, 0, INFO_PROG_OPTION },
-  { "interactive",              octave_no_arg,       0, 'i' },
-  { "jit-compiler",             octave_no_arg,       0, JIT_COMPILER_OPTION },
-  { "line-editing",             octave_no_arg,       0, LINE_EDITING_OPTION },
-  { "no-gui",                   octave_no_arg,       0, NO_GUI_OPTION },
-  { "no-gui-libs",              octave_no_arg,       0, NO_GUI_LIBS_OPTION },
-  { "no-history",               octave_no_arg,       0, 'H' },
-  { "no-init-file",             octave_no_arg,       0, NO_INIT_FILE_OPTION },
-  { "no-init-path",             octave_no_arg,       0, NO_INIT_PATH_OPTION },
-  { "no-line-editing",          octave_no_arg,       0, NO_LINE_EDITING_OPTION },
-  { "no-site-file",             octave_no_arg,       0, NO_SITE_FILE_OPTION },
-  { "no-window-system",         octave_no_arg,       0, 'W' },
-  { "norc",                     octave_no_arg,       0, 'f' },
-  { "path",                     octave_required_arg, 0, 'p' },
-  { "persist",                  octave_no_arg,       0, PERSIST_OPTION },
-  { "quiet",                    octave_no_arg,       0, 'q' },
-  { "server",                   octave_no_arg,       0, SERVER_OPTION },
-  { "silent",                   octave_no_arg,       0, 'q' },
-  { "texi-macros-file",         octave_required_arg, 0, TEXI_MACROS_FILE_OPTION },
-  { "traditional",              octave_no_arg,       0, TRADITIONAL_OPTION },
-  { "verbose",                  octave_no_arg,       0, 'V' },
-  { "version",                  octave_no_arg,       0, 'v' },
-  { 0,                          0,                   0, 0 }
+  { "braindead",                octave_no_arg,       nullptr, TRADITIONAL_OPTION },
+  { "built-in-docstrings-file", octave_required_arg, nullptr, BUILT_IN_DOCSTRINGS_FILE_OPTION },
+  { "debug",                    octave_no_arg,       nullptr, 'd' },
+  { "debug-jit",                octave_no_arg,       nullptr, DEBUG_JIT_OPTION },
+  { "doc-cache-file",           octave_required_arg, nullptr, DOC_CACHE_FILE_OPTION },
+  { "echo-commands",            octave_no_arg,       nullptr, 'x' },
+  { "eval",                     octave_required_arg, nullptr, EVAL_OPTION },
+  { "exec-path",                octave_required_arg, nullptr, EXEC_PATH_OPTION },
+  { "experimental-terminal-widget", octave_no_arg,   nullptr, EXPERIMENTAL_TERMINAL_WIDGET_OPTION },
+  { "force-gui",                octave_no_arg,       nullptr, GUI_OPTION },
+  { "gui",                      octave_no_arg,       nullptr, GUI_OPTION },
+  { "help",                     octave_no_arg,       nullptr, 'h' },
+  { "image-path",               octave_required_arg, nullptr, IMAGE_PATH_OPTION },
+  { "info-file",                octave_required_arg, nullptr, INFO_FILE_OPTION },
+  { "info-program",             octave_required_arg, nullptr, INFO_PROG_OPTION },
+  { "interactive",              octave_no_arg,       nullptr, 'i' },
+  { "jit-compiler",             octave_no_arg,       nullptr, JIT_COMPILER_OPTION },
+  { "line-editing",             octave_no_arg,       nullptr, LINE_EDITING_OPTION },
+  { "no-gui",                   octave_no_arg,       nullptr, NO_GUI_OPTION },
+  { "no-gui-libs",              octave_no_arg,       nullptr, NO_GUI_LIBS_OPTION },
+  { "no-history",               octave_no_arg,       nullptr, 'H' },
+  { "no-init-file",             octave_no_arg,       nullptr, NO_INIT_FILE_OPTION },
+  { "no-init-path",             octave_no_arg,       nullptr, NO_INIT_PATH_OPTION },
+  { "no-line-editing",          octave_no_arg,       nullptr, NO_LINE_EDITING_OPTION },
+  { "no-site-file",             octave_no_arg,       nullptr, NO_SITE_FILE_OPTION },
+  { "no-window-system",         octave_no_arg,       nullptr, 'W' },
+  { "norc",                     octave_no_arg,       nullptr, 'f' },
+  { "path",                     octave_required_arg, nullptr, 'p' },
+  { "persist",                  octave_no_arg,       nullptr, PERSIST_OPTION },
+  { "quiet",                    octave_no_arg,       nullptr, 'q' },
+  { "server",                   octave_no_arg,       nullptr, SERVER_OPTION },
+  { "silent",                   octave_no_arg,       nullptr, 'q' },
+  { "texi-macros-file",         octave_required_arg, nullptr, TEXI_MACROS_FILE_OPTION },
+  { "traditional",              octave_no_arg,       nullptr, TRADITIONAL_OPTION },
+  { "verbose",                  octave_no_arg,       nullptr, 'V' },
+  { "version",                  octave_no_arg,       nullptr, 'v' },
+  { nullptr,                    0,                   nullptr, 0 }
 };
-
-#if defined (HAVE_PRAGMA_GCC_DIAGNOSTIC)
-   // Restore prevailing warning state for remainder of the file.
-#  pragma GCC diagnostic pop
-#endif
 
 #endif

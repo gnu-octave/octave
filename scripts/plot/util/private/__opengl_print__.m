523fdf257ccf55568e984cd27aa254e696bc6269
Gene Harvey <gharveymn@gmail.com>
2021-09-24 15:44:38 -0500

    __opengl_print__.m: Use "findstr" on Windows instead of "cat".
    * scripts/plot/util/private/__opengl_print__.m: Use "findstr" on Windows
    instead of "cat".

--- scripts/plot/util/private/__opengl_print__.m
+++ scripts/plot/util/private/__opengl_print__.m
@@ -71,9 +71,8 @@ function opts = __opengl_print__ (opts)
       gl2ps_device = {sprintf("%snotxt", lower (suffix))};
       gl2ps_device{2} = "tex";
       if (dos_shell)
-        ## FIXME: this will only work on MinGW with the MSYS shell
-        pipeline = {sprintf('cat > "%s-inc.%s"', name, suffix)};
-        pipeline{2} = sprintf ('cat > "%s.tex"', name);
+        pipeline = {sprintf('findstr . > "%s-inc.%s"', name, suffix)};
+        pipeline{2} = sprintf ('findstr . > "%s.tex"', name);
       else
         pipeline = {sprintf('cat > "%s-inc.%s"', name, suffix)};
         pipeline{2} = sprintf ('cat > "%s.tex"', name);

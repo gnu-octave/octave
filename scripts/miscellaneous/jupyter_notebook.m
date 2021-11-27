340ad8f2a48bed96bdeb4292b365e0ffd2dffc84
Rik <rik@octave.org>
2021-11-26 17:39:45 -0800

    maint: Use same Copyright code block in all files.
    * jupyter_notebook.m, make_rel_paths.m, octave-svgconvert.cc:
    Use same Copyright code block in all files.

--- scripts/miscellaneous/jupyter_notebook.m
+++ scripts/miscellaneous/jupyter_notebook.m
@@ -1,19 +1,25 @@
+########################################################################
+##
 ## Copyright (C) 2021 The Octave Project Developers
 ##
-## This program is free software: you can redistribute it and/or modify it
+## See the file COPYRIGHT.md in the top-level directory of this
+## distribution or <https://octave.org/copyright/>.
+##
+## Octave is free software: you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## This program is distributed in the hope that it will be useful, but
+## Octave is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
+## along with Octave; see the file COPYING.  If not, see
 ## <https://www.gnu.org/licenses/>.
-
+##
+########################################################################
 
 classdef jupyter_notebook < handle
 

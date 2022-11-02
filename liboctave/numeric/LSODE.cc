6041c08ae4c75ea764fdf8197dc39390954c8682
Markus Mützel <markus.muetzel@gmx.de>
2022-11-02 18:26:14 +0100

    LSODE: Make sure m_iwork is sufficiently large for following assignments (bug #63303).
    * liboctave/numeric/LSODE.cc (LSODE::do_integrate): Make sure m_iwork is
    sufficiently large for following assignments during initialization.

--- liboctave/numeric/LSODE.cc
+++ liboctave/numeric/LSODE.cc
@@ -131,6 +131,8 @@ LSODE::do_integrate (double tout)
 
       user_jac_ignore_ml_mu = true;
 
+      m_iwork = Array<octave_f77_int_type> (dim_vector (2, 1));
+
       m_iwork(0) = lower_jacobian_subdiagonals ();  // 'ML' in dlsode.f
 
       m_iwork(1) = upper_jacobian_subdiagonals ();  // 'MU' in dlsode.f

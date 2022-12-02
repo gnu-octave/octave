728c8796ea36309cc1d58476ff27050183ad90ec
John W. Eaton <jwe@octave.org>
2022-12-02 10:11:46 -0500

    * resource-manager.cc: Fix copy/paste error from previous change.

--- libgui/src/resource-manager.cc
+++ libgui/src/resource-manager.cc
@@ -249,7 +249,7 @@ namespace octave
 
   gui_settings * resource_manager::get_default_settings (void) const
   {
-    if (m_settings)
+    if (! m_default_settings)
       {
         QString msg (QT_TR_NOOP ("Octave has lost its default settings.\n"
                                  "This should not happen.\n"

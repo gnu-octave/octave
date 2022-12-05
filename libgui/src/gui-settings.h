a7c4d7ffa0cf76b4229a39e45c7f45cdff82382c
John W. Eaton <jwe@octave.org>
2022-12-05 10:17:17 -0500

    Backed out changeset 34c3cd39c4b9
    Revert change because there is code that uses gui_pref.ignore, but
    only on MAC_OS: global_use_native_dialogs in
    gui-preferences-global.h.  Are there others?

--- libgui/src/gui-settings.h
+++ libgui/src/gui-settings.h
@@ -75,6 +75,9 @@ namespace octave
 
     QVariant value (const gui_pref& pref) const
     {
+      if (pref.ignore)
+        return pref.def;  // ignore the current pref and always use default
+
       return value (pref.key, pref.def);
     }
 

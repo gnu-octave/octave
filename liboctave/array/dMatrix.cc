436a7ce910ccc71184b5b9d989636320ae43ba59
Rik <rik@octave.org>
2023-03-24 15:43:28 -0700

    Re-use linspace code for scalars for computing linspace of vectors.
    Delete hand-rolled code for linspace of vectors which was not sophisticated and
    missed corner cases, and instead re-use existing code for scalars.
    * CMatrix.cc, dMatrix.cc, fCMatrix.cc, fMatrix.cc (linspace):
    Use a for loop to call linspace for scalars with each element from
    the vector input.  Aggregate the resulting row vectors into a return matrix.

--- liboctave/array/dMatrix.cc
+++ liboctave/array/dMatrix.cc
@@ -3029,19 +3029,7 @@ Matrix linspace (const ColumnVector& x1,
 
   retval.clear (m, n);
   for (octave_idx_type i = 0; i < m; i++)
-    retval.xelem (i, 0) = x1(i);
-
-  // The last column is unused so temporarily store delta there
-  double *delta = &retval.xelem (0, n-1);
-  for (octave_idx_type i = 0; i < m; i++)
-    delta[i] = (x1(i) == x2(i)) ? 0 : (x2(i) - x1(i)) / (n - 1);
-
-  for (octave_idx_type j = 1; j < n-1; j++)
-    for (octave_idx_type i = 0; i < m; i++)
-      retval.xelem (i, j) = x1(i) + j*delta[i];
-
-  for (octave_idx_type i = 0; i < m; i++)
-    retval.xelem (i, n-1) = x2(i);
+    retval.insert (linspace (x1(i), x2(i), n), i, 0);
 
   return retval;
 }
--- liboctave/array/fCMatrix.cc
+++ liboctave/array/fCMatrix.cc
@@ -3676,19 +3676,7 @@ FloatComplexMatrix linspace (const FloatComplexColumnVector& x1,
 
   retval.clear (m, n);
   for (octave_idx_type i = 0; i < m; i++)
-    retval.xelem (i, 0) = x1(i);
-
-  // The last column is unused so temporarily store delta there
-  FloatComplex *delta = &retval.xelem (0, n-1);
-  for (octave_idx_type i = 0; i < m; i++)
-    delta[i] = (x1(i) == x2(i)) ? 0 : (x2(i) - x1(i)) / (n - 1.0f);
-
-  for (octave_idx_type j = 1; j < n-1; j++)
-    for (octave_idx_type i = 0; i < m; i++)
-      retval.xelem (i, j) = x1(i) + static_cast<float> (j)*delta[i];
-
-  for (octave_idx_type i = 0; i < m; i++)
-    retval.xelem (i, n-1) = x2(i);
+    retval.insert (linspace (x1(i), x2(i), n), i, 0);
 
   return retval;
 }

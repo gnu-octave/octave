73f2efc1e0d2d39713ce8808690ccd482ea04693
John W. Eaton <jwe@octave.org>
2023-06-20 00:53:04 -0400

    * test/compile-bench/module.mk (compile_bench_TEST_FILES): Fix file list.

--- test/compile-bench/module.mk
+++ test/compile-bench/module.mk
@@ -1,23 +1,23 @@
 compile_bench_TEST_FILES = \
-  %reldir%/compile-bench/bench-octave/bench.m \
-  %reldir%/compile-bench/bench-octave/bench_cov.m\
-  %reldir%/compile-bench/bench-octave/bench_median.m\
-  %reldir%/compile-bench/bench-octave/do_until_loop_empty.m\
-  %reldir%/compile-bench/bench-octave/fib.m\
-  %reldir%/compile-bench/bench-octave/for_loop_binop_1.m\
-  %reldir%/compile-bench/bench-octave/for_loop_empty.m\
-  %reldir%/compile-bench/bench-octave/for_loop_fncall.m\
-  %reldir%/compile-bench/bench-octave/for_loop_ifs.m\
-  %reldir%/compile-bench/bench-octave/for_loop_matselfmul.m\
-  %reldir%/compile-bench/bench-octave/for_loop_silly.m\
-  %reldir%/compile-bench/bench-octave/for_loop_sinpi.m\
-  %reldir%/compile-bench/bench-octave/for_loop_subfun_1.m\
-  %reldir%/compile-bench/bench-octave/for_sum_1.m\
-  %reldir%/compile-bench/bench-octave/for_sum_2.m\
-  %reldir%/compile-bench/bench-octave/qsort_iterative.m\
-  %reldir%/compile-bench/bench-octave/qsort_recursive.m\
-  %reldir%/compile-bench/bench-octave/str_mod.m\
-  %reldir%/compile-bench/bench-octave/while_loop_empty.m\
-  %reldir%/compile-bench/bench-py3/bench.py
+  %reldir%/bench-octave/bench.m \
+  %reldir%/bench-octave/bench_cov.m\
+  %reldir%/bench-octave/bench_median.m\
+  %reldir%/bench-octave/do_until_loop_empty.m\
+  %reldir%/bench-octave/fib.m\
+  %reldir%/bench-octave/for_loop_binop_1.m\
+  %reldir%/bench-octave/for_loop_empty.m\
+  %reldir%/bench-octave/for_loop_fncall.m\
+  %reldir%/bench-octave/for_loop_ifs.m\
+  %reldir%/bench-octave/for_loop_matselfmul.m\
+  %reldir%/bench-octave/for_loop_silly.m\
+  %reldir%/bench-octave/for_loop_sinpi.m\
+  %reldir%/bench-octave/for_loop_subfun_1.m\
+  %reldir%/bench-octave/for_sum_1.m\
+  %reldir%/bench-octave/for_sum_2.m\
+  %reldir%/bench-octave/qsort_iterative.m\
+  %reldir%/bench-octave/qsort_recursive.m\
+  %reldir%/bench-octave/str_mod.m\
+  %reldir%/bench-octave/while_loop_empty.m\
+  %reldir%/bench-py3/bench.py
 
 TEST_FILES += $(compile_bench_TEST_FILES)

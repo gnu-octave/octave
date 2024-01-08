aadd90be18fa8f43baed7bc2c8b229e9894f5914
Markus Mützel <markus.muetzel@gmx.de>
2024-01-08 14:30:13 +0100

    Array.h: Do not export inlined function "fortran_vec".
    * liboctave/array/Array.h (fortran_vec): Function is defined in header and has
    inline attribute. So, do not export it from library.

--- liboctave/array/Array.h
+++ liboctave/array/Array.h
@@ -667,7 +667,7 @@ public:
 
   // Alias for direct read-write access to Array data.
   // FIXME: It is recommended to use rwdata() in future code for clarity.
-  inline OCTARRAY_API T * fortran_vec ()
+  inline T * fortran_vec ()
   { return rwdata (); }
 
   OCTARRAY_OVERRIDABLE_FUNC_API bool is_shared () const

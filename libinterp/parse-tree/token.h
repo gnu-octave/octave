f5dee78ac2d201821e095304f8186d669ac9d6a9
John W. Eaton <jwe@octave.org>
2024-02-29 15:00:40 -0500

    * token.h (class token): Use member initializers where possible.

--- libinterp/parse-tree/token.h
+++ libinterp/parse-tree/token.h
@@ -71,52 +71,34 @@ public:
     while_end,
   };
 
+public:
+
   token (int tv, const filepos& beg_pos, const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (generic_token),
-      m_tok_info (), m_orig_text ()
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv)
   { }
 
-  token (int tv, bool is_kw, const filepos& beg_pos,
-         const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv),
-      m_type_tag (is_kw ? keyword_token : generic_token), m_tok_info (),
-      m_orig_text ()
+  token (int tv, bool is_kw, const filepos& beg_pos, const filepos& end_pos)
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (is_kw ? keyword_token : generic_token)
   { }
 
-  token (int tv, const char *s, const filepos& beg_pos,
-         const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (string_token),
-      m_tok_info (s), m_orig_text ()
+  token (int tv, const char *s, const filepos& beg_pos, const filepos& end_pos)
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (string_token), m_tok_info (s)
   { }
 
-  token (int tv, const std::string& s, const filepos& beg_pos,
-         const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (string_token),
-      m_tok_info (s), m_orig_text ()
+  token (int tv, const std::string& s, const filepos& beg_pos, const filepos& end_pos)
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (string_token), m_tok_info (s)
   { }
 
-  token (int tv, const octave_value& val, const std::string& s,
-         const filepos& beg_pos, const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (numeric_token),
-      m_tok_info (val), m_orig_text (s)
+  token (int tv, const octave_value& val, const std::string& s, const filepos& beg_pos, const filepos& end_pos)
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (numeric_token), m_tok_info (val), m_orig_text (s)
   { }
 
   token (int tv, end_tok_type t, const filepos& beg_pos, const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (ettype_token),
-      m_tok_info (t), m_orig_text ()
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (ettype_token), m_tok_info (t)
   { }
 
-  token (int tv, const std::string& meth, const std::string& cls,
-         const filepos& beg_pos, const filepos& end_pos)
-    : m_maybe_cmd (false), m_tspc (false), m_beg_pos (beg_pos),
-      m_end_pos (end_pos), m_tok_val (tv), m_type_tag (scls_name_token),
-      m_tok_info (meth, cls), m_orig_text ()
+  token (int tv, const std::string& meth, const std::string& cls, const filepos& beg_pos, const filepos& end_pos)
+    : m_beg_pos (beg_pos), m_end_pos (end_pos), m_tok_val (tv), m_type_tag (scls_name_token), m_tok_info (meth, cls)
   { }
 
   OCTAVE_DEFAULT_COPY_MOVE_DELETE (token)
@@ -159,16 +141,16 @@ public:
 
 private:
 
-  bool m_maybe_cmd;
+  bool m_maybe_cmd {false};
 
-  bool m_tspc;
+  bool m_tspc {false};
 
   filepos m_beg_pos;
   filepos m_end_pos;
 
   int m_tok_val;
 
-  token_type m_type_tag;
+  token_type m_type_tag {generic_token};
 
   class superclass_info
   {

5b77130e066bb05ed11b06033324ad2b4547fba5
Markus Mützel <markus.muetzel@gmx.de>
2024-03-11 17:44:16 +0100

    patch: Allow row or column vector for color per face (bug #65431).
    * scripts/plot/draw/private/__patch__.m: Ignore orientation of input vector
    with color values per face.

--- scripts/plot/draw/private/__patch__.m
+++ scripts/plot/draw/private/__patch__.m
@@ -118,7 +118,7 @@ function h = __patch__ (p, varargin)
           args{8} = "flat";
         endif
         args{9} = "facevertexcdata";
-        args{10} = c;
+        args{10} = c(:);
       elseif (isrow (c) && numel (c) == 3)
         ## One RGB color
         args{7} = "facecolor";

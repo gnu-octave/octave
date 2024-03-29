ea021d0228eabc6290ccab6766aab4fcc9d3a943
Markus Mützel <markus.muetzel@gmx.de>
2024-03-29 14:53:49 +0100

    gui: Right-click menu for selected text in command window widget (bug #65518).
    * libgui/qterminal/libqterminal/QTerminal.cc
    (QTerminal::handleCustomContextMenuRequested): Discard any non-word characters
    at start of selected string (not just whitespace) for options in context menu.
    Use double-quotes around matched strings from selection in context menu. Make
    sure there is a match for the used regular expression for the "Documentation"
    option before using it.

--- libgui/qterminal/libqterminal/QTerminal.cc
+++ libgui/qterminal/libqterminal/QTerminal.cc
@@ -108,7 +108,7 @@ QTerminal::handleCustomContextMenuRequested (const QPoint& at)
     if (has_selected_text)
       {
         // Find first word in selected text, trim everything else
-        QRegularExpression expr {"^\\s*(\\w+)"};
+        QRegularExpression expr {"(\\w+)"};
         QRegularExpressionMatch match = expr.match (selected_text);
 
         if (match.hasMatch ())
@@ -116,19 +116,24 @@ QTerminal::handleCustomContextMenuRequested (const QPoint& at)
             QString expr_found = match.captured (1);
 
             m_edit_selected_action->setVisible (true);
-            m_edit_selected_action->setText (tr ("Edit %1").arg (expr_found));
+            m_edit_selected_action->setText (tr ("Edit \"%1\"").arg (expr_found));
             m_edit_selected_action->setData (expr_found);
 
             m_help_selected_action->setVisible (true);
-            m_help_selected_action->setText (tr ("Help on %1").arg (expr_found));
+            m_help_selected_action->setText (tr ("Help on \"%1\"").arg (expr_found));
             m_help_selected_action->setData (expr_found);
+          }
 
-            // Grab all of selected text, but trim leading/trailing whitespace
-            expr.setPattern ("^\\s*(\\w.*)\\s*$");
-            match = expr.match (selected_text);
-            expr_found = match.captured (1);
+        // Grab all of selected text, but trim leading non-word characters
+        // and trailing whitespace
+        expr.setPattern ("(\\w.*)\\s*$");
+        match = expr.match (selected_text);
+        if (match.hasMatch ())
+          {
+            QString expr_found = match.captured (1);
+
             m_doc_selected_action->setVisible (true);
-            m_doc_selected_action->setText (tr ("Documentation on %1")
+            m_doc_selected_action->setText (tr ("Documentation on \"%1\"")
                                             .arg (expr_found));
             m_doc_selected_action->setData (expr_found);
           }

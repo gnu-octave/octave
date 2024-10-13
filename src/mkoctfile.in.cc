bfedc2c1e1e28a2854fccbac266d21deaa21b302
Markus Mützel <markus.muetzel@gmx.de>
2024-10-13 16:28:13 +0200

    mkoctfile: Use more resilient approach to link Octave libraries.
    * configure.ac: Add path to Octave libraries to linker search path in
    MKOCTFILE_OCT_LINK_DEPS when linking to these libraries (i.e., when linking all
    dependencies or on Windows). That is more resilient than adding it to LDFLAGS in
    mkoctfile because LDFLAGS could be overridden with the value of an environment
    variable with that name. Expand configure variables in MKOCTFILE_OCT_LINK_DEPS.
    (The latter was already missing since 0bf6b7e1367c.)
    * src/mkoctfile.in.cc: Remove OS specific code that is no longer needed.

--- src/mkoctfile.in.cc
+++ src/mkoctfile.in.cc
@@ -250,12 +250,6 @@ make_vars_map (bool link_stand_alone, bool verbose, bool debug)
 
   std::string DEFAULT_LDFLAGS;
 
-#if (defined (OCTAVE_USE_WINDOWS_API) || defined (CROSS) || defined (OCTAVE_LINK_ALL_DEPS))
-  // We'll be linking the files we compile with -loctinterp and -loctave,
-  // so we need to know where to find them.
-  DEFAULT_LDFLAGS += "-L" + quote_path (vars["OCTLIBDIR"]);
-#endif
-
   if (vars["LIBDIR"] != "/usr/lib")
     DEFAULT_LDFLAGS += " -L" + quote_path (vars["LIBDIR"]);
 

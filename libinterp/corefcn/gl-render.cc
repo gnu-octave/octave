de5b5e88202a8b614cf387ae572788553bc291d1
Rik <rik@octave.org>
2025-08-24 23:02:55 +0200

    Render ticklabels exactly as given (bug #67302)
    Octave was stripping leading and trailing spaces of ticklabels before rendering
    which is not Matlab-compatible and ignores the user's intent.
    * NEWS.11.md: Announce that Octave no longer strips leading and trailing spaces
    from ticklabels.
    
    * gl-render.cc (render_ticktexts): Delete 2 lines that trim spaces from around
    ticklabels.

--- libinterp/corefcn/gl-render.cc
+++ libinterp/corefcn/gl-render.cc
@@ -1097,8 +1097,6 @@ opengl_renderer::render_ticktexts (const Matrix& ticks,
           Matrix b;
 
           std::string label (ticklabels(i % nlabels));
-          label.erase (0, label.find_first_not_of (' '));
-          label = label.substr (0, label.find_last_not_of (' ')+1);
 
           // FIXME: As tick text is transparent, shouldn't it be
           //        drawn after axes object, for correct rendering?

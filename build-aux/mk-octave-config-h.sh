5d19be823595bd711309ec9f9994b300001a5ad6
Markus Mützel <markus.muetzel@gmx.de>
2026-06-03 14:03:01 +0200

    Use matching file offset bits in Octave and its dependencies (bug #68353).
    * build-aux/mk-octave-config-h.sh: Define _FILE_OFFSET_BITS when building Octave
    dependencies to the same value that was used when building Octave.

--- build-aux/mk-octave-config-h.sh
+++ build-aux/mk-octave-config-h.sh
@@ -142,6 +142,10 @@ fi
 
 echo ""
 
+$SED -n 's/#\(\(undef\|define\) _FILE_OFFSET_BITS.*$\)/#  \1/p' $config_h_file
+
+echo ""
+
 have_roundl=no
 if grep "#define HAVE_ROUNDL" $config_h_file > /dev/null; then
   have_roundl=yes

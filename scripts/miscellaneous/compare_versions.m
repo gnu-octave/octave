999e4d5be1a8d3174c415ef5b02f8d872079b270
Markus Mützel <markus.muetzel@gmx.de>
2026-06-10 18:30:14 +0200

    compare_versions: Fix regression with different length numbers and suffix.
    * scripts/miscellaneous/compare_versions.m: Use "all" instead of "any" in
    conditions. Add a new test case.

--- scripts/miscellaneous/compare_versions.m
+++ scripts/miscellaneous/compare_versions.m
@@ -112,11 +112,11 @@ function tf = compare_versions (v1, v2, operator)
   endif
 
   v1n = str2double (ostrsplit (v1nochar, ".")');
-  if (any (isnan (v1n)))
+  if (all (isnan (v1n)))
     v1n = [];
   endif
   v2n = str2double (ostrsplit (v2nochar, ".")');
-  if (any (isnan (v2n)))
+  if (all (isnan (v2n)))
     v2n = [];
   endif
 
@@ -206,6 +206,7 @@ endfunction
 %!assert (compare_versions ("1.1.0a", "1.1.0b", "!="), true)
 %!assert (compare_versions ("1.1.0test", "1.1.0b", "=="), false)
 %!assert (compare_versions ("1.1.0test", "1.1.0test", "=="), true)
+%!assert (compare_versions ("1.15.0.dev", "1.9", ">="), true)
 
 ## make sure that it won't just give true output
 %!assert (compare_versions ("1", "0", "=="), false)

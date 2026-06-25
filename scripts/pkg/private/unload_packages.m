1945e331da204dd947eb9ebfeda64f0035bc1db3
Torsten Lilge <ttl-octave@mailbox.org>
2026-06-25 22:18:41 +0200

    register package qt-help files in doc subdirectory also if gui is not running
    * scripts/pkg/private/load_packages_and_dependencies.m:
    * scripts/pkg/private/unload_packages.m
      call event manager un-/register routine even if gui is not running

--- scripts/pkg/private/unload_packages.m
+++ scripts/pkg/private/unload_packages.m
@@ -108,19 +108,17 @@ function unload_packages (files, handle_deps, local_list, global_list)
   endfor
 
   ## Unregister Qt help files
-  if (isguirunning)
-    for (ii = 1:numel (dirs))
-      qt_help_file = fullfile (dirs{ii}, "doc", [names{ii},".qch"]);
-      try
-        if (exist (qt_help_file, "file") &&
-            exist("__event_manager_unregister_documentation__"))
-          __event_manager_unregister_documentation__ (qt_help_file);
-        endif
-      catch
-        # do nothing
-      end_try_catch
-    endfor
-  endif
+  for (ii = 1:numel (dirs))
+    qt_help_file = fullfile (dirs{ii}, "doc", [names{ii},".qch"]);
+    try
+      if (exist (qt_help_file, "file") &&
+          exist("__event_manager_unregister_documentation__"))
+        __event_manager_unregister_documentation__ (qt_help_file);
+      endif
+    catch
+      # do nothing
+    end_try_catch
+  endfor
 
 endfunction
 

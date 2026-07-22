1d4a5ada532f078fee1c682707e11ce1948d3e11
Dmitri A. Sergatskov <dasergatskov@gmail.com>
2026-07-22 12:27:35 -0400

    Avoid removed std::shared_ptr::unique method (bug #68546)
      The std::shared_ptr::unique method was removed in C++20 and is unavailable
      with libc++.  Comparing use_count with one provides equivalent behavior.
    
    * liboctinterp/parse-tree/pt-bytecode-vm.cc (vm::execute_code): Replace calls
      to shared_ptr::unique with use_count comparison.

--- liboctinterp/parse-tree/pt-bytecode-vm.cc
+++ liboctinterp/parse-tree/pt-bytecode-vm.cc
@@ -1583,7 +1583,8 @@ ret:
     // If the pointer is not shared, stash it in a cache which is used
     // to avoid having to allocate shared pointers each frame push.
     // If it is a closure context, there might be weak pointers to it from function handles.
-    if (fp.unique () && m_frame_ptr_cache.size () < 8 && ! fp->is_closure_context () 
+    if (fp.use_count () == 1 && m_frame_ptr_cache.size () < 8
+        && ! fp->is_closure_context ()
         && fp->is_user_fcn_frame ())
       {
         fp->vm_clear_for_cache ();
@@ -6627,7 +6628,7 @@ ret_anon:
     std::shared_ptr<stack_frame> fp = m_tw->pop_return_stack_frame ();
     // If the pointer is not shared, stash it in a cache which is used
     // to avoid having to allocate shared pointers each frame push.
-    if (fp.unique () && m_frame_ptr_cache.size () < 8)
+    if (fp.use_count () == 1 && m_frame_ptr_cache.size () < 8)
       {
         fp->vm_clear_for_cache ();
         m_frame_ptr_cache.push_back (std::move (fp));
